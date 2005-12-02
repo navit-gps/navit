4ff12509b7a6ceacd4f641e752f4a5ab2f86cf87
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2005-12-02 12:32:47 +0000

    Converting to autoconf/automake

--- src/speech.c
+++ src/speech.c
@@ -1,5 +1,5 @@
 /* speechd simple client program
- * CVS revision: $Id: speech.c,v 1.1 2005-12-02 10:41:56 martin-s Exp $
+ * CVS revision: $Id: speech.c,v 1.2 2005-12-02 12:32:47 martin-s Exp $
  * Author: Tomas Cerha <cerha@brailcom.cz> */
 
 #include <sys/types.h>
@@ -11,8 +11,10 @@
 #include <stdlib.h>
 #include <glib.h>
 #include <stdarg.h>
-
+#include "config.h"
+#ifdef HAVE_LIBSPEECHD
 #include <libspeechd.h>
+#endif
 #include "speech.h"
 
 struct speech {
@@ -21,6 +23,7 @@ struct speech {
 
 struct speech *
 speech_new(void) {
+#ifdef HAVE_LIBSPEECHD
 	struct speech *this;
 	int sockfd;
 
@@ -32,30 +35,41 @@ speech_new(void) {
 		this->sockfd=sockfd;
 	}
 	return this;
+#else
+	return NULL;
+#endif
 }
 
 int 
 speech_say(struct speech *this, char *text) {
+#ifdef HAVE_LIBSPEECHD
 	int err;
 
 	err = spd_sayf(this->sockfd, 2, text);
 	if (err != 1)
 		return 1;
+#endif
 	return 0;
 }
 
 int
 speech_sayf(struct speech *this, char *format, ...) {
+#ifdef HAVE_LIBSPEECHD
 	char buffer[8192];
 	va_list ap;
 	va_start(ap,format);
 	vsnprintf(buffer, 8192, format, ap);
 	return speech_say(this, buffer);
+#else
+	return 0;
+#endif
 }
 
 int 
 speech_destroy(struct speech *this) {
+#ifdef HAVE_LIBSPEECHD
 	spd_close(this->sockfd);
 	g_free(this);
+#endif
 	return 0;
 }
