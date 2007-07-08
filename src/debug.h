1f8e2f001ad5d68ca926f04f5ea71ee240986f78
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-07-08 17:46:27 +0000

    Made debugging configurable via xml

--- src/debug.h
+++ src/debug.h
@@ -3,6 +3,7 @@ int debug_level;
 
 /* prototypes */
 void debug_init(void);
-void debug_level_set(char *name, int level);
+void debug_level_set(const char *name, int level);
 int debug_level_get(const char *name);
 void debug_print(int level, const char *module, const char *function, const char *fmt, ...);
+/* end of prototypes */
