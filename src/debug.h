842386e55befa3faf19ca751dcb2cc3a8599ce05
horwitz <horwitz@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-07-13 07:34:47 +0000

    Fix some warnings
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@330 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 int debug_level;
 #define dbg(level,fmt...) ({ if (debug_level >= level) debug_printf(level,MODULE,__PRETTY_FUNCTION__,1,fmt); })
 
@@ -5,5 +6,6 @@ int debug_level;
 void debug_init(void);
 void debug_level_set(const char *name, int level);
 int debug_level_get(const char *name);
+void debug_vprintf(int level, const char *module, const char *function, int prefix, const char *fmt, va_list ap);
 void debug_printf(int level, const char *module, const char *function, int prefix, const char *fmt, ...);
 /* end of prototypes */
