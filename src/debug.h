61a93fb2eb3b36bcb65c13c5abc49d540be701e3
kazer_ <kazer_@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-07-16 12:20:34 +0000

    Added c++ compliance
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@343 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdarg.h>
 int debug_level;
 #define dbg(level,fmt...) ({ if (debug_level >= level) debug_printf(level,MODULE,__PRETTY_FUNCTION__,1,fmt); })
@@ -9,3 +13,7 @@ int debug_level_get(const char *name);
 void debug_vprintf(int level, const char *module, const char *function, int prefix, const char *fmt, va_list ap);
 void debug_printf(int level, const char *module, const char *function, int prefix, const char *fmt, ...);
 /* end of prototypes */
+
+#ifdef __cplusplus
+}
+#endif
