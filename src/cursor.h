76039ffaec1515513ec8e527745fca007b574611
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-07-21 08:35:06 +0000

    Improvements to bookmarks and cursor handling

--- src/cursor.h
+++ src/cursor.h
@@ -8,6 +8,7 @@ struct transformation;
 struct vehicle;
 struct coord *cursor_pos_get(struct cursor *this);
 void cursor_pos_set(struct cursor *this, struct coord *pos);
+void cursor_redraw(struct cursor *this);
 int cursor_get_dir(struct cursor *this);
 int cursor_get_speed(struct cursor *this);
 struct cursor *cursor_new(struct graphics *gra, struct vehicle *v, struct color *c, struct transformation *t);
