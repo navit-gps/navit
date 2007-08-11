a4d20cbbb98b252619b8384a83c51c430b19f673
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-08-11 10:30:00 +0000

    Made it compile again

--- trunk/navit/src/binding/python/binding_python.c
+++ trunk/navit/src/binding/python/binding_python.c
@@ -173,7 +173,7 @@ map_new_py(PyObject *self, PyObject *args)
 	if (!PyArg_ParseTuple(args, "ss:navit.map", &type, &filename))
 		return NULL;
 	ret=PyObject_NEW(mapObject, &map_Type);
-	ret->m=map_new(type,filename);
+	ret->m=map_new(type,filename,NULL);
 	return (PyObject *)ret;
 }
 
