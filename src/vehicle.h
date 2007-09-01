70fd60ad115510eed21e4b4fd9e80b77f40303d6
kazer_ <kazer_@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-09-01 16:21:27 +0000

    Made vehicle.h c++ compliant
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@402 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- src/vehicle.h
+++ src/vehicle.h
@@ -1,15 +1,20 @@
-/* prototypes */
+#ifdef __cplusplus
+extern "C" {
+#endif/* prototypes */
 enum projection;
 struct callback;
 struct coord;
 struct vehicle;
-enum projection vehicle_projection(struct vehicle *this);
-struct coord *vehicle_pos_get(struct vehicle *this);
-double *vehicle_speed_get(struct vehicle *this);
-double *vehicle_dir_get(struct vehicle *this);
-void vehicle_set_position(struct vehicle *this, struct coord *pos);
+enum projection vehicle_projection(struct vehicle *this_);
+struct coord *vehicle_pos_get(struct vehicle *this_);
+double *vehicle_speed_get(struct vehicle *this_);
+double *vehicle_dir_get(struct vehicle *this_);
+void vehicle_set_position(struct vehicle *this_, struct coord *pos);
 struct vehicle *vehicle_new(const char *url);
-void vehicle_callback_add(struct vehicle *this, struct callback *cb);
-void vehicle_callback_remove(struct vehicle *this, struct callback *cb);
-void vehicle_destroy(struct vehicle *this);
+void vehicle_callback_add(struct vehicle *this_, struct callback *cb);
+void vehicle_callback_remove(struct vehicle *this_, struct callback *cb);
+void vehicle_destroy(struct vehicle *this_);
 /* end of prototypes */
+#ifdef __cplusplus
+}
+#endif
