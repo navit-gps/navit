b49bf13e01342b6707ff64f5d09c3d6d8c4a98a1
kazer_ <kazer_@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-11-05 16:15:26 +0000

    Made country.h C++ compliant

--- trunk/navit/src/country.h
+++ trunk/navit/src/country.h
@@ -1,12 +1,20 @@
 #ifndef NAVIT_COUNTRY_H
 #define NAVIT_COUNTRY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* prototypes */
 struct attr;
 struct country_search;
 struct item;
 struct country_search *country_search_new(struct attr *search, int partial);
-struct item *country_search_get_item(struct country_search *this);
-void country_search_destroy(struct country_search *this);
+struct item *country_search_get_item(struct country_search *this_);
+void country_search_destroy(struct country_search *this_);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
