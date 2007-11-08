a3f2f5cce0286eafbe53abd75af1babad130ccd5
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-11-08 19:21:13 +0000

    Fixed a signess bug which occurs with osm maps > 32767 tiles

--- src/zipfile.h
+++ src/zipfile.h
@@ -6,10 +6,10 @@ struct zip_lfh {
 	short ziptime;
 	short zipdate;
 	int zipcrc;
-	int zipsize;
-	int zipuncmp;
-	short zipfnln;
-	short zipxtraln;
+	unsigned int zipsize;
+	unsigned int zipuncmp;
+	unsigned short zipfnln;
+	unsigned short zipxtraln;
 	char zipname[0];
 } __attribute__ ((packed));
 
@@ -24,26 +24,26 @@ struct zip_cd {
 	short ziptim;
 	short zipdat;
 	int zipccrc;
-	int zipcsiz;
-	int zipcunc;
-	short zipcfnl;
-	short zipcxtl;
-	short zipccml;
-	short zipdsk;
-	short zipint;
-	int zipext;
-	int zipofst;
+	unsigned int zipcsiz;
+	unsigned int zipcunc;
+	unsigned short zipcfnl;
+	unsigned short zipcxtl;
+	unsigned short zipccml;
+	unsigned short zipdsk;
+	unsigned short zipint;
+	unsigned int zipext;
+	unsigned int zipofst;
 	char zipcfn[0];	
 } __attribute__ ((packed));
 
 struct zip_eoc {
 	int zipesig;
-	short zipedsk;
-	short zipecen;
-	short zipenum;
-	short zipecenn;
-	int zipecsz;
-	int zipeofst;
+	unsigned short zipedsk;
+	unsigned short zipecen;
+	unsigned short zipenum;
+	unsigned short zipecenn;
+	unsigned int zipecsz;
+	unsigned int zipeofst;
 	short zipecoml;
 	char zipecom[0];
 } __attribute__ ((packed));
