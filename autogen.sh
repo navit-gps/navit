e1f569954d947a12ed2e9cd4aa081208905f1fb9
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2007-12-04 22:08:45 +0000

    Added checks for pkg-config and libtool to avoid failed autogens

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+if ! pkg-config --version >/dev/null
+then
+	echo "You need to install pkg-config"
+	exit 1
+fi
+
+if ! libtool --version >/dev/null
+then
+	echo "You need to install libtool"
+	exit 1
+fi
+
 autoreconf --force --install -I m4
