e496daa06c5766f89303e70dfcd95a55941fc8eb
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-01-13 11:29:06 +0000

    Fix:Core:Improved popup handling
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@760 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- src/menu.h
+++ src/menu.h
@@ -15,6 +15,7 @@ struct menu_methods {
 	struct menu_priv *(*add)(struct menu_priv *menu, struct menu_methods *meth, char *name, enum menu_type type, struct callback *cb);
 	void (*set_toggle)(struct menu_priv *menu, int active);
 	int (*get_toggle)(struct menu_priv *menu);
+	void (*popup)(struct menu_priv *menu);
 };
 
 struct menu {
@@ -28,5 +29,6 @@ void menu_route_update(struct container *co);
 struct menu *menu_add(struct menu *menu, char *name, enum menu_type type, struct callback *cb);
 void menu_set_toggle(struct menu *menu, int active);
 int menu_get_toggle(struct menu *menu);
+void menu_popup(struct menu *menu);
 /* end of prototypes */
 #endif
