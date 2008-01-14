f1eb9ef2278f5e44c957c88ce32bedfe80709b8d
zaxl <zaxl@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-01-14 23:21:16 +0000

    Fix: avoid unecessery strlen/strcats for debug, pass lenghts as arguments so they can be calculated at compile time, module name and __PRITY_FUNCTION__ are constant strings
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@765 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- src/debug.h
+++ src/debug.h
@@ -10,14 +10,14 @@ extern int debug_level;
 #define dbg_str2(x) #x
 #define dbg_str1(x) dbg_str2(x)
 #define dbg_module dbg_str1(MODULE)
-#define dbg(level,fmt...) ({ if (debug_level >= level) debug_printf(level,dbg_module,__PRETTY_FUNCTION__,1,fmt); })
+#define dbg(level,fmt...) ({ if (debug_level >= level) debug_printf(level,dbg_module,strlen(dbg_module),__PRETTY_FUNCTION__, strlen(__PRETTY_FUNCTION__),1,fmt); })
 
 /* prototypes */
 void debug_init(void);
 void debug_level_set(const char *name, int level);
 int debug_level_get(const char *name);
-void debug_vprintf(int level, const char *module, const char *function, int prefix, const char *fmt, va_list ap);
-void debug_printf(int level, const char *module, const char *function, int prefix, const char *fmt, ...);
+void debug_vprintf(int level, const char *module, const int mlen, const char *function, const int flen, int prefix, const char *fmt, va_list ap);
+void debug_printf(int level, const char *module, const int mlen, const char *function, const int flen, int prefix, const char *fmt, ...);
 /* end of prototypes */
 
 #ifdef __cplusplus
