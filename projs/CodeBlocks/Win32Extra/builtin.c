1c653abcf563d6ce2e55b802be9b65888e40ea91
afaber <afaber@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-01-15 19:36:12 +0000

    Add:projs/CodeBlocks:support for data/textfile

--- projs/CodeBlocks/Win32Extra/builtin.c
+++ projs/CodeBlocks/Win32Extra/builtin.c
@@ -1,3 +1,4 @@
+extern void module_data_textfile_init(void);
 extern void module_data_binfile_init(void);
 extern void module_data_mg_init(void);
 extern void module_data_garmin_init(void);
@@ -9,6 +10,7 @@ extern void module_vehicle_file_init(void);
 extern void module_speech_speech_dispatcher_init(void);
 extern void builtin_init(void);
 void builtin_init(void) {
+	module_data_textfile_init();
 	module_data_binfile_init();
 	module_data_mg_init();
     module_data_garmin_init();
