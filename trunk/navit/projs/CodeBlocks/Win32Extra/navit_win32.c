403704ee645e91f44e2fabd8c0096a697ab48df6
afaber <afaber@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-02-04 00:46:04 +0000

    Fix:projs/CodeBlocks:update project files, more work on native win32 gui/graphics

---
+++ trunk/navit/projs/CodeBlocks/Win32Extra/navit_win32.c
@@ -0,0 +1,17 @@
+
+extern void module_vehicle_demo_init(void);
+extern void module_vehicle_file_init(void);
+extern void module_win32_gui_init(void);
+extern void module_speech_speech_dispatcher_init(void);
+extern void builtin_init(void);
+void builtin_init(void) {
+	module_data_textfile_init();
+	module_data_binfile_init();
+	module_data_mg_init();
+	module_win32_gui_init();
+    module_data_garmin_init();
+	module_data_poi_geodownload_init();
+	module_vehicle_demo_init();
+	module_vehicle_file_init();
+	module_speech_speech_dispatcher_init();
+}
