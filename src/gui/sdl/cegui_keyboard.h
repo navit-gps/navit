5a70db19c127ba153fbf8d817abd2179706dbeab
horwitz <horwitz@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-02-19 13:06:19 +0000

    SDL:Fix:Fix no new line ... warning

--- src/gui/sdl/cegui_keyboard.h
+++ src/gui/sdl/cegui_keyboard.h
@@ -2,3 +2,4 @@ bool ShowKeyboard(const CEGUI::EventArgs& event);
 void Add_KeyBoard_key(CEGUI::String key,int x,int y,int w);
 void BuildKeyboard();
 bool Handle_Virtual_Key_Down(const CEGUI::EventArgs& event);
+
