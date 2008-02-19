0bb9f5a2ede4d6a18fbd2982ce852581c8707eb1
horwitz <horwitz@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-02-19 13:04:14 +0000

    core:Add:Merge callback lists into one callback list.

--- src/callback.h
+++ src/callback.h
@@ -1,6 +1,9 @@
 #ifndef NAVIT_CALLBACK_H
 #define NAVIT_CALLBACK_H
 
+#include "item.h"
+#include "attr.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -22,9 +25,21 @@ void callback_list_call(struct callback_list *l, int pcount, void **p);
 void callback_list_destroy(struct callback_list *l);
 /* end of prototypes */
 
+static inline struct callback *callback_new_attr_0(void (*func)(void), enum attr_type type)
+{
+	return callback_new_attr(func, type, 0, NULL);
+}
+
 static inline struct callback *callback_new_0(void (*func)(void))
 {
 	return callback_new(func, 0, NULL);
+}
+
+static inline struct callback *callback_new_attr_1(void (*func)(void), enum attr_type type, void *p1)
+{
+	void *p[1];
+	p[0]=p1;
+	return callback_new_attr(func, type, 1, p);
 }
 
 static inline struct callback *callback_new_1(void (*func)(void), void *p1)
@@ -68,12 +83,26 @@ static inline void callback_list_call_0(struct callback_list *l)
 	callback_list_call(l, 0, NULL);
 }
 
+static inline void callback_list_call_attr_1(struct callback_list *l, enum attr_type type, void *p1)
+{
+	void *p[1];
+	p[0]=p1;
+	callback_list_call_attr(l, type, 1, p);
+}
 
 static inline void callback_list_call_1(struct callback_list *l, void *p1)
 {
 	void *p[1];
 	p[0]=p1;
 	callback_list_call(l, 1, p);
+}
+
+static inline void callback_list_call_attr_2(struct callback_list *l, enum attr_type type, void *p1, void *p2)
+{
+	void *p[2];
+	p[0]=p1;
+	p[1]=p2;
+	callback_list_call_attr(l, type, 2, p);
 }
 
 static inline void callback_list_call_2(struct callback_list *l, void *p1, void *p2)
