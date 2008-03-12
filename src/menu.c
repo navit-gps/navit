c089d600fa3d9d38b0b24c17b6cc59559a123a99
horwitz <horwitz@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-03-12 10:58:26 +0000

    Core:Cleanup:Remove dead code from menu

--- src/menu.c
+++ src/menu.c
@@ -2,30 +2,6 @@
 #include "menu.h"
 #include "debug.h"
 
-void
-menu_route_do_update(struct container *co)
-{
-#if 0
-	if (co->cursor) {
-#if 0 /* FIXME */
-		route_set_position(co->route, cursor_pos_get(co->cursor));
-#endif
-		graphics_redraw(co);
-		if (co->statusbar && co->statusbar->statusbar_route_update)
-			co->statusbar->statusbar_route_update(co->statusbar, co->route);
-	}
-#endif
-}
-
-void
-menu_route_update(struct container *co)
-{
-#if 0
-	menu_route_do_update(co);
-	graphics_redraw(co);
-#endif
-}
-
 struct menu *
 menu_add(struct menu *menu, char *name, enum menu_type type, struct callback *cb)
 {
