0df900d9e4dd0a2f596b79a8656acc7ee7afab23
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-04-14 14:41:17 +0000

    Add:Core:Initial coding for map datum conversion

--- trunk/navit/src/projection.h
+++ trunk/navit/src/projection.h
@@ -5,6 +5,10 @@ enum projection {
 	projection_none, projection_mg, projection_garmin
 };
 
+enum map_datum {
+	map_datum_none, map_datum_wgs84, map_datum_dhdn
+};
+
 enum projection projection_from_name(const char *name);
 char * projection_to_name(enum projection proj);
 
