b3f04209a9bf9857c3ebce94d027a78e7c34a8f9
horwitz <horwitz@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-06-05 21:56:14 +0000

    Add license files and headers

--- trunk/navit/navit/speech/speech_dispatcher/speech_speech_dispatcher.c
+++ trunk/navit/navit/speech/speech_dispatcher/speech_speech_dispatcher.c
@@ -1,7 +1,21 @@
-
-/* speechd simple client program
- * CVS revision: $Id: speech_speech_dispatcher.c,v 1.3 2007-07-08 20:41:35 martin-s Exp $
- * Author: Tomas Cerha <cerha@brailcom.cz> */
+/**
+ * Navit, a modular navigation system.
+ * Copyright (C) 2005-2008 Navit Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 
 #include <sys/types.h>
 #include <sys/socket.h>
