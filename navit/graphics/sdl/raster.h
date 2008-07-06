06a8d81e1b876ff0062fd66c0e2631cf0c913781
rphlx <rphlx@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-07-06 08:35:34 +0000

    Add:graphics_sdl:Custom rastering code with anti-aliasing support, based on SDL_gfx and SGE. Remove external SDL_gfx and Imlib2 dependencies.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@1197 ffa7fe5e-494d-0410-b361-a75ebd5db220

---
+++ navit/graphics/sdl/raster.h
@@ -0,0 +1,25 @@
+/* raster.h -- line/rect/circle/poly rasterization
+
+   copyright (c) 2008 bryan rittmeyer <bryanr@bryanr.org>
+
+   license: LGPLv2
+*/
+
+#ifndef __RASTER_H
+#define __RASTER_H
+
+#include <stdint.h>
+#include <SDL/SDL.h>
+
+void raster_rect(SDL_Surface *s, int16_t x, int16_t y, int16_t w, int16_t h, uint32_t col);
+
+void raster_line(SDL_Surface *s, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col);
+void raster_circle(SDL_Surface *s, int16_t x, int16_t y, int16_t r, uint32_t col);
+void raster_polygon(SDL_Surface *s, int16_t n, int16_t *vx, int16_t *vy, uint32_t col);
+
+void raster_aaline(SDL_Surface *s, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col);
+void raster_aacircle(SDL_Surface *s, int16_t x, int16_t y, int16_t r, uint32_t col);
+void raster_aapolygon(SDL_Surface *s, int16_t n, int16_t *vx, int16_t *vy, uint32_t col);
+
+#endif /* __RASTER_H */
+
