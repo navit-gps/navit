2779675885f7a3de4d4312dde143fd61fdc7d191
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-10-12 20:39:00 +0000

    Add:Core:First try to add a general purpose cache to navit
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@1453 ffa7fe5e-494d-0410-b361-a75ebd5db220

---
+++ navit/cache.h
@@ -0,0 +1,12 @@
+struct cache_entry;
+struct cache;
+/* prototypes */
+struct cache *cache_new(int id_size, int size);
+void *cache_entry_new(struct cache *cache, void *id, int size);
+void cache_entry_destroy(struct cache *cache, void *data);
+void *cache_lookup(struct cache *cache, void *id);
+void cache_insert(struct cache *cache, void *data);
+void *cache_insert_new(struct cache *cache, void *id, int size);
+void *cache_lookup_or_insert(struct cache *cache, void *id, int size);
+void cache_dump(struct cache *cache);
+/* end of prototypes */
