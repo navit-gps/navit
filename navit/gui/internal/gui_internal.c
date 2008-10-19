9ee3b65d87ffc21c009a4cd9565cad40eb6d2999
steven_s <steven_s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-10-19 15:18:45 +0000

    Fix:Core:Adding configure check to define HAVE_BYTESSWAP_H, ticket #239
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@1509 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/gui/internal/gui_internal.c
+++ navit/gui/internal/gui_internal.c
@@ -71,6 +71,8 @@ enum widget_type {
 	widget_button,
 	widget_label,
 	widget_image,
+	widget_table,
+	widget_table_row
 };
 
 enum flags {
@@ -109,6 +111,11 @@ struct widget {
 	struct graphics_gc *foreground_frame;
 	char *text;
 	struct graphics_image *img;
+	 /**
+	  * A function to be invoked on actions.
+	  * @li widget The widget that is receiving the button press.
+	  *
+	  */
 	void (*func)(struct gui_priv *priv, struct widget *widget);
 	int reason;
 	int datai;
@@ -136,7 +143,7 @@ struct widget {
 	int deflt;
 	int is_on;
 	int redraw;
-	GList *children;
+	GList *children;  
 };
 
 /**
@@ -232,12 +239,93 @@ struct gui_priv {
         struct gui_config_settings config;
 };
 
+
+/**
+ * @brief A structure to store information about a table.
+ *
+ * The table_data widget stores pointers to extra information needed by the
+ * table widget.
+ *
+ * The table_data structure needs to be freed with data_free along with the widget.
+ * 
+ */
+struct table_data
+{
+  /**
+   * A GList pointer into a widget->children list that indicates the row
+   * currently being rendered at the top of the table.
+   */
+  GList * top_row;
+  /**
+   * A Glist pointer into a widget->children list that indicates the row
+   * currently being rendered at the bottom of the table.
+   */
+  GList * bottom_row;
+
+  /**
+   * A list of table_row widgets that mark the
+   * top rows for each page of the table.
+   * This is needed for the 'previous page' function of the table.
+   */
+  GList * page_headers;
+
+  /**
+   * A container box that is the child of the table widget that contains+groups
+   * the next and previous button.
+   */
+  struct widget * button_box;
+
+  /**
+   * A button widget to handle 'next page' requests
+   */
+  struct widget * next_button;
+  /**
+   * A button widget to handle 'previous page' requests.
+   */
+  struct widget * prev_button;
+    
+
+  /**
+   * a pointer to the gui context.
+   * This is needed by the free function to destory the buttons.
+   */
+  struct  gui_priv *  this;
+};
+
+/**
+ * A data structure that holds information about a column that makes up a table.
+ *
+ *
+ */
+struct table_column_desc
+{
+
+  /**
+   * The computed height of a cell in the table.
+   */
+  int height;
+
+  /**
+   * The computed width of a cell in the table.
+   */
+  int width;
+};
+
 static void gui_internal_widget_render(struct gui_priv *this, struct widget *w);
 static void gui_internal_widget_pack(struct gui_priv *this, struct widget *w);
 static struct widget * gui_internal_box_new(struct gui_priv *this, enum flags flags);
 static void gui_internal_widget_append(struct widget *parent, struct widget *child);
 static void gui_internal_widget_destroy(struct gui_priv *this, struct widget *w);
 static void gui_internal_apply_config(struct gui_priv *this);
+
+static struct widget* gui_internal_widget_table_new(struct gui_priv * this, enum flags flags);
+static void gui_internal_widget_table_add_row(struct widget * table,  struct widget * row);
+static void gui_internal_table_render(struct gui_priv * this, struct widget * w);
+static void gui_internal_cmd_route(struct gui_priv * this, struct widget * w);
+static void gui_internal_table_pack(struct gui_priv * this, struct widget * w);
+static void gui_internal_table_button_next(struct gui_priv * this, struct widget * wm);
+static void gui_internal_table_button_prev(struct gui_priv * this, struct widget * wm);
+static void gui_internal_table_data_free(void * d);
 
 static struct graphics_image *
 image_new_scaled(struct gui_priv *this, char *name, int w, int h)
@@ -581,11 +669,13 @@ static void gui_internal_clear(struct gui_priv *this)
 static struct widget *
 gui_internal_find_widget(struct widget *wi, struct point *p, int flags)
 {
-	GList *l=wi->children;
 	struct widget *ret,*child;
+	GList *l=wi->children;
 
-	if (p && (wi->p.x > p->x || wi->p.y > p->y || wi->p.x + wi->w < p->x || wi->p.y + wi->h < p->y)) 
+
+	if (p && (wi->p.x > p->x || wi->p.y > p->y || wi->p.x + wi->w < p->x || wi->p.y + wi->h < p->y)) {
 		return NULL;
+	}
 	if (wi->state & flags) 
 		return wi;
 	while (l) {
@@ -676,7 +766,7 @@ static void gui_internal_box_render(struct gui_priv *this, struct widget *w)
 
 	l=w->children;
 	while (l) {
-		wc=l->data;
+		wc=l->data;	
 		gui_internal_widget_render(this, wc);
 		l=g_list_next(l);
 	}
@@ -929,6 +1019,9 @@ gui_internal_widget_render(struct gui_priv *this, struct widget *w)
 	case widget_image:
 		gui_internal_image_render(this, w);
 		break;
+	case widget_table:
+		gui_internal_table_render(this,w);
+		break;
 	default:
 		break;
 	}
@@ -941,6 +1034,8 @@ gui_internal_widget_pack(struct gui_priv *this, struct widget *w)
 	case widget_box:
 		gui_internal_box_pack(this, w);
 		break;
+	case widget_table:
+	  gui_internal_table_pack(this,w);
 	default:
 		break;
 	}
@@ -2433,6 +2528,14 @@ static void gui_internal_menu_root(struct gui_priv *this)
 			gui_internal_cmd_settings, NULL));
 	gui_internal_widget_append(w, gui_internal_button_new(this, "Tools",
 			image_new_l(this, "gui_tools"), gravity_center|orientation_vertical));
+
+#if 0
+	gui_internal_widget_append(w, gui_internal_button_new_with_callback(this, "Table Test",
+			image_new_l(this, "gui_settings"), gravity_center|orientation_vertical,
+			gui_internal_cmd_route, NULL));
+#endif
+
+							      
 	gui_internal_menu_render(this);
 	graphics_draw_mode(this->gra, draw_mode_end);
 }
@@ -2840,3 +2943,483 @@ void plugin_init(void)
 {
 	plugin_register_gui_type("internal", gui_internal_new);
 }
+
+/**
+ * @brief Creates a new table widget.
+ * 
+ * Creates and returns a new table widget.  This function will
+ * setup next/previous buttons as children.
+ * 
+ * @param this The graphics context.
+ * @param flags widget sizing flags.
+ * @returns The newly created widget
+ */
+struct widget * gui_internal_widget_table_new(struct gui_priv * this, enum flags flags)
+{
+	struct widget * widget = g_new0(struct widget,1);
+	struct table_data * data = NULL;
+	widget->type=widget_table;
+	widget->flags=flags;
+	widget->data = g_new0(struct table_data,1);
+	widget->data_free=g_free;
+	data = (struct table_data*)widget->data;
+	
+
+	data->next_button = gui_internal_button_new_with_callback
+		(this,"Next",image_new_xs(this, "gui_active") ,
+		 gravity_left_center  |orientation_vertical,
+		 gui_internal_table_button_next,NULL);
+	data->next_button->data=widget;
+	
+  
+	data->prev_button =  gui_internal_button_new_with_callback
+		(this,"Prev",
+		 image_new_xs(this, "gui_active")
+		 ,gravity_right_center |orientation_vertical,
+		 gui_internal_table_button_prev,NULL);
+	
+	data->prev_button->data=widget;
+	
+	data->this=this;
+	
+	data->button_box=gui_internal_box_new(this,
+					      gravity_center|orientation_horizontal);
+	data->button_box->children=g_list_append(data->button_box->children,
+						 data->next_button);
+	data->button_box->children=g_list_append(data->button_box->children,
+						 data->prev_button);
+	data->button_box->background=this->background2;
+	data->button_box->bl=this->spacing;
+	widget->children=g_list_append(widget->children,data->button_box);
+	gui_internal_widget_pack(this,data->button_box);
+	
+	return widget;
+
+}
+
+/**
+ *
+ * @brief Adds a row to a table_widget.
+ *
+ * @param table The table to add the row to.
+ * @param row A table_row widget that will be added to the table.
+ */
+void gui_internal_widget_table_add_row(struct widget * table,  struct widget *  row)
+{
+  table->children=g_list_append(table->children,row);
+  
+}
+
+/**
+ * Creates a new table_row widget.
+ * @param this The graphics context
+ * @param flags Sizing flags for the row
+ * @returns The new table_row widget.
+ */
+struct widget * gui_internal_widget_table_row_new(struct gui_priv * this, enum flags flags)
+{
+	struct widget * widget = g_new0(struct widget,1);
+	widget->type=widget_table_row;
+	widget->flags=flags;
+	return widget;
+}
+
+
+
+/**
+ * @brief Computes the column dimensions for the table.
+ *
+ * @param w The table widget to compute dimensions for.
+ *
+ * This function examines all of the rows and columns for the table w
+ * and returns a list (GList) of table_column_desc elements that
+ * describe each column of the table.
+ *
+ * The caller is responsible for freeing the returned list.
+ */
+static GList * gui_internal_compute_table_dimensions(struct widget * w)
+{
+  
+	GList * column_desc = NULL;
+	GList * current_desc=NULL;
+	GList * cur_row = w->children;
+	struct widget * cur_row_widget=NULL;
+	GList * cur_column=NULL;
+	struct widget * cell_w=NULL;
+	struct table_column_desc * current_cell=NULL;
+	int height=0;
+	int width=0;
+
+  
+	/**
+	 * Scroll through the the table and
+	 * 1. Compute the maximum width + height of each column across all rows.
+	 */
+	
+	for(cur_row=w->children;  cur_row ; cur_row = g_list_next(cur_row) )
+	{
+		cur_row_widget = (struct widget*) cur_row->data;
+		current_desc = column_desc;
+		for(cur_column = cur_row_widget->children; cur_column; 
+		    cur_column=g_list_next(cur_column))
+		{
+			cell_w = (struct widget*) cur_column->data;
+			if(current_desc == 0) 
+			{
+				current_cell = g_new0(struct table_column_desc,1);	
+				column_desc = g_list_append(column_desc,current_cell);
+				current_desc = g_list_last(column_desc);
+				current_cell->height=cell_w->h; 
+				current_cell->width=cell_w->w; 
+				
+			}
+			else
+			{
+				current_cell = current_desc->data;
+				height = cell_w->h;
+				width = cell_w->w;
+				if(current_cell->height < height )
+				{
+					current_cell->height = height;
+				}
+				if(current_cell->width < width)
+				{
+					current_cell->width = width;
+				}
+				current_desc = g_list_next(current_desc);
+			}
+			
+		}/* column loop */
+		
+	} /*row loop */
+	return column_desc;
+}
+
+
+/**
+ * @brief Computes the height and width for the table.
+ *
+ * The height and widht are computed to display all cells in the table
+ * at the requested height/width.
+ *
+ * @param this The graphics context
+ * @param w The widget to pack.
+ *
+ */
+void gui_internal_table_pack(struct gui_priv * this, struct widget * w)
+{
+  
+	struct widget * row_widget=NULL;
+	int height=0;
+	int width=0;
+	int count=0;
+	GList * column_data = gui_internal_compute_table_dimensions(w);
+	GList * current=0;
+	struct table_column_desc * cell_desc=0;
+	struct table_data * table_data = (struct table_data*)w->data;
+	
+	for(current = column_data; current; current=g_list_next(current))
+	{
+		if(table_data->button_box == current->data )
+		{
+			continue;
+		}
+		cell_desc = (struct table_column_desc *) current->data;
+		width = width + cell_desc->width;
+		if(height < cell_desc->height) 
+		{
+			height = cell_desc->height;
+		}
+	}
+
+	for(current=w->children; current; current=g_list_next(current))
+	{
+		if(current->data!= table_data->button_box)
+		{
+			count++;
+		}
+	}
+	gui_internal_widget_pack(this,table_data->button_box);  
+	w->h = count * height + table_data->button_box;
+	
+	if(w->h + w->c.y   > this->root.h   )
+	{
+		/**
+		 * Do not allow the widget to exceed the screen.
+		 * 
+		 */
+		w->h = this->root.h- w->c.y  - height;
+	}
+	w->w = width;
+	
+	
+	/**
+	 * Deallocate column descriptions.
+	 */
+	current = column_data;
+	while( (current = g_list_last(current)) )
+	{
+		current = g_list_remove(current,current->data);
+	}
+	
+}
+
+
+
+/**
+ * @brief Renders a table widget.
+ * 
+ * @param this The graphics context
+ * @param w The table widget to render.
+ */
+void gui_internal_table_render(struct gui_priv * this, struct widget * w)
+{
+
+	int x;
+	int y;
+	GList * column_desc=NULL;
+	GList * cur_row = NULL;
+	GList * current_desc=NULL;
+	struct table_data * table_data = (struct table_data*)w->data;
+	int idx=0;
+	int is_skipped=0;
+	struct table_column_desc * dim=NULL;
+	
+	column_desc = gui_internal_compute_table_dimensions(w);
+	
+	y=w->p.y;
+	
+	/**
+	 * Skip rows that are on previous pages.
+	 */
+	cur_row = w->children;
+	if(table_data->top_row )
+	{
+		cur_row = table_data->top_row;
+	}
+	
+	
+	/**
+	 * Loop through each row.  Drawing each cell with the proper sizes, 
+	 * at the proper positions.
+	 */
+	for(table_data->top_row=cur_row; cur_row; cur_row = g_list_next(cur_row))
+	{
+		GList * cur_column=NULL;
+		current_desc = column_desc;
+		struct widget * cur_row_widget = (struct widget*)cur_row->data;
+		int max_height=0;
+		x =w->p.x;        
+		if(cur_row_widget == table_data->button_box )
+		{
+			continue;
+		}
+		dim = (struct table_column_desc*)current_desc->data;
+		
+		if( y + dim->height + table_data->button_box->h  >= w->p.y + w->h )
+		{
+			/*
+			 * No more drawing space left.
+			 */
+			is_skipped=1;
+			break;
+			
+		}      
+		for(cur_column = cur_row_widget->children; cur_column; 
+		    cur_column=g_list_next(cur_column))
+		{
+			struct  widget * cur_widget = (struct widget*) cur_column->data;
+			dim = (struct table_column_desc*)current_desc->data;
+			
+			cur_widget->p.x=x;
+			cur_widget->w=dim->width;
+			cur_widget->p.y=y;
+			cur_widget->h=dim->height;
+			x=x+dim->width;
+			max_height = dim->height;
+			gui_internal_widget_render(this,cur_widget);
+      
+			if(dim->height > max_height)
+			{
+				max_height = dim->height;
+			}
+		}
+		y = y + max_height;
+		table_data->bottom_row=cur_row;
+		current_desc = g_list_next(current_desc);
+	}
+	if(table_data  && table_data->button_box )
+	{
+		table_data->button_box->p.y =w->c.y+w->h-table_data->button_box->h;
+		if(table_data->button_box->p.y < y )
+		{
+			table_data->button_box->p.y=y;
+		}	
+		table_data->button_box->p.x = w->p.x;
+		table_data->button_box->w = w->w;
+		//    table_data->button_box->h = w->h - y;
+		//    table_data->next_button->h=table_data->button_box->h;    
+		//    table_data->prev_button->h=table_data->button_box->h;
+		//    table_data->next_button->c.y=table_data->button_box->c.y;    
+		//    table_data->prev_button->c.y=table_data->button_box->c.y;    
+
+		gui_internal_widget_pack(this,table_data->button_box);
+		if(table_data->next_button->p.y > w->p.y + w->h + table_data->next_button->h)
+		{
+		
+			table_data->button_box->p.y = w->p.y + w->h - 
+				table_data->button_box->h;
+		}
+		if(is_skipped) 
+	        {
+			table_data->next_button->state|= STATE_SENSITIVE;
+		}
+		else
+		{
+			table_data->next_button->state&= ~STATE_SENSITIVE;
+		}
+		
+		if(table_data->top_row != w->children)
+		{
+			table_data->prev_button->state|= STATE_SENSITIVE;
+		}
+		else
+		{
+			table_data->prev_button->state&= ~STATE_SENSITIVE;
+		}
+		gui_internal_widget_render(this,table_data->button_box);
+		
+
+	}
+	
+	/**
+	 * Deallocate column descriptions.
+	 */
+	current_desc = column_desc;
+	while( (current_desc = g_list_last(current_desc)) )
+	{
+		current_desc = g_list_remove(current_desc,current_desc->data);
+	}
+}
+
+
+/**
+ * TEST function for demonstrating table.
+ */
+void gui_internal_cmd_route(struct gui_priv * this, struct widget * wm)
+{
+#if 0 
+	struct widget * menu;
+	struct widget * w;
+	int idx;
+	char buffer[10];
+	
+	w = gui_internal_widget_table_new(this,0);
+	w->c = wm->c;
+	for(idx=0; idx < 50; idx++)
+	{
+		struct widget * row = gui_internal_widget_table_row_new
+			(this,gravity_left_top);
+		sprintf(buffer, "Test %d",idx);
+		row->children = g_list_append(row->children,
+					      gui_internal_label_new(this,buffer));
+		row->children= g_list_append(row->children,
+					     gui_internal_label_new(this,"column 2 4 6 8"));
+		gui_internal_widget_table_add_row(w,row);
+	}
+	menu=gui_internal_menu(this,"Route");
+	gui_internal_widget_append(menu,w);
+	gui_internal_menu_render(this);
+#endif
+}
+
+
+
+/**
+ * @brief handles the 'next page' table event.
+ * A callback function that is invoked when the 'next page' button is pressed
+ * to advance the contents of a table widget.
+ *
+ * @param this The graphics context.
+ * @param wm The button widget that was pressed.
+ */
+static void gui_internal_table_button_next(struct gui_priv * this, struct widget * wm)
+{
+	struct widget * table_widget = (struct widget * ) wm->data;
+	struct table_data * table_data = NULL;
+	int found=0;
+	GList * iterator;
+	
+	if(table_widget)
+	{
+		table_data = (struct table_data*) table_widget->data;
+		
+	}
+	if(table_data)
+	{
+		/**
+		 * Before advancing to the next page we need to ensure
+		 * that the current top_row is in the list of previous top_rows 
+		 * so previous page can work.
+		 *
+		 */
+		for(iterator=table_data->page_headers; iterator != NULL;
+		    iterator = g_list_next(iterator) )
+		{
+			if(iterator->data == table_data->top_row)
+			{
+				found=1;
+				break;
+			}
+			
+		}
+		if( ! found)
+		{
+			table_data->page_headers=g_list_append(table_data->page_headers,
+							       table_data->top_row);
+		}
+		
+		table_data->top_row = g_list_next(table_data->bottom_row);
+	}
+	wm->state&= ~STATE_HIGHLIGHTED;
+	gui_internal_menu_render(this);
+}
+
+
+
+/**
+ * @brief handles the 'previous page' table event.
+ * A callback function that is invoked when the 'previous page' button is pressed
+ * to go back in the contents of a table widget.
+ *
+ * @param this The graphics context.
+ * @param wm The button widget that was pressed.
+ */
+static void gui_internal_table_button_prev(struct gui_priv * this, struct widget * wm)
+{
+	struct widget * table_widget = (struct widget * ) wm->data;
+	struct table_data * table_data = NULL;
+	GList * current_page_top=NULL;
+
+	GList * iterator;
+	if(table_widget)
+	{
+		table_data = (struct table_data*) table_widget->data;
+		current_page_top = table_data->top_row;
+		if(table_data) 
+		{
+			for(iterator = table_data->page_headers; iterator != NULL;
+			    iterator = g_list_next(iterator))
+			{
+				if(current_page_top == iterator->data)
+				{
+					break;
+				}
+				table_data->top_row = (GList*) iterator->data;
+			}
+		}
+	}
+	wm->state&= ~STATE_HIGHLIGHTED;
+	gui_internal_menu_render(this);
+}
+
