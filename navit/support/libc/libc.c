c593bf2b26f92d02b65109a32fbe04380e024066
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2008-10-29 11:50:13 +0000

    Add:Core:Added libc stubs for devices which don't have a libc

---
+++ navit/support/libc/libc.c
@@ -0,0 +1,82 @@
+#include "locale.h"
+int errno;
+
+char *
+getenv(char *name)
+{
+	return 0;
+}
+
+void
+setenv(void)
+{
+}
+
+char *
+getcwd(void)
+{
+	return "dummy";
+}
+
+char *
+getwd(void)
+{
+	return "dummy";
+}
+
+char *strtok_r(char *str, const char *delim, char **saveptr)
+{
+	return strtok(str, delim);
+}
+
+void
+perror(char *x)
+{
+}
+
+void
+raise(void)
+{
+}
+
+void *
+popen(void)
+{
+	return 0;
+}
+
+void
+pclose(void)
+{
+}
+
+void
+rewind(void)
+{
+}
+
+int
+GetThreadLocale(void)
+{
+	return 0;
+}
+
+int
+signal(void)
+{
+	return 0;
+}
+
+void
+setlocale(void)
+{
+	return 0;
+}
+
+static struct lconv localedata={"."};
+
+struct lconv *
+localeconv(void)
+{
+	return &localedata;
+}
