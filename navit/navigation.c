91990676f2cca17f7f2d6e59b3c5f429a60d5ffd
tinloaf <tinloaf@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-01-06 12:00:13 +0000

    Fix:Core:Improved translator's documentation

--- navit/navigation.c
+++ navit/navigation.c
@@ -1346,6 +1346,7 @@ show_maneuver(struct navigation *nav, struct navigation_itm *itm, struct navigat
 			return g_strdup(_("Enter the roundabout soon"));
 		case 1:
 			d = get_distance(distance, type, 1);
+			/* TRANSLATORS: %s is the distance to the roundabout */
 			ret = g_strdup_printf(_("In %s, enter the roundabout"), d);
 			g_free(d);
 			return ret;
