be32ba1f47589dfee91892b786aa16aff44224ff
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-02-03 13:31:32 +0000

    Fix:Core:Made assignments actually work

--- navit/command.c
+++ navit/command.c
@@ -562,6 +562,7 @@ eval_assignment(struct context *ctx, struct result *res)
 	if (!get_op(ctx,0,"=",NULL)) return;
     	eval_conditional(ctx, &tmp);
 	if (ctx->error) return;
+	resolve_object(ctx, res);
 	command_set_attr(ctx, res, &tmp);
 }
 
