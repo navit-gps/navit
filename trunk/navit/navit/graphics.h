07918956e786f2efcb9778b23d6c43f0ec23a563
tinloaf <tinloaf@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-03-17 21:19:10 +0000

    Add:Core:Adding the possibility to resize overlays

--- trunk/navit/navit/graphics.h
+++ trunk/navit/navit/graphics.h
@@ -66,6 +66,7 @@ struct graphics_methods {
 	void (*image_free)(struct graphics_priv *gr, struct graphics_image_priv *priv);
 	void (*get_text_bbox)(struct graphics_priv *gr, struct graphics_font_priv *font, char *text, int dx, int dy, struct point *ret, int estimate);
 	void (*overlay_disable)(struct graphics_priv *gr, int disable);
+	void (*overlay_resize)(struct graphics_priv *gr, struct point *p, int w, int h, int alpha, int wraparound);
 };
 
 
@@ -152,6 +153,7 @@ void graphics_draw_rectangle_rounded(struct graphics *this_, struct graphics_gc 
 void graphics_draw_text(struct graphics *this_, struct graphics_gc *gc1, struct graphics_gc *gc2, struct graphics_font *font, char *text, struct point *p, int dx, int dy);
 void graphics_get_text_bbox(struct graphics *this_, struct graphics_font *font, char *text, int dx, int dy, struct point *ret, int estimate);
 void graphics_overlay_disable(struct graphics *this_, int disable);
+void graphics_overlay_resize(struct graphics *this_, struct point *p, int w, int h, int alpha, int wraparound);
 void graphics_draw_image(struct graphics *this_, struct graphics_gc *gc, struct point *p, struct graphics_image *img);
 int graphics_draw_drag(struct graphics *this_, struct point *p);
 void graphics_background_gc(struct graphics *this_, struct graphics_gc *gc);
