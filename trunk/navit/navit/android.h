3eb642806930a8c43420e0db7794d30b96723091
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-08-11 19:58:23 +0000

    Add:Core:More android stuff

--- trunk/navit/navit/android.h
+++ trunk/navit/navit/android.h
@@ -1,3 +1,6 @@
 #include <jni.h>
 extern JNIEnv *jnienv;
 extern jobject *android_activity;
+int android_find_class_global(char *name, jclass *ret);
+int android_find_method(jclass class, char *name, char *args, jmethodID *ret);
+int android_find_static_method(jclass class, char *name, char *args, jmethodID *ret);
