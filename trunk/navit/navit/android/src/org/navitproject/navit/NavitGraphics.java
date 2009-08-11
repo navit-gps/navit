5c0cbf584927cb213bf1d053fafac9ee5779eeb1
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-08-11 19:58:09 +0000

    Add:Core:More android stuff

--- trunk/navit/navit/android/src/org/navitproject/navit/NavitGraphics.java
+++ trunk/navit/navit/android/src/org/navitproject/navit/NavitGraphics.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.navitproject.navitgraphics;
+package org.navitproject.navit;
 
 import android.app.Activity;
 import android.content.Context;
