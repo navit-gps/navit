8bfae62db897efbeb1b37aa4c71a67c934ed882a
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-11-03 09:07:04 +0000

    Add:binding_dbus:Avoid some Polymorphisms
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@2719 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/binding/dbus/test.py
+++ navit/binding/dbus/test.py
@@ -5,7 +5,7 @@ conn = bus.get_object('org.navit_project.navit',
                        '/org/navit_project/navit')
 iface = dbus.Interface(conn, dbus_interface='org.navit_project.navit');
 iter=iface.attr_iter();
-navit=bus.get_object('org.navit_project.navit', conn.get_attr("navit",iter)[1]);
+navit=bus.get_object('org.navit_project.navit', conn.get_attr_wi("navit",iter)[1]);
 iface.attr_iter_destroy(iter);
 navit_iface = dbus.Interface(navit, dbus_interface='org.navit_project.navit.navit');
 navit_iface.set_center((1,0x138a4a,0x5d773f));
