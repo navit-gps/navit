10fdcf475cd5f895f5376db09a309ce04e3b6916
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2009-12-08 20:24:32 +0000

    Add:Android:Callback on activity change

--- navit/android.h
+++ navit/android.h
@@ -1,6 +1,7 @@
 #include <jni.h>
 extern JNIEnv *jnienv;
 extern jobject *android_activity;
+extern struct callback_list *android_activity_cbl;
 int android_find_class_global(char *name, jclass *ret);
 int android_find_method(jclass class, char *name, char *args, jmethodID *ret);
 int android_find_static_method(jclass class, char *name, char *args, jmethodID *ret);
