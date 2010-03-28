930236896bcae8522f352dee186d166b1203b49f
woglinde <woglinde@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-03-28 11:57:05 +0000

    Fix:Core: After leaving the loop, unref the loop
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@3093 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/event_glib.c
+++ navit/event_glib.c
@@ -29,16 +29,19 @@ static GMainLoop *loop;
 static void event_glib_main_loop_run(void)
 {
 	loop = g_main_loop_new (NULL, TRUE);
-        if (g_main_loop_is_running (loop))
-        {
+	if (g_main_loop_is_running (loop))
+	{
 		g_main_loop_run (loop);
 	}
 }
 
 static void event_glib_main_loop_quit(void)
 {
-	if (loop)
+	if (loop) {
 		g_main_loop_quit(loop);
+		g_main_loop_unref(loop);
+	}
+
 }
 
 struct event_watch {
