fbbf33e5ed17901aeb271a91c360e3d57f085146
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-06-28 19:02:15 +0000

    Fix:Core:Build on older glibs

--- navit/maptool/ch.c
+++ navit/maptool/ch.c
@@ -5,6 +5,14 @@
 #include "file.h"
 #include "debug.h"
 
+#if GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 10
+#define g_slice_alloc0 g_malloc0
+#define g_slice_new(x) g_new(x,1)
+#define g_slice_new0(x) g_new0(x,1)
+#define g_slice_free(x,y) g_free(y)
+#define g_slice_free1(x,y) g_free(y)
+#endif
+
 struct ch_edge {
 	int flags;
 	int weight;
--- navit/route.c
+++ navit/route.c
@@ -70,6 +70,12 @@
 #include "roadprofile.h"
 #include "debug.h"
 
+#if GLIB_MAJOR_VERSION == 2 && GLIB_MINOR_VERSION < 10
+#define g_slice_alloc0 g_malloc0
+#define g_slice_new0(x) g_new0(x,1)
+#define g_slice_free(x,y) g_free(y)
+#define g_slice_free1(x,y) g_free(y)
+#endif
 
 struct map_priv {
 	struct route *route;
