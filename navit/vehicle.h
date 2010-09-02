d9254950e4d43a418b2c96d78a66f8cc935dd714
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-09-02 08:58:54 +0000

    Add:Core:Missing prototypes
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@3546 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/vehicle.h
+++ navit/vehicle.h
@@ -34,19 +34,25 @@ struct vehicle_methods {
 };
 
 /* prototypes */
-void vehicle_log_gpx_add_tag(char *tag, char **logstr);
+enum attr_type;
+struct attr;
+struct attr_iter;
+struct cursor;
+struct graphics;
+struct point;
+struct vehicle;
 struct vehicle *vehicle_new(struct attr *parent, struct attr **attrs);
 void vehicle_destroy(struct vehicle *this_);
-struct attr_iter *vehicle_attr_iter_new(void); 
-void vehicle_attr_iter_destroy(struct attr_iter *iter); 
-
+struct attr_iter *vehicle_attr_iter_new(void);
+void vehicle_attr_iter_destroy(struct attr_iter *iter);
 int vehicle_get_attr(struct vehicle *this_, enum attr_type type, struct attr *attr, struct attr_iter *iter);
 int vehicle_set_attr(struct vehicle *this_, struct attr *attr);
 int vehicle_add_attr(struct vehicle *this_, struct attr *attr);
 int vehicle_remove_attr(struct vehicle *this_, struct attr *attr);
-
 void vehicle_set_cursor(struct vehicle *this_, struct cursor *cursor);
 void vehicle_draw(struct vehicle *this_, struct graphics *gra, struct point *pnt, int lazy, int angle, int speed);
+int vehicle_get_cursor_data(struct vehicle *this, struct point *pnt, int *angle, int *speed);
+void vehicle_log_gpx_add_tag(char *tag, char **logstr);
 /* end of prototypes */
 
 #ifdef __cplusplus
