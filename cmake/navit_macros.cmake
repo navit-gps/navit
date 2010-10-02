2d8f06256b4a649cdbae32156d38312fc2cf2962
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-10-02 19:58:55 +0000

    Add:core:Initial support for cmake build system. This first stage partly supports windows and linux platforms.

---
+++ cmake/navit_macros.cmake
@@ -0,0 +1,44 @@
+macro(set_with_reason VARIABLE REASON)
+   set(${VARIABLE} ${ARGN})
+   set(${VARIABLE}_REASON ${REASON})
+endmacro()
+
+# modules may be linked static, or shared for import at runtime
+macro(add_module MODULE_PATH)
+   list(APPEND ALL_MODULES ${MODULE_PATH})
+   set(${MODULE_PATH} ${ARGN})
+   set(${MODULE_PATH}_REASON "default")
+endmacro()
+
+# plugins are always linked static
+macro(add_plugin PLUGIN_PATH)
+   list(APPEND ALL_PLUGINS ${PLUGIN_PATH})
+   set(${PLUGIN_PATH} ${ARGN})
+   set(${PLUGIN_PATH}_REASON "default")
+endmacro()
+
+macro(module_add_library MODULE_NAME )
+#   add_definitions( -DMODULE=${MODULE_NAME} -fPIC)
+   add_library(${MODULE_NAME} ${MODULE_BUILD_TYPE} ${ARGN}) 
+   SET_TARGET_PROPERTIES(${MODULE_NAME} PROPERTIES COMPILE_DEFINITIONS "MODULE=${MODULE_NAME}")
+   
+   if (USE_PLUGINS)
+      SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES COMPILE_FLAGS ${NAVIT_COMPILE_FLAGS})
+      TARGET_LINK_LIBRARIES(${MODULE_NAME} navit_core)
+      install(TARGETS ${MODULE_NAME}
+              DESTINATION ${LIB_DIR}/navit
+              PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+   endif()
+#   TARGET_LINK_LIBRARIES(${MODULE_NAME} ${NAVIT_SUPPORT_LIBS})
+endmacro(module_add_library)
+
+macro(supportlib_add_library LIB_NAME )
+   add_library(${LIB_NAME} ${ARGN}) 
+   if (USE_PLUGINS)
+      SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES COMPILE_FLAGS ${NAVIT_COMPILE_FLAGS})
+      TARGET_LINK_LIBRARIES(${MODULE_NAME} navit_core)
+   endif()
+#   SET_TARGET_PROPERTIES(${LIB_NAME} PROPERTIES COMPILE_DEFINITIONS "MODULE=${LIB_NAME}")
+
+#   TARGET_LINK_LIBRARIES(${LIB_NAME} ${NAVIT_SUPPORT_LIBS})
+endmacro(supportlib_add_library)
---
+++ cmake/navit_xml_parser.cmake
@@ -0,0 +1,8 @@
+file(STRINGS "${SRC}" TEXT_LINES REGEX "_\\(")
+file(WRITE ${DST} "// Strings from navit_shipped.xml\n")
+
+foreach (LINE ${TEXT_LINES})
+   message(STATUS ${LINE})
+   string(REGEX REPLACE ".*(_\\(\"[^\"]*\"\\)).*" "\\1\n" OUTPUT_LINE ${LINE})
+   file(APPEND ${DST} ${OUTPUT_LINE})
+endforeach()
