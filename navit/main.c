d48c82eb2a57a385f196ab56d824e07a2274688e
geoghegan <geoghegan@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-10-26 18:52:04 +0000

    Add:core: Adds virtually all languages to the NLS Lookup table for WinCE / Win32 Locale settings

--- navit/main.c
+++ navit/main.c
@@ -128,48 +128,177 @@ main_setup_environment(int mode)
 
 #ifdef HAVE_API_WIN32_BASE
 char *nls_table[][3]={
+	// NLS Table compiled by Nick "Number6" Geoghegan
+	// Not an exhaustive list, but supports 99% of all languages in Windows
 	//{"LANGNAME", "CTRYNAME", "Language Code"},
-	// TODO - Find out the Languages for Spanish speaking countries
-	{"DAN", "DNK", "da_DK"},	// Danish
-	{"DEU", "DEU", "de_DE"},	// German
-	{"DEA", "AUT", "de_AT"},	// German - Austrian
-	{"FRA", "FRA", "fr_FR"},	// French
-	{"RUS", "RUS", "ru_RU"},	// Russian
-	{"PLK", "POL", "pl_PL"},	// Polish
-	{"BGR", "BGR", "bg_BG"},	// Bulgarian
-	{"ITA", "ITA", "it_IT"},	// Italian
-	{"CSY", "CZE", "cs_CZ"},	// Czech
-	{"FIN", "FIN", "fi_FI"},	// Finnish
-	{"ROM", "ROM", "ro_RO"},	// Romanian
-	{"SVE", "SWE", "sv_SE"},	// Swedish
-	{"BEL", "BLR", "be_BY"},	// Belarussian
-	{"HRV", "HRV", "hr_HR"},	// Croatian
-	{"HUN", "HUN", "hu_HU"},	// Hungary
-	{"NLD", "NLD", "nl_NL"},	// Dutch - Netherlands
-	{"FOS", "FRO", "fo_FO"}, 	// Faroese
-	{"ELL", "GRC", "el_GR"},	// Greek
-	{"HEB", "ISR", "he_IL"},	// Hebrew
-	{"ISL", "ISL", "is_IS"},	// Icelandic
-	{"IND", "IDN", "id_ID"},	// Indonesian
-	{"MSL", "MYS", "ms_MY"},	// Malay - Malaysia
-	{"NOR", "NOR", "nb_NO"},	// Norwegian - Bokmal
-	{"NON", "NOR", "nn_NO"},	// Norwegian - Nynorsk
-	{"PTB", "BRA", "pt_BR"},	// Portuguse - Brazil
-	{"PTG", "PRT", "pt_PT"},	// Portuguse - Portugal
-	{"SKY", "SVK", "sk_SK"},	// Slovak
-	{"THA", "THA", "th_TH"},	// Thai
-	{"TRK", "TUR", "tr_TR"},	// Turkish
-	{"ENI", "IRL", "en_IE"},	// English - Ireland
-	{"ENU", "USA", "en_US"},	// English - US
-	{"ENA", "AUS", "en_AU"},	// English - Australia
-	{"ENC", "CAN", "en_CA"},	// English - Canada
-	{"ENG", "GBR", "en_GB"},	// English - UK
-	{"ENZ", "NZL", "en_NZ"},	// English - New Zealand
-	{"DES", "CHE", "de_CH"},	// Swiss - German
-	{"FRS", "CHE", "fr_CH"},	// Swiss - French
-	{"ITS", "CHE", "it_CH"},	// Swiss - Italian
-	{"RMC", "CHE", "rm_CH"},	// Swiss - Romansh
-	{NULL,NULL,NULL},		
+	{"AFK", "ZAF", "af_ZA"},	// Afrikaans (South Africa)
+	{"SQI", "ALB", "sq_AL"},	// Albanian (Albania)
+	{"AMH", "ETH", "am_ET"},	// Amharic (Ethiopia)
+	{"ARG", "DZA", "ar_DZ"},	// Arabic (Algeria)
+	{"ARH", "BHR", "ar_BH"},	// Arabic (Bahrain)
+	{"ARE", "EGY", "ar_EG"},	// Arabic (Egypt)
+	{"ARI", "IRQ", "ar_IQ"},	// Arabic (Iraq)
+	{"ARJ", "JOR", "ar_JO"},	// Arabic (Jordan)
+	{"ARK", "KWT", "ar_KW"},	// Arabic (Kuwait)
+	{"ARB", "LBN", "ar_LB"},	// Arabic (Lebanon)
+	{"ARL", "LBY", "ar_LY"},	// Arabic (Libya)
+	{"ARM", "MAR", "ar_MA"},	// Arabic (Morocco)
+	{"ARO", "OMN", "ar_OM"},	// Arabic (Oman)
+	{"ARQ", "QAT", "ar_QA"},	// Arabic (Qatar)
+	{"ARA", "SAU", "ar_SA"},	// Arabic (Saudi Arabia)
+	{"ARS", "SYR", "ar_SY"},	// Arabic (Syria)
+	{"ART", "TUN", "ar_TN"},	// Arabic (Tunisia)
+	{"ARU", "ARE", "ar_AE"},	// Arabic (U.A.E.)
+	{"ARY", "YEM", "ar_YE"},	// Arabic (Yemen)
+	{"HYE", "ARM", "hy_AM"},	// Armenian (Armenia)
+	{"ASM", "IND", "as_IN"},	// Assamese (India)
+	{"BAS", "RUS", "ba_RU"},	// Bashkir (Russia)
+	{"EUQ", "ESP", "eu_ES"},	// Basque (Basque)
+	{"BEL", "BLR", "be_BY"},	// Belarusian (Belarus)
+	{"BNG", "BDG", "bn_BD"},	// Bengali (Bangladesh)
+	{"BNG", "IND", "bn_IN"},	// Bengali (India)
+	{"BRE", "FRA", "br_FR"},	// Breton (France)
+	{"BGR", "BGR", "bg_BG"},	// Bulgarian (Bulgaria)
+	{"CAT", "ESP", "ca_ES"},	// Catalan (Catalan)
+	{"ZHH", "HKG", "zh_HK"},	// Chinese (Hong Kong S.A.R.)
+	{"ZHM", "MCO", "zh_MO"},	// Chinese (Macao S.A.R.)
+	{"CHS", "CHN", "zh_CN"},	// Chinese (People's Republic of China)
+	{"ZHI", "SGP", "zh_SG"},	// Chinese (Singapore)
+	{"CHT", "TWN", "zh_TW"},	// Chinese (Taiwan)
+	{"COS", "FRA", "co_FR"},	// Corsican (France)
+	{"HRV", "HRV", "hr_HR"},	// Croatian (Croatia)
+	{"HRB", "BIH", "hr_BA"},	// Croatian (Latin, Bosnia and Herzegovina)
+	{"CSY", "CZE", "cs_CZ"},	// Czech (Czech Republic)
+	{"DAN", "DNK", "da_DK"},	// Danish (Denmark)
+	{"NLB", "BEL", "nl_BE"},	// Dutch (Belgium)
+	{"NLD", "NLD", "nl_NL"},	// Dutch (Netherlands)
+	{"ENA", "AUS", "en_AU"},	// English (Australia)
+	{"ENL", "BLZ", "en_BZ"},	// English (Belize)
+	{"ENC", "CAN", "en_CA"},	// English (Canada)
+	{"ENB", "CAR", "en_CB"},	// English (Caribbean)
+	{"ENN", "IND", "en_IN"},	// English (India)
+	{"ENI", "IRL", "en_IE"},	// English (Ireland)
+	{"ENJ", "JAM", "en_JM"},	// English (Jamaica)
+	{"ENM", "MYS", "en_MY"},	// English (Malaysia)
+	{"ENZ", "NZL", "en_NZ"},	// English (New Zealand)
+	{"ENP", "PHL", "en_PH"},	// English (Republic of the Philippines)
+	{"ENE", "SGP", "en_SG"},	// English (Singapore)
+	{"ENS", "ZAF", "en_ZA"},	// English (South Africa)
+	{"ENT", "TTO", "en_TT"},	// English (Trinidad and Tobago)
+	{"ENG", "GBR", "en_GB"},	// English (United Kingdom)
+	{"ENU", "USA", "en_US"},	// English (United States)
+	{"ENW", "ZWE", "en_ZW"},	// English (Zimbabwe)
+	{"ETI", "EST", "et_EE"},	// Estonian (Estonia)
+	{"FOS", "FRO", "fo_FO"},	// Faroese (Faroe Islands)
+	{"FIN", "FIN", "fi_FI"},	// Finnish (Finland)
+	{"FRB", "BEL", "fr_BE"},	// French (Belgium)
+	{"FRC", "CAN", "fr_CA"},	// French (Canada)
+	{"FRA", "FRA", "fr_FR"},	// French (France)
+	{"FRL", "LUX", "fr_LU"},	// French (Luxembourg)
+	{"FRM", "MCO", "fr_MC"},	// French (Principality of Monaco)
+	{"FRS", "CHE", "fr_CH"},	// French (Switzerland)
+	{"FYN", "NLD", "fy_NL"},	// Frisian (Netherlands)
+	{"GLC", "ESP", "gl_ES"},	// Galician (Galician)
+	{"KAT", "GEO", "ka_GE"},	// Georgian (Georgia)
+	{"DEA", "AUT", "de_AT"},	// German (Austria)
+	{"DEU", "DEU", "de_DE"},	// German (Germany)
+	{"DEC", "LIE", "de_LI"},	// German (Liechtenstein)
+	{"DEL", "LUX", "de_LU"},	// German (Luxembourg)
+	{"DES", "CHE", "de_CH"},	// German (Switzerland)
+	{"ELL", "GRC", "el_GR"},	// Greek (Greece)
+	{"KAL", "GRL", "kl_GL"},	// Greenlandic (Greenland)
+	{"GUJ", "IND", "gu_IN"},	// Gujarati (India)
+	{"HEB", "ISR", "he_IL"},	// Hebrew (Israel)
+	{"HIN", "IND", "hi_IN"},	// Hindi (India)
+	{"HUN", "HUN", "hu_HU"},	// Hungarian (Hungary)
+	{"ISL", "ISL", "is_IS"},	// Icelandic (Iceland)
+	{"IBO", "NGA", "ig_NG"},	// Igbo (Nigeria)
+	{"IND", "IDN", "id_ID"},	// Indonesian (Indonesia)
+	{"IRE", "IRL", "ga_IE"},	// Irish (Ireland)
+	{"XHO", "ZAF", "xh_ZA"},	// isiXhosa (South Africa)
+	{"ZUL", "ZAF", "zu_ZA"},	// isiZulu (South Africa)
+	{"ITA", "ITA", "it_IT"},	// Italian (Italy)
+	{"ITS", "CHE", "it_CH"},	// Italian (Switzerland)
+	{"JPN", "JPN", "ja_JP"},	// Japanese (Japan)
+	{"KDI", "IND", "kn_IN"},	// Kannada (India)
+	{"KKZ", "KAZ", "kk_KZ"},	// Kazakh (Kazakhstan)
+	{"KHM", "KHM", "km_KH"},	// Khmer (Cambodia)
+	{"KIN", "RWA", "rw_RW"},	// Kinyarwanda (Rwanda)
+	{"SWK", "KEN", "sw_KE"},	// Kiswahili (Kenya)
+	{"KOR", "KOR", "ko_KR"},	// Korean (Korea)
+	{"KYR", "KGZ", "ky_KG"},	// Kyrgyz (Kyrgyzstan)
+	{"LAO", "LAO", "lo_LA"},	// Lao (Lao P.D.R.)
+	{"LVI", "LVA", "lv_LV"},	// Latvian (Latvia)
+	{"LTH", "LTU", "lt_LT"},	// Lithuanian (Lithuania)
+	{"LBX", "LUX", "lb_LU"},	// Luxembourgish (Luxembourg)
+	{"MKI", "MKD", "mk_MK"},	// Macedonian (Former Yugoslav Republic of Macedonia)
+	{"MSB", "BRN", "ms_BN"},	// Malay (Brunei Darussalam)
+	{"MSL", "MYS", "ms_MY"},	// Malay (Malaysia)
+	{"MYM", "IND", "ml_IN"},	// Malayalam (India)
+	{"MLT", "MLT", "mt_MT"},	// Maltese (Malta)
+	{"MRI", "NZL", "mi_NZ"},	// Maori (New Zealand)
+	{"MAR", "IND", "mr_IN"},	// Marathi (India)
+	{"MON", "MNG", "mn_MN"},	// Mongolian (Cyrillic, Mongolia)
+	{"NEP", "NEP", "ne_NP"},	// Nepali (Nepal)
+	{"NOR", "NOR", "nb_NO"},	// Norwegian, Bokmå(Norway)
+	{"NON", "NOR", "nn_NO"},	// Norwegian, Nynorsk (Norway)
+	{"OCI", "FRA", "oc_FR"},	// Occitan (France)
+	{"ORI", "IND", "or_IN"},	// Oriya (India)
+	{"PAS", "AFG", "ps_AF"},	// Pashto (Afghanistan)
+	{"FAR", "IRN", "fa_IR"},	// Persian
+	{"PLK", "POL", "pl_PL"},	// Polish (Poland)
+	{"PTB", "BRA", "pt_BR"},	// Portuguese (Brazil)
+	{"PTG", "PRT", "pt_PT"},	// Portuguese (Portugal)
+	{"PAN", "IND", "pa_IN"},	// Punjabi (India)
+	{"ROM", "ROM", "ro_RO"},	// Romanian (Romania)
+	{"RMC", "CHE", "rm_CH"},	// Romansh (Switzerland)
+	{"RUS", "RUS", "ru_RU"},	// Russian (Russia)
+	{"SMG", "FIN", "se_FI"},	// Sami, Northern (Finland)
+	{"SME", "NOR", "se_NO"},	// Sami, Northern (Norway)
+	{"SMF", "SWE", "se_SE"},	// Sami, Northern (Sweden)
+	{"SAN", "IND", "sa_IN"},	// Sanskrit (India)
+	{"TSN", "ZAF", "tn_ZA"},	// Setswana (South Africa)
+	{"SIN", "LKA", "si_LK"},	// Sinhala (Sri Lanka)
+	{"SKY", "SVK", "sk_SK"},	// Slovak (Slovakia)
+	{"SLV", "SVN", "sl_SI"},	// Slovenian (Slovenia)
+	{"ESS", "ARG", "es_AR"},	// Spanish (Argentina)
+	{"ESB", "BOL", "es_BO"},	// Spanish (Bolivia)
+	{"ESL", "CHL", "es_CL"},	// Spanish (Chile)
+	{"ESO", "COL", "es_CO"},	// Spanish (Colombia)
+	{"ESC", "CRI", "es_CR"},	// Spanish (Costa Rica)
+	{"ESD", "DOM", "es_DO"},	// Spanish (Dominican Republic)
+	{"ESF", "ECU", "es_EC"},	// Spanish (Ecuador)
+	{"ESE", "SLV", "es_SV"},	// Spanish (El Salvador)
+	{"ESG", "GTM", "es_GT"},	// Spanish (Guatemala)
+	{"ESH", "HND", "es_HN"},	// Spanish (Honduras)
+	{"ESM", "MEX", "es_MX"},	// Spanish (Mexico)
+	{"ESI", "NIC", "es_NI"},	// Spanish (Nicaragua)
+	{"ESA", "PAN", "es_PA"},	// Spanish (Panama)
+	{"ESZ", "PRY", "es_PY"},	// Spanish (Paraguay)
+	{"ESR", "PER", "es_PE"},	// Spanish (Peru)
+	{"ESU", "PRI", "es_PR"},	// Spanish (Puerto Rico)
+	{"ESN", "ESP", "es_ES"},	// Spanish (Spain)
+	{"EST", "USA", "es_US"},	// Spanish (United States)
+	{"ESY", "URY", "es_UY"},	// Spanish (Uruguay)
+	{"ESV", "VEN", "es_VE"},	// Spanish (Venezuela)
+	{"SVF", "FIN", "sv_FI"},	// Swedish (Finland)
+	{"SVE", "SWE", "sv_SE"},	// Swedish (Sweden)
+	{"TAM", "IND", "ta_IN"},	// Tamil (India)
+	{"TTT", "RUS", "tt_RU"},	// Tatar (Russia)
+	{"TEL", "IND", "te_IN"},	// Telugu (India)
+	{"THA", "THA", "th_TH"},	// Thai (Thailand)
+	{"BOB", "CHN", "bo_CN"},	// Tibetan (PRC)
+	{"TRK", "TUR", "tr_TR"},	// Turkish (Turkey)
+	{"TUK", "TKM", "tk_TM"},	// Turkmen (Turkmenistan)
+	{"UIG", "CHN", "ug_CN"},	// Uighur (PRC)
+	{"UKR", "UKR", "uk_UA"},	// Ukrainian (Ukraine)
+	{"URD", "PAK", "ur_PK"},	// Urdu (Islamic Republic of Pakistan)
+	{"VIT", "VNM", "vi_VN"},	// Vietnamese (Vietnam)
+	{"CYM", "GBR", "cy_GB"},	// Welsh (United Kingdom)
+	{"WOL", "SEN", "wo_SN"},	// Wolof (Senegal)
+	{"III", "CHN", "ii_CN"},	// Yi (PRC)
+	{"YOR", "NGA", "yo_NG"},	// Yoruba (Nigeria)
+	{NULL,NULL,NULL},		// Default - Can't find the language / Language not listed above		
 };
 
 static void
