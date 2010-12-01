cf5557667fb7465362650881d7662ef7851cd0ce
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-12-01 13:22:41 +0000

    Add:Build:Incomplete support for arm-mingw32ce
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@3735 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- Toolchain/arm-mingw32ce.cmake
+++ Toolchain/arm-mingw32ce.cmake
@@ -3,14 +3,5 @@ SET(CMAKE_SYSTEM_NAME WINCE)
 # specify the cross compiler
 SET(CMAKE_C_COMPILER "arm-mingw32ce-gcc")
 SET(CMAKE_CXX_COMPILER "arm-mingw32ce-g++")
+SET(PKG_CONFIG_EXECUTABLE "arm-mingw32ce-pkg-config")
 SET(WINCE TRUE)
-
-# where is the target environment
-#SET(CMAKE_FIND_ROOT_PATH /usr/i686-pc-mingw32/sys-root/mingw)
-
-# search for programs in the build host directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
