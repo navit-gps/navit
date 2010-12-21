61d036ed9713e083cbe44790adcf5d74f67a6aaa
norad <norad@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-12-21 15:35:37 +0000

    Fix:core:Fix assertion

--- trunk/navit/navit/graphics.c
+++ trunk/navit/navit/graphics.c
@@ -1238,7 +1238,7 @@ graphics_draw_polyline_as_polygon(struct graphics *gra, struct graphics_gc *gc, 
 			l=1;
 		if (wi*lscale > 10000)
 			lscale=10000/wi;
-		dbg_assert(wi*lscale < 10000);
+		dbg_assert(wi*lscale <= 10000);
 		calc_offsets(wi*lscale, l, dx, dy, &o);
 		pos.x = pnt[i].x + o.ny;
 		pos.y = pnt[i].y + o.px;
