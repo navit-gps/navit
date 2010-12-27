e272bc0d40fbbbd80908fa0bb747b1c812ebce94
norad <norad@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2010-12-27 13:03:13 +0000

    fix:vehicle/webos:PDL can give us error-texts :D . Use that!

--- navit/vehicle/webos/vehicle_webos.c
+++ navit/vehicle/webos/vehicle_webos.c
@@ -117,7 +117,7 @@ vehicle_webos_open(struct vehicle_priv *priv)
 			priv,
 			PDL_FALSE);
 	if (err != PDL_NOERROR) {
-		dbg(0,"PDL_ServiceCallWithCallback failed with %d\n", err);
+		dbg(0,"PDL_ServiceCallWithCallback failed with (%d): (%s)\n", err, PDL_GetError());
 		vehicle_webos_close(priv);
 		return 0;
 	}
