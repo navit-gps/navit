ef495a4b12f2cf980d11cc453e166424820d4c34
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-01-04 14:26:50 +0000

    Fix:Build:Fix cmake build to link external libraries to plugins/modules which depend on it, not to navit_core.

--- cmake/navit_macros.cmake
+++ cmake/navit_macros.cmake
@@ -1,6 +1,8 @@
-macro(set_with_reason VARIABLE REASON)
-   set(${VARIABLE} ${ARGN})
+macro(set_with_reason VARIABLE REASON ENABLE)
+   set(${VARIABLE} ${ENABLE})
    set(${VARIABLE}_REASON ${REASON})
+   string(REPLACE "/" "_" VARIABLE_NAMES ${VARIABLE})
+   set(${VARIABLE_NAMES}_LIBS ${ARGN})
 endmacro()
 
 # modules may be linked static, or shared for import at runtime
@@ -20,6 +22,7 @@ endmacro()
 macro(module_add_library MODULE_NAME )
    add_library(${MODULE_NAME} ${MODULE_BUILD_TYPE} ${ARGN}) 
    SET_TARGET_PROPERTIES(${MODULE_NAME} PROPERTIES COMPILE_DEFINITIONS "MODULE=${MODULE_NAME}")
+   TARGET_LINK_LIBRARIES(${MODULE_NAME} ${${MODULE_NAME}_LIBS})
    
    if (USE_PLUGINS)
       SET_TARGET_PROPERTIES( ${MODULE_NAME} PROPERTIES COMPILE_FLAGS ${NAVIT_COMPILE_FLAGS})
