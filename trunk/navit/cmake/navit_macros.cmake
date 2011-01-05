7bc9ba954cd83c6b073f021cb68305d80f25f7a5
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-01-05 13:01:30 +0000

    Add:Build:Add more infos to cmake summary

--- trunk/navit/cmake/navit_macros.cmake
+++ trunk/navit/cmake/navit_macros.cmake
@@ -5,6 +5,17 @@ macro(set_with_reason VARIABLE REASON ENABLE)
    set(${VARIABLE_NAMES}_LIBS ${ARGN})
 endmacro()
 
+macro(add_feature FEATURE REASON ENABLE)
+   list(APPEND ALL_FEATURES ${FEATURE})
+   set(${FEATURE} ${ENABLE})
+   set(${FEATURE}_REASON ${REASON})
+endmacro()
+
+macro(cfg_feature FEATURE REASON ENABLE)
+   set(${FEATURE} ${ENABLE})
+   set(${FEATURE}_REASON ${REASON})
+endmacro()
+
 # modules may be linked static, or shared for import at runtime
 macro(add_module MODULE_PATH REASON)
    list(APPEND ALL_MODULES ${MODULE_PATH})
@@ -20,16 +31,16 @@ macro(add_plugin PLUGIN_PATH REASON)
 endmacro()
 
 macro(module_add_library MODULE_NAME )
-   add_library(${MODULE_NAME} ${MODULE_BUILD_TYPE} ${ARGN}) 
+   add_library(${MODULE_NAME} ${MODULE_BUILD_TYPE} ${ARGN})
    SET_TARGET_PROPERTIES(${MODULE_NAME} PROPERTIES COMPILE_DEFINITIONS "MODULE=${MODULE_NAME}")
    TARGET_LINK_LIBRARIES(${MODULE_NAME} ${${MODULE_NAME}_LIBS})
-   
+
    if (USE_PLUGINS)
       SET_TARGET_PROPERTIES( ${MODULE_NAME} PROPERTIES COMPILE_FLAGS ${NAVIT_COMPILE_FLAGS})
-      
+
       # workaround to be compatible with old paths
       set_target_properties( ${MODULE_NAME} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/.libs")
-      
+
       TARGET_LINK_LIBRARIES(${MODULE_NAME} navit_core)
       install(TARGETS ${MODULE_NAME}
               DESTINATION ${LIB_DIR}/navit/${${MODULE_NAME}_TYPE}
@@ -38,7 +49,7 @@ macro(module_add_library MODULE_NAME )
 endmacro(module_add_library)
 
 macro(supportlib_add_library LIB_NAME )
-   add_library(${LIB_NAME} ${ARGN}) 
+   add_library(${LIB_NAME} ${ARGN})
    if (USE_PLUGINS)
       SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES COMPILE_FLAGS ${NAVIT_COMPILE_FLAGS})
       TARGET_LINK_LIBRARIES(${MODULE_NAME} navit_core)
