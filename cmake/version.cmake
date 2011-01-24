5767e352e09340c4142169ffcaf380ada28c2827
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-01-24 14:50:16 +0000

    Fix:Build/cmake:Cleanup of version.cmake
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@3960 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,23 +1,5 @@
-#FIND_PACKAGE(Subversion)
-
-#if (Subversion_FOUND)
-#    Subversion_WC_INFO("${PROJECT_SOURCE_DIR}" Project)
-#    MESSAGE("Current revision is ${Project_WC_REVISION}")
-#    set (VERSION ${Project_WC_REVISION})
-#    Subversion_WC_LOG(${PROJECT_SOURCE_DIR} Project)
-#    MESSAGE("Last changed log is ${Project_LAST_CHANGED_LOG}")
-#endif(Subversion_FOUND)
-
-
-#FIND_PROGRAM(Subversion_SVN_EXECUTABLE svn DOC "subversion command line client")
-#EXECUTE_PROCESS(
-#     COMMAND ${Subversion_SVN_EXECUTABLE} --version
-#     OUTPUT_VARIABLE VERSION
-#     OUTPUT_STRIP_TRAILING_WHITESPACE
-#)
 FIND_PROGRAM(GIT_EXECUTABLE git DOC "git command line client")
 
-#set (VERSION "")
 EXECUTE_PROCESS(
      COMMAND ${GIT_EXECUTABLE} svn info
      COMMAND grep "Revision"
