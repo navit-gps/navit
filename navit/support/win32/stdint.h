6b53ba253d93737726ebcb19af27f69a67ca7be1
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-01-25 15:02:38 +0000

    Add:support_win32:Define types for msvc

---
+++ navit/support/win32/stdint.h
@@ -0,0 +1,12 @@
+#ifndef _MSC_VER
+#include_next <stdint.h>
+#else
+typedef signed char int8_t;
+typedef unsigned char   uint8_t;
+typedef short  int16_t;
+typedef unsigned short  uint16_t;
+typedef int  int32_t;
+typedef unsigned   uint32_t;
+typedef long long  int64_t;
+typedef unsigned long long   uint64_t;
+#endif /* _MSC_VER */
