e51fc7c3b35c32eda70e76a189c74731bc38277a
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-03-12 20:40:05 +0000

    Fix:android:Remove some superfluous code

--- navit/android/src/org/navitproject/navit/Navit.java
+++ navit/android/src/org/navitproject/navit/Navit.java
@@ -410,55 +410,29 @@ public class Navit extends Activity implements Handler.Callback
 			Log.e("Navit", "Failed to extract language resource " + langc);
 		}
 
-		my_display_density = "mdpi";
-		// ldpi display (120 dpi)
 		if (Navit.metrics.densityDpi <= 120)
 		{
 			my_display_density = "ldpi";
-			if (!extractRes("navitldpi", NAVIT_DATA_DIR + "/share/navit.xml"))
-			{
-				Log.e("Navit", "Failed to extract navit.xml for ldpi device(s)");
-			}
 		}
-		// mdpi display (120 dpi)
-		else if ((Navit.metrics.densityDpi > 120) && (Navit.metrics.densityDpi <= 160))
+		else if (Navit.metrics.densityDpi <= 160)
 		{
 			my_display_density = "mdpi";
-			if (!extractRes("navitmdpi", NAVIT_DATA_DIR + "/share/navit.xml"))
-			{
-				Log.e("Navit", "Failed to extract navit.xml for mdpi device(s)");
-			}
 		}
-		// hdpi display (240 dpi)
-		else if ((Navit.metrics.densityDpi > 160) && (Navit.metrics.densityDpi < 320))
-		//else if (Navit.metrics.densityDpi == 240)
+		else if (Navit.metrics.densityDpi < 320)
 		{
 			my_display_density = "hdpi";
-			if (!extractRes("navithdpi", NAVIT_DATA_DIR + "/share/navit.xml"))
-			{
-				Log.e("Navit", "Failed to extract navit.xml for hdpi device(s)");
-			}
 		}
-		// xhdpi display (320 dpi)
-		else if (Navit.metrics.densityDpi >= 320)
+		else
 		{
 			Log.e("Navit", "found xhdpi device, this is not fully supported!!");
 			Log.e("Navit", "using hdpi values");
 			my_display_density = "hdpi";
-			if (!extractRes("navithdpi", NAVIT_DATA_DIR + "/share/navit.xml"))
-			{
-				Log.e("Navit", "Failed to extract navit.xml for xhdpi device(s)");
-			}
 		}
-		else
+
+		if (!extractRes("navit" + my_display_density, NAVIT_DATA_DIR + "/share/navit.xml"))
 		{
-			/* default, meaning we just dont know what display this is */
-			if (!extractRes("navit", NAVIT_DATA_DIR + "/share/navit.xml"))
-			{
-				Log.e("Navit", "Failed to extract navit.xml (default version)");
-			}
+			Log.e("Navit", "Failed to extract navit.xml for " + my_display_density);
 		}
-		// Debug.startMethodTracing("calc");
 
 		// --> dont use!! NavitMain(this, langu, android.os.Build.VERSION.SDK_INT);
 		Log.e("Navit", "android.os.Build.VERSION.SDK_INT="
