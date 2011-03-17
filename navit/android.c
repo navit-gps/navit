af81dc48daec6befea39e81c2be0aa7f1eb1947d
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-03-17 11:54:14 +0000

    Fix:android:Code cleanup and fixed some warnings
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@4356 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/android.c
+++ navit/android.c
@@ -10,8 +10,11 @@
 #include "debug.h"
 #include "event.h"
 #include "callback.h"
+#include "country.h"
 #include "projection.h"
 #include "map.h"
+#include "mapset.h"
+#include "navit_nls.h"
 #include "transform.h"
 #include "color.h"
 #include "types.h"
@@ -24,72 +27,72 @@ struct callback_list *android_activity_cbl;
 int android_version;
 
 struct navit {
-        struct attr self;
-        GList *mapsets;
-        GList *layouts;
-        struct gui *gui;
-        struct layout *layout_current;
-        struct graphics *gra;
-        struct action *action;
-        struct transformation *trans, *trans_cursor;
-        struct compass *compass;
-        struct route *route;
-        struct navigation *navigation;
-        struct speech *speech;
-        struct tracking *tracking;
-        int ready;
-        struct window *win;
-        struct displaylist *displaylist;
-        int tracking_flag;
-        int orientation;
-        int recentdest_count;
-        int osd_configuration;
-        GList *vehicles;
-        GList *windows_items;
-        struct navit_vehicle *vehicle;
-        struct callback_list *attr_cbl;
-        struct callback *nav_speech_cb, *roadbook_callback, *popup_callback, *route_cb, *progress_cb;
-        struct datawindow *roadbook_window;
-        struct map *former_destination;
-        struct point pressed, last, current;
-        int button_pressed,moved,popped,zoomed;
-        int center_timeout;
-        int autozoom_secs;
-        int autozoom_min;
-        int autozoom_active;
-        struct event_timeout *button_timeout, *motion_timeout;
-        struct callback *motion_timeout_callback;
-        int ignore_button;
-        int ignore_graphics_events;
-        struct log *textfile_debug_log;
-        struct pcoord destination;
-        int destination_valid;
-        int blocked;
-        int w,h;
-        int drag_bitmap;
-        int use_mousewheel;
-        struct messagelist *messages;
-        struct callback *resize_callback,*button_callback,*motion_callback,*predraw_callback;
-        struct vehicleprofile *vehicleprofile;
-        GList *vehicleprofiles;
-        int pitch;
-        int follow_cursor;
-        int prevTs;
-        int graphics_flags;
-        int zoom_min, zoom_max;
-        int radius;
-        struct bookmarks *bookmarks;
-        int flags;
-                 /* 1=No graphics ok */
-                 /* 2=No gui ok */
-        int border;
+	struct attr self;
+	GList *mapsets;
+	GList *layouts;
+	struct gui *gui;
+	struct layout *layout_current;
+	struct graphics *gra;
+	struct action *action;
+	struct transformation *trans, *trans_cursor;
+	struct compass *compass;
+	struct route *route;
+	struct navigation *navigation;
+	struct speech *speech;
+	struct tracking *tracking;
+	int ready;
+	struct window *win;
+	struct displaylist *displaylist;
+	int tracking_flag;
+	int orientation;
+	int recentdest_count;
+	int osd_configuration;
+	GList *vehicles;
+	GList *windows_items;
+	struct navit_vehicle *vehicle;
+	struct callback_list *attr_cbl;
+	struct callback *nav_speech_cb, *roadbook_callback, *popup_callback, *route_cb, *progress_cb;
+	struct datawindow *roadbook_window;
+	struct map *former_destination;
+	struct point pressed, last, current;
+	int button_pressed,moved,popped,zoomed;
+	int center_timeout;
+	int autozoom_secs;
+	int autozoom_min;
+	int autozoom_active;
+	struct event_timeout *button_timeout, *motion_timeout;
+	struct callback *motion_timeout_callback;
+	int ignore_button;
+	int ignore_graphics_events;
+	struct log *textfile_debug_log;
+	struct pcoord destination;
+	int destination_valid;
+	int blocked;
+	int w,h;
+	int drag_bitmap;
+	int use_mousewheel;
+	struct messagelist *messages;
+	struct callback *resize_callback,*button_callback,*motion_callback,*predraw_callback;
+	struct vehicleprofile *vehicleprofile;
+	GList *vehicleprofiles;
+	int pitch;
+	int follow_cursor;
+	int prevTs;
+	int graphics_flags;
+	int zoom_min, zoom_max;
+	int radius;
+	struct bookmarks *bookmarks;
+	int flags;
+			/* 1=No graphics ok */
+			/* 2=No gui ok */
+	int border;
 };
 
 struct attr attr;
 
 struct config {
-        struct attr **attrs;
-        struct callback_list *cbl;
+	struct attr **attrs;
+	struct callback_list *cbl;
 } *config;
 
 
@@ -211,64 +214,7 @@ struct gui_priv {
 	} html[10];
 };
 
-
-
-
-static void
-gui_internal_search_list_set_default_country2(struct gui_priv *this)
-{
-	struct attr search_attr, country_name, country_iso2, *country_attr;
-	struct item *item;
-	struct country_search *cs;
-	struct tracking *tracking;
-	struct search_list_result *res;
-
-	dbg(0,"### 1");
-
-	country_attr=country_default();
-	tracking=navit_get_tracking(this->nav);
-
-	if (tracking && tracking_get_attr(tracking, attr_country_id, &search_attr, NULL))
-		country_attr=&search_attr;
-	if (country_attr) {
-		dbg(0,"### 2");
-		cs=country_search_new(country_attr, 0);
-		item=country_search_get_item(cs);
-		if (item && item_attr_get(item, attr_country_name, &country_name)) {
-			search_attr.type=attr_country_all;
-			dbg(0,"country %s\n", country_name.u.str);
-			search_attr.u.str=country_name.u.str;
-			search_list_search(this->sl, &search_attr, 0);
-			while((res=search_list_get_result(this->sl)));
-			if(this->country_iso2)
-			{
-				// this seems to cause a crash, no idea why
-				//g_free(this->country_iso2);
-				this->country_iso2=NULL;
-			}
-			if (item_attr_get(item, attr_country_iso2, &country_iso2))
-			{
-				this->country_iso2=g_strdup(country_iso2.u.str);
-			}
-		}
-		country_search_destroy(cs);
-	} else {
-		dbg(0,"warning: no default country found\n");
-		if (this->country_iso2) {
-		    dbg(0,"attempting to use country '%s'\n",this->country_iso2);
-		    search_attr.type=attr_country_iso2;
-		    search_attr.u.str=this->country_iso2;
-            search_list_search(this->sl, &search_attr, 0);
-            while((res=search_list_get_result(this->sl)));
-		}
-	}
-	dbg(0,"### 99");
-}
-
-
-
-struct navit *global_navit;
-
+extern struct navit *global_navit;
 
 int
 android_find_class_global(char *name, jclass *ret)
@@ -367,7 +313,7 @@ Java_org_navitproject_navit_NavitGraphics_KeypressCallback( JNIEnv* env, jobject
 	const char *s;
 	dbg(0,"enter %p %p\n",(struct callback *)id,str);
 	s=(*env)->GetStringUTFChars(env, str, NULL);
-	dbg(0,"key=%s\n",s);
+	dbg(0,"key=%d",s);
 	if (id)
 		callback_call_1((struct callback *)id,s);
 	(*env)->ReleaseStringUTFChars(env, str, s);
@@ -444,136 +390,35 @@ android_return_search_result(struct jni_object *jni_o, char *str)
 	(*env2)->DeleteLocalRef(jni_o->env, js2);
 }
 
-
 JNIEXPORT void JNICALL
-Java_org_navitproject_navit_NavitGraphics_CallbackSearchResultList( JNIEnv* env, jobject thiz, int id, int partial, jobject str)
+Java_org_navitproject_navit_NavitGraphics_CallbackSearchResultList( JNIEnv* env, jobject thiz, int partial, jobject str)
 {
-	const char *s;
-	s=(*env)->GetStringUTFChars(env, str, NULL);
-	dbg(0,"*****string=%s\n",s);
-
+	const char *search_string =(*env)->GetStringUTFChars(env, str, NULL);
+	dbg(0,"search '%s'\n", search_string);
 
 	config_get_attr(config, attr_navit, &attr, NULL);
-	// attr.u.navit
 
-	jstring js2 = NULL;
 	jclass cls = (*env)->GetObjectClass(env,thiz);
 	jmethodID aMethodID = (*env)->GetMethodID(env, cls, "fillStringArray", "(Ljava/lang/String;)V");
-	if(aMethodID == 0)
+	if(aMethodID != 0)
 	{
+		struct jni_object my_jni_object;
+		GList *ret;
+		struct mapset *ms4=navit_get_mapset(attr.u.navit);
+
+		my_jni_object.env=env;
+		my_jni_object.jo=thiz;
+		my_jni_object.jm=aMethodID;
+
+/* TODO (rikky#1#): does return nothing yet, also should use a generic callback instead of jni_object */		ret=search_by_address(ms4,search_string,partial,&my_jni_object);
+		dbg(0,"ret=%p\n",ret);
+
+		g_list_free(ret);
+	}
+	else
 		dbg(0,"**** Unable to get methodID: fillStringArray");
-		return;
-	}
 
-	if (id)
-	{
-		// search for town in variable "s" within current country -> return a list of towns as result
-		if (id==1)
-		{
-			// try to search for a town in the current country
-			struct attr s_attr;
-			struct gui_priv *gp;
-			struct gui_priv gp_2;
-			gp=&gp_2;
-			gp->nav=attr.u.navit;
-			struct mapset *ms=navit_get_mapset(gp->nav);
-			gp->sl=search_list_new(ms);
-
-			s_attr.type=attr_town_or_district_name;
-			// search for variable "s" as town name
-			s_attr.u.str=s;
-
-			gui_internal_search_list_set_default_country2(gp);
-
-			if (gp->sl)
-			{
-				search_list_select(gp->sl, attr_country_all, 0, 0);
-				search_list_search(gp->sl, &s_attr, partial);
-
-				struct search_list_result *res;
-				struct attr attr;
-				while((res=search_list_get_result(gp->sl)))
-				{
-					//dbg(0,"**** aaa8");
-					if (item_attr_get(&res->town->itemt, attr_label, &attr))
-					{
-						dbg(0,"***search result C=%s T=%s",res->country->iso2,attr.u.str);
-						// return all the town names
-						js2 = (*env)->NewStringUTF(env,attr.u.str);
-						(*env)->CallVoidMethod(env, thiz, aMethodID, js2);
-						(*env)->DeleteLocalRef(env, js2);
-					}
-
-					//if (res->town->common.district_name)
-					//{
-					//	dbg(0,"***444-ccc %s",res->town->common.district_name);
-					//}
-				}
-			}
-			g_free(gp->country_iso2);
-			gp->country_iso2=NULL;
-
-			if (gp->sl)
-			{
-				search_list_destroy(gp->sl);
-				gp->sl=NULL;
-			}
-		}
-		// search for street in variable "s" within current country -> return a list of streets as result
-		else if (id == 2)
-		{
-			struct attr s_attr4;
-			struct gui_priv *gp4;
-			struct gui_priv gp_24;
-
-
-			struct jni_object my_jni_object;
-			my_jni_object.env=env;
-			my_jni_object.jo=thiz;
-			my_jni_object.jm=aMethodID;
-
-			gp4=&gp_24;
-			gp4->nav=attr.u.navit;
-			struct mapset *ms4=navit_get_mapset(gp4->nav);
-			GList *ret=NULL;
-			ret=search_by_address(ret,ms4,s,partial,&my_jni_object);
-			dbg(0,"ret=%p\n",ret);
-
-			//struct search_list_result *res;
-
-			// get the list in the right order
-			//ret=g_list_reverse(ret);
-			// set to first element
-			//ret=g_list_first(ret);
-			// iterate thru the list
-			//dbg(0,"ret=%p\n",ret);
-			//while (ret)
-			//{
-				//dbg(0,"result list iterate %s\n",ret->data);
-				// return all the results to java
-				// return a string like: "16.766:48.76:full address name is at the end"
-
-				// **android_return_search_result(&my_jni_object,ret->data);
-
-				//## js2 = (*env)->NewStringUTF(env, ret->data);
-				//## (*env)->CallVoidMethod(env, thiz, aMethodID, js2);
-				//## (*env)->DeleteLocalRef(env, js2);
-
-			//	ret=g_list_next(ret);
-			//}
-			// free the memory
-			g_list_free(ret);
-			//dbg(0,"ret=%p\n",ret);
-
-			if (gp4->sl)
-			{
-				search_list_destroy(gp4->sl);
-				gp4->sl=NULL;
-			}
-		}
-	}
-
-	(*env)->ReleaseStringUTFChars(env, str, s);
+	(*env)->ReleaseStringUTFChars(env, str, search_string);
 }
 
 
@@ -598,136 +443,133 @@ Java_org_navitproject_navit_NavitGraphics_CallbackLocalizedString( JNIEnv* env, 
 }
 
 JNIEXPORT void JNICALL
-Java_org_navitproject_navit_NavitGraphics_CallbackMessageChannel( JNIEnv* env, jobject thiz, int i, jobject str)
+Java_org_navitproject_navit_NavitGraphics_CallbackMessageChannel( JNIEnv* env, jobject thiz, int channel, jobject str)
 {
 	const char *s;
-	dbg(0,"enter %p %p\n",(struct callback *)i,str);
+	dbg(0,"enter %d %p\n",channel,str);
 
 	config_get_attr(config, attr_navit, &attr, NULL);
-	// attr.u.navit
 
-	if (i)
+	switch(channel)
 	{
-		if (i == 1)
-		{
-			// zoom in
-			navit_zoom_in_cursor(global_navit, 2);
-			// navit_zoom_in_cursor(attr.u.navit, 2);
-		}
-		else if (i == 2)
-		{
-			// zoom out
-			navit_zoom_out_cursor(global_navit, 2);
-			// navit_zoom_out_cursor(attr.u.navit, 2);
-		}
-		else if (i==6)
+	case 1:
+		// zoom in
+		navit_zoom_in_cursor(global_navit, 2);
+		break;
+	case 2:
+		// zoom out
+		navit_zoom_out_cursor(global_navit, 2);
+		break;
+	case 6:
+		// this procedure is not used at the moment!!
+
+		// hopefully use a newly downloaded map, we just hope its set in navit.xml (make nicer soon)
+		// remove all curents maps
+		// ******global_navit->mapsets=NULL; // is this the correct way to clear the list?
+		// now add the default /sdcard/navitmap.bin entry to the list
+		// ******navit_add_mapset(global_navit,ms); // but how? please write me!!
+		// now reload some stuff to make the change stick
+		dbg(0,"trying to apply newly downloaded map to mapset %p\n",global_navit->mapsets);
+		if (global_navit->mapsets)
 		{
 			struct mapset *ms;
 			struct map *map;
+			struct mapset_handle *msh;
 
-			// this procedure is not used at the moment!!
-
-			// hopefully use a newly downloaded map, we just hope its set in navit.xml (make nicer soon)
-			// remove all curents maps
-			// ******global_navit->mapsets=NULL; // is this the correct way to clear the list?
-			// now add the default /sdcard/navitmap.bin entry to the list
-			// ******navit_add_mapset(global_navit,ms); // but how? please write me!!
-			// now reload some stuff to make the change stick
-			dbg(0,"trying to apply newly downloaded map to mapset %p\n",global_navit->mapsets);
-			if (global_navit->mapsets)
-			{
-				struct mapset_handle *msh;
-				ms=global_navit->mapsets->data;
-				msh=mapset_open(ms);
-				while (msh && (map=mapset_next(msh, 0))) {
-				}
-				mapset_close(msh);
+			ms=global_navit->mapsets->data;
+			msh=mapset_open(ms);
+			while (msh && (map=mapset_next(msh, 0))) {
 			}
+			mapset_close(msh);
 		}
-		else if (i==5)
-		{
-			// call a command (like in gui)
-			s=(*env)->GetStringUTFChars(env, str, NULL);
-			dbg(0,"*****string=%s\n",s);
-			command_evaluate(&attr.u.navit->self,s);
-			(*env)->ReleaseStringUTFChars(env, str, s);
-		}
-		else if (i == 4)
-		{
-			char *pstr;
-			struct point p;
-			struct coord c;
-			struct pcoord pc;
+		break;
+	case 5:
+		// call a command (like in gui)
+		s=(*env)->GetStringUTFChars(env, str, NULL);
+		dbg(0,"*****string=%s\n",s);
+		command_evaluate(&attr.u.navit->self,s);
+		(*env)->ReleaseStringUTFChars(env, str, s);
+		break;
+	case 4:
+	{
+		char *pstr;
+		struct point p;
+		struct coord c;
+		struct pcoord pc;
 
-			s=(*env)->GetStringUTFChars(env, str, NULL);
-			char parse_str[strlen(s) + 1];
-			strcpy(parse_str, s);
-			(*env)->ReleaseStringUTFChars(env, str, s);
-			dbg(0,"*****string=%s\n",parse_str);
+		s=(*env)->GetStringUTFChars(env, str, NULL);
+		char parse_str[strlen(s) + 1];
+		strcpy(parse_str, s);
+		(*env)->ReleaseStringUTFChars(env, str, s);
+		dbg(0,"*****string=%s\n",parse_str);
 
-			// set destination to (pixel-x#pixel-y)
-			// pixel-x
-			pstr = strtok (parse_str,"#");
-			p.x = atoi(pstr);
-			// pixel-y
-			pstr = strtok (NULL, "#");
-			p.y = atoi(pstr);
+		// set destination to (pixel-x#pixel-y)
+		// pixel-x
+		pstr = strtok (parse_str,"#");
+		p.x = atoi(pstr);
+		// pixel-y
+		pstr = strtok (NULL, "#");
+		p.y = atoi(pstr);
 
-			dbg(0,"11x=%d\n",p.x);
-			dbg(0,"11y=%d\n",p.y);
+		dbg(0,"11x=%d\n",p.x);
+		dbg(0,"11y=%d\n",p.y);
 
-			transform_reverse(global_navit->trans, &p, &c);
+		transform_reverse(global_navit->trans, &p, &c);
 
 
-			pc.x = c.x;
-			pc.y = c.y;
-			pc.pro = transform_get_projection(global_navit->trans);
+		pc.x = c.x;
+		pc.y = c.y;
+		pc.pro = transform_get_projection(global_navit->trans);
 
-			dbg(0,"22x=%d\n",pc.x);
-			dbg(0,"22y=%d\n",pc.y);
+		dbg(0,"22x=%d\n",pc.x);
+		dbg(0,"22y=%d\n",pc.y);
 
-			// start navigation asynchronous
-			navit_set_destination(global_navit, &pc, parse_str, 1);
-		}
-		else if (i == 3)
-		{
-			char *name;
-			s=(*env)->GetStringUTFChars(env, str, NULL);
-			char parse_str[strlen(s) + 1];
-			strcpy(parse_str, s);
-			(*env)->ReleaseStringUTFChars(env, str, s);
-			dbg(0,"*****string=%s\n",s);
+		// start navigation asynchronous
+		navit_set_destination(global_navit, &pc, parse_str, 1);
+	}
+	break;
+	case 3:
+	{
+		char *name;
+		s=(*env)->GetStringUTFChars(env, str, NULL);
+		char parse_str[strlen(s) + 1];
+		strcpy(parse_str, s);
+		(*env)->ReleaseStringUTFChars(env, str, s);
+		dbg(0,"*****string=%s\n",s);
 
-			// set destination to (lat#lon#title)
-			struct coord_geo g;
-			char *p;
-			char *stopstring;
+		// set destination to (lat#lon#title)
+		struct coord_geo g;
+		char *p;
+		char *stopstring;
 
-			// lat
-			p = strtok (parse_str,"#");
-			g.lat = strtof(p, &stopstring);
-			// lon
-			p = strtok (NULL, "#");
-			g.lng = strtof(p, &stopstring);
-			// description
-			name = strtok (NULL, "#");
+		// lat
+		p = strtok (parse_str,"#");
+		g.lat = strtof(p, &stopstring);
+		// lon
+		p = strtok (NULL, "#");
+		g.lng = strtof(p, &stopstring);
+		// description
+		name = strtok (NULL, "#");
 
-			dbg(0,"lat=%f\n",g.lat);
-			dbg(0,"lng=%f\n",g.lng);
-			dbg(0,"str1=%s\n",name);
+		dbg(0,"lat=%f\n",g.lat);
+		dbg(0,"lng=%f\n",g.lng);
+		dbg(0,"str1=%s\n",name);
 
-			struct coord c;
-			transform_from_geo(projection_mg, &g, &c);
+		struct coord c;
+		transform_from_geo(projection_mg, &g, &c);
 
-			struct pcoord pc;
-			pc.x=c.x;
-			pc.y=c.y;
-			pc.pro=projection_mg;
+		struct pcoord pc;
+		pc.x=c.x;
+		pc.y=c.y;
+		pc.pro=projection_mg;
 
-			// start navigation asynchronous
-			navit_set_destination(global_navit, &pc, name, 1);
+		// start navigation asynchronous
+		navit_set_destination(global_navit, &pc, name, 1);
 
-		}
+	}
+	break;
+	default:
+		dbg(0, "Unknown command");
 	}
 }
 
