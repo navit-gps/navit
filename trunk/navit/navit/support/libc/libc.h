ca2b1ceaba4ec4ea4a2981898bbe668b9c97c8f1
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-05-09 12:43:32 +0000

    Fix:support_libc:MSVC fixes

--- trunk/navit/navit/support/libc/libc.h
+++ trunk/navit/navit/support/libc/libc.h
@@ -1,9 +1,6 @@
 #ifndef _SUPPORT_LIBC_H
 #define _SUPPORT_LIBC_H       1
 
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE	1
-#endif
 
 void *popen(const char *command, const char *type);
 
@@ -20,5 +17,35 @@ char * setlocale ( int category, const char * locale );
 struct lconv *localeconv(void);
 
 int signal(int signum, void *handler);
+
+#ifdef _MSC_VER
+
+#define GetWindowLongPtr        GetWindowLong
+#define SetWindowLongPtr        SetWindowLong
+
+#define DWLP_MSGRESULT  0
+#define DWLP_DLGPROC    DWLP_MSGRESULT + sizeof(LRESULT)
+#define DWLP_USER       DWLP_DLGPROC + sizeof(DLGPROC)
+
+struct tm *localtime (const time_t *t);
+
+size_t strftime (char *s, size_t maxsize, const char *format, const struct tm *tp);
+
+#define mkdir       _mkdir
+#define open        _open
+#define close       _close
+#define read        _read
+#define write       _write
+#define lseek       _lseek
+#define vsnprintf   _vsnprintf
+
+#else
+
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE	1
+#endif
+
+#endif
+
 
 #endif
