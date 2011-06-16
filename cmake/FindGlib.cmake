6aa08217ff615c45f3de8157c3e1421316ef4f8f
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-06-16 22:05:51 +0000

    Fix:Build:Fix gtk builds on win32
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@4535 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- cmake/FindGlib.cmake
+++ cmake/FindGlib.cmake
@@ -13,21 +13,21 @@ libfind_pkg_check_modules(Glib_PKGCONF glib-2.0)
 # Main include dir
 find_path(Glib_INCLUDE_DIR
   NAMES glib.h
-  PATHS ${Glib_PKGCONF_INCLUDE_DIRS}
+  PATHS ${Glib_PKGCONF_INCLUDE_DIRS} /sw/include
   PATH_SUFFIXES glib-2.0
 )
 
 # Glib-related libraries also use a separate config header, which is in lib dir
 find_path(GlibConfig_INCLUDE_DIR
   NAMES glibconfig.h
-  PATHS ${Glib_PKGCONF_INCLUDE_DIRS} /usr
+  PATHS ${Glib_PKGCONF_INCLUDE_DIRS} /usr /sw
   PATH_SUFFIXES lib/glib-2.0/include
 )
 
 # Finally the library itself
 find_library(Glib_LIBRARY
   NAMES glib-2.0
-  PATHS ${Glib_PKGCONF_LIBRARY_DIRS}
+  PATHS ${Glib_PKGCONF_LIBRARY_DIRS} /sw/lib
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
