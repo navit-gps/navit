9a8f7f92561a81e909b09bddaa1d59819cf6c8bc
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-07-21 21:07:32 +0000

    Add:Core:Initial work for graphics_cocoa
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@4658 ffa7fe5e-494d-0410-b361-a75ebd5db220

---
+++ navit/graphics/cocoa/nick.h
@@ -0,0 +1,17 @@
+//
+//  nick.h
+//  HelloWorld
+//
+//  Created by Nick Geoghegan on 21/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface nick : NSView 
+{
+
+}
+
+@end
