2c22255321f22e770e53eefd86ad67e1cf03bea6
geoghegan <geoghegan@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-08-09 22:02:16 +0000

    Add:iPhone:Initial import of the map Downloader. These are, mainly, skeleton files needed by Xcode as well as fancy GUI elements

---
+++ trunk/navit/navit/iphone/downloader/Classes/Classes-1.moved-aside/DownloaderDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DownloaderDetailViewController.h
+//  Downloader
+//
+//  Created by Nick Geoghegan on 09/08/2011.
+//  Copyright 2011 Navit Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DownloaderDetailViewController : UITableViewController {
+
+}
+
+@end
