7eed97e61c08e37816dcd86556e691ccd73a9959
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-08-20 22:48:19 +0000

    Fix:cmake/build:remove double define

--- trunk/navit/config.h.cmake
+++ trunk/navit/config.h.cmake
@@ -14,7 +14,6 @@
 #cmakedefine AVOID_UNALIGNED 1
 #cmakedefine USE_LIBGNUINTL 1
 #cmakedefine HAVE_BYTESWAP_H 1
-#cmakedefine AVOID_FLOAT 1
 /* Versions */
 #cmakedefine PACKAGE_VERSION "@PACKAGE_VERSION@"
 #cmakedefine PACKAGE_NAME "@PACKAGE_NAME@"
