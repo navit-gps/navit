c4b9ee93392877fbe55d0cc751c5768878a9754e
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-09-05 11:03:44 +0000

    Add:Build:More flexibility for arm-mingw32ce toolchain

--- trunk/navit/Toolchain/arm-mingw32ce.cmake
+++ trunk/navit/Toolchain/arm-mingw32ce.cmake
@@ -1,8 +1,8 @@
 SET(CMAKE_SYSTEM_NAME WINCE)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER "arm-mingw32ce-gcc")
-SET(CMAKE_CXX_COMPILER "arm-mingw32ce-g++")
-SET(CMAKE_RC_COMPILER_INIT "arm-mingw32ce-windres")
+FIND_PROGRAM(CMAKE_C_COMPILER NAMES arm-mingw32ce-gcc arm-wince-mingw32ce-gcc)
+FIND_PROGRAM(CMAKE_CXX_COMPILER NAMES arm-mingw32ce-g++ arm-wince-mingw32ce-g++)
+FIND_PROGRAM(CMAKE_RC_COMPILER_INIT NAMES arm-mingw32ce-windres arm-wince-mingw32ce-windres)
 SET(PKG_CONFIG_EXECUTABLE "arm-mingw32ce-pkg-config")
 SET(WINCE TRUE)
