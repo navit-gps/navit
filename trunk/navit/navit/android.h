9b15d3fba3b98ef8d6c4a227a040cfe2e7fc1b24
rikky <rikky@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-09-06 19:25:39 +0000

    Fix:android:Small cleanup

--- trunk/navit/navit/android.h
+++ trunk/navit/navit/android.h
@@ -3,7 +3,6 @@
 #include <jni.h>
 extern JNIEnv *jnienv;
 extern jobject *android_activity;
-extern struct callback_list *android_activity_cbl;
 extern int android_version;
 int android_find_class_global(char *name, jclass *ret);
 int android_find_method(jclass class, char *name, char *args, jmethodID *ret);
