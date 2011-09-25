0007bb579bc7d563e90065d0597cc861813eda2e
korrosa <korrosa@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2011-09-25 18:06:13 +0000

    Add:Core:Add parking_aisle service ways to routing. Closes #840|Thanks ziaou!
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@4791 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/item.c
+++ navit/item.c
@@ -66,6 +66,7 @@ struct default_flags default_flags2[]={
 	{type_footway, AF_PBH},
 	{type_living_street, AF_ALL},
 	{type_street_service, AF_ALL},
+	{type_street_parking_lane, AF_ALL},
 	{type_bridleway, AF_PBH},
 	{type_path, AF_PBH},
 	{type_steps, AF_PBH},
