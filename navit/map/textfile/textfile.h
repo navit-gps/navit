2d622cdaee0d9447c7fb81747aefaac1b0dede87
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-01-01 22:40:23 +0000

    Fix:map_textfile:Rename SIZE define because it's hardly compatible with Win* platform.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@4883 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/map/textfile/textfile.h
+++ navit/map/textfile/textfile.h
@@ -32,19 +32,19 @@ struct map_priv {
 	int flags;
 };
 
-#define SIZE 512
+#define TEXTFILE_LINE_SIZE 512
 
 struct map_rect_priv {
 	struct map_selection *sel;
 
 	FILE *f;
 	long pos;
-	char line[SIZE];
+	char line[TEXTFILE_LINE_SIZE];
 	int attr_pos;
 	enum attr_type attr_last;
-	char attrs[SIZE];
-	char attr[SIZE];
-	char attr_name[SIZE];
+	char attrs[TEXTFILE_LINE_SIZE];
+	char attr[TEXTFILE_LINE_SIZE];
+	char attr_name[TEXTFILE_LINE_SIZE];
 	struct coord c;
 	int eoc;
 	int more;
