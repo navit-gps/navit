981de1bf9403f5b54dbf017c6138bf64dfafb98b
sleske <sleske@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-05-13 18:21:14 +0000

    Fix:maps:Make build fail cleanly if sample map download fails, but tell user how to skip it.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@5104 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- cmake/navit_download.cmake
+++ cmake/navit_download.cmake
@@ -3,6 +3,8 @@ file(DOWNLOAD ${URL} ${DST} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
 list(GET DOWNLOAD_STATUS 0 DOWNLOAD_ERR)
 list(GET DOWNLOAD_STATUS 1 DOWNLOAD_MSG)
 if(DOWNLOAD_ERR)
-  message(WARNING "Download of sample map from ${URL} failed: ${DOWNLOAD_MSG}.")
   file(REMOVE ${DST})
+  message(SEND_ERROR "Download of sample map from ${URL} failed: "
+  "${DOWNLOAD_MSG}\n"
+  "To disable the sample map, run cmake with -DSAMPLE_MAP=n .")
 endif(DOWNLOAD_ERR)
