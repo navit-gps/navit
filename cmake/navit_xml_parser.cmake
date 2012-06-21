78e9feea1327c21d46ae96a33437cdb73c532b9e
sleske <sleske@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-06-21 18:51:48 +0000

    Fix:build:Remove unnecessary output during build; add comment.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@5159 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- cmake/navit_xml_parser.cmake
+++ cmake/navit_xml_parser.cmake
@@ -2,7 +2,6 @@ file(STRINGS "${SRC}" TEXT_LINES REGEX "_\\(")
 file(WRITE ${DST} "// Strings from navit_shipped.xml\n")
 
 foreach (LINE ${TEXT_LINES})
-   message(STATUS ${LINE})
    string(REGEX REPLACE ".*(_\\(\"[^\"]*\"\\)).*" "\\1\n" OUTPUT_LINE ${LINE})
    file(APPEND ${DST} ${OUTPUT_LINE})
 endforeach()
