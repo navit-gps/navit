99d8b76b005f12187d0163c7858e17ed70372efb
worldcitizen <worldcitizen@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-10-01 16:38:09 +0000

    Fixed: initialization makes pointer from integer without a cast
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@5242 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/bookmarks.c
+++ navit/bookmarks.c
@@ -37,7 +37,6 @@
 #include "navit.h"
 #include "navit_nls.h"
 #include "util.h"
-#include "attr.h"
 
 /* FIXME: Move this to support directory */
 #ifdef _MSC_VER
@@ -260,7 +259,7 @@ bookmarks_new(struct attr *parent, struct attr **attrs, struct transformation *t
 		//Load map now
 		struct attr type={attr_type, {"textfile"}},
 			    data={attr_data, {this_->bookmark_file}},
-			    no_warn={attr_no_warning_if_map_file_missing, {1}};
+			    no_warn={attr_no_warning_if_map_file_missing, {(void *)1}};
 		struct attr *attrs[]={&type, &data, &no_warn, NULL};
 		this_->bookmark=map_new(this_->parent, attrs);
 		if (!this_->bookmark)
