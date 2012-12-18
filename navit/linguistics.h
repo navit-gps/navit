538554ed0d340877f9087c9ef86f9aebc3dac157
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-12-18 20:20:33 +0000

    Add:core:Cyrillic search improvements: Do accent insensitive search for Cyrillic in town and POI filter. Do Cyrilic case insensitive filtering of POIs.

--- navit/linguistics.h
+++ navit/linguistics.h
@@ -4,6 +4,8 @@ extern "C" {
 char *linguistics_expand_special(char *str, int mode);
 char *linguistics_next_word(char *str);
 void linguistics_init(void);
+void linguistics_free(void);
+char *linguistics_casefold(char *in);
 #ifdef __cplusplus
 }
 #endif
