92c24ee0ffdb64b3ddb4a30cc478cba8d6a9b7f7
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2012-12-26 16:04:59 +0000

    Fix:core:Don't crash on Android when search hits non-latin-1 symbols. Fixes #1086 and its duplicate #1088.

--- navit/start_real.c
+++ navit/start_real.c
@@ -226,9 +226,11 @@ int main_real(int argc, const char **argv)
 	}
 	event_main_loop_run();
 
-	linguistics_free();
-
+	/* TODO: Android actually has no event loop, so we can't free all allocated resources here. Have to find better place to
+	 *  free all allocations on program exit. And don't forget to free all the stuff allocated in the code above.
+	 */
 #ifndef HAVE_API_ANDROID
+	linguistics_free();
 	debug_finished();
 #endif
 	return 0;
