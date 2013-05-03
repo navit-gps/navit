dcf9509a77ec9f7c9c970b4f4c1d770831975645
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2013-05-03 21:05:37 +0000

    Add:core:New function for case- and accent- insensitive string comparison.

--- navit/linguistics.h
+++ navit/linguistics.h
@@ -1,15 +1,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-char *linguistics_expand_special(char *str, int mode);
+char *linguistics_expand_special(const char *str, int mode);
 char *linguistics_next_word(char *str);
 void linguistics_init(void);
 void linguistics_free(void);
-char *linguistics_casefold(char *in);
+char *linguistics_casefold(const char *in);
+int linguistics_search(const char *str);
+enum linguistics_cmp_mode {
+	linguistics_cmp_expand=1,
+	linguistics_cmp_partial=2,
+	linguistics_cmp_words=4
+};
+int linguistics_compare(const char *s1, const char *s2, enum linguistics_cmp_mode mode);
 #ifdef __cplusplus
 }
 #endif
 
-/* Prototypes */
-int linguistics_search(char *str);
 
