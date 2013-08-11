59ece87dfdb898536673d1b8293e628065cb043a
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2013-08-11 16:00:27 +0000

    Add:port_android:Backup / Restore Function for Android Devices, see #1148|thank you, kamikaaze!

--- trunk/navit/navit/android/src/org/navitproject/navit/NavitDialogs.java
+++ trunk/navit/navit/android/src/org/navitproject/navit/NavitDialogs.java
@@ -1,19 +1,26 @@
 package org.navitproject.navit;
 
 
+import java.io.File;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import android.os.Environment;
 
 public class NavitDialogs extends Handler{
 	// Dialogs
 	public static final int           DIALOG_MAPDOWNLOAD               = 1;
-
+	public static final int           DIALOG_BACKUP_RESTORE            = 2;
+	public static final int           DIALOG_SELECT_BACKUP             = 3;
+	
 	// dialog messages
 	static final int MSG_MAP_DOWNLOAD_FINISHED   = 0;
 	static final int MSG_PROGRESS_BAR          = 1;
@@ -107,6 +114,8 @@ public class NavitDialogs extends Handler{
 
 	Dialog createDialog(int id)
 	{
+	    AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+	    
 		switch (id)
 		{
 			case DIALOG_MAPDOWNLOAD :
@@ -131,8 +140,68 @@ public class NavitDialogs extends Handler{
 						Navit.get_text("Map data (c) OpenStreetMap contributors, CC-BY-SA"),
 						Toast.LENGTH_LONG).show(); //TRANS
 				return mapdownloader_dialog;
+				
+			case DIALOG_BACKUP_RESTORE :
+			    /* Create a Dialog that Displays Options wether to Backup or Restore */
+			    builder.setTitle(mActivity.getString(R.string.choose_an_action)).
+			        setCancelable(true).
+			        setItems(R.array.dialog_backup_restore_items,
+			                new DialogInterface.OnClickListener() {
+			            
+			            @Override
+			            public void onClick(DialogInterface dialog, int which) {
+			                /* Notify User if no SD Card present */
+			                if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
+			                    Toast.makeText(mActivity,mActivity.getString(R.string.please_insert_an_sd_card), Toast.LENGTH_LONG).show();
+			                
+			                switch (which) {
+			                    case 0:
+			                        /* Backup */
+			                        new NavitBackupTask(mActivity).execute();
+			                        break;
+			                    case 1:
+			                        /* Restore */
+			                        mActivity.showDialog(DIALOG_SELECT_BACKUP);
+			                        break;
+			                }
+			            }});
+			            return builder.create();
+			            
+			case DIALOG_SELECT_BACKUP :
+			    /* Create a Dialog with a list from which the user selects the Backup to be restored */
+			    File mainBackupDir = new File(Environment.getExternalStorageDirectory().getPath() + "/navit/backup/");
+			    
+			    String[] backups = null;
+			    if(mainBackupDir.isDirectory())
+			        backups = mainBackupDir.list();
+
+			    if(backups == null || backups.length == 0) {
+			        /* No Backups were found */
+			        builder.setTitle(mActivity.getText(R.string.no_backup_found));
+			        builder.setNegativeButton(mActivity.getText(android.R.string.cancel), null);
+			        return builder.create();
+			    }
+			    
+			    builder.setTitle(mActivity.getString(R.string.select_backup));
+			    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_spinner_item, backups);			    
+			    builder.setAdapter(adapter, new OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new NavitRestoreTask(mActivity, adapter.getItem(which)).execute();
+                    }});
+                builder.setNegativeButton(mActivity.getString(android.R.string.cancel), null);
+
+                return builder.create();
 		}
 		// should never get here!!
 		return null;
 	}
+
+    public void prepareDialog(int id, Dialog dialog) {
+        
+        /* Remove the Dialog to force Android to rerun onCreateDialog */
+        if(id == DIALOG_SELECT_BACKUP)
+            mActivity.removeDialog(id);
+    }
 }
