c0b07fc45b32db66195c67e88dd59b4b02888c56
sleske <sleske@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2013-08-13 22:00:32 +0000

    Fix:core:Partially revert rev. 5571 to fix builds which use support/glib.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@5576 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/xmlconfig.h
+++ navit/xmlconfig.h
@@ -83,7 +83,7 @@ extern const int xml_attr_distance;
 /* prototypes */
 enum attr_type;
 struct object_func *object_func_lookup(enum attr_type type);
-void xml_parse_text(const char *document, void *data, void (*start)(GMarkupParseContext *, const char *, const char **, const char **, void *, GError **), void (*end)(GMarkupParseContext *, const char *, void *, GError **), void (*text)(GMarkupParseContext*, const char *, gsize, void *, GError **));
+void xml_parse_text(const char *document, void *data, void (*start)(void *, const char *, const char **, const char **, void *, void *), void (*end)(void *, const char *, void *, void *), void (*text)(void *, const char *, int, void *, void *));
 gboolean config_load(const char *filename, xmlerror **error);
 //static void xinclude(GMarkupParseContext *context, const gchar **attribute_names, const gchar **attribute_values, struct xmldocument *doc_old, xmlerror **error);
 
