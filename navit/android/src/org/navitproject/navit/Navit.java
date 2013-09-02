03acb28e742498c6addbf9fabf35fe2105221f51
mdankov <mdankov@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2013-09-02 18:59:48 +0000

    Add:port_android:Let notification area messages be translated.

--- navit/android/src/org/navitproject/navit/Navit.java
+++ navit/android/src/org/navitproject/navit/Navit.java
@@ -259,9 +259,9 @@ public class Navit extends Activity
 		// Setup the status bar notification		
 		// This notification is removed in the exit() function
 		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);	// Grab a handle to the NotificationManager
-		Notification NavitNotification = new Notification(R.drawable.icon,"Navit started",System.currentTimeMillis());	// Create a new notification, with the text string to show when the notification first appears
+		Notification NavitNotification = new Notification(R.drawable.icon, getString(R.string.notification_ticker), System.currentTimeMillis());	// Create a new notification, with the text string to show when the notification first appears
 		PendingIntent appIntent = PendingIntent.getActivity(getApplicationContext(), 0, getIntent(), 0);
-		NavitNotification.setLatestEventInfo(getApplicationContext(), "Navit", "Navit running", appIntent);	// Set the text in the notification
+		NavitNotification.setLatestEventInfo(getApplicationContext(), "Navit", getString(R.string.notification_event_default), appIntent);	// Set the text in the notification
 		NavitNotification.flags|=Notification.FLAG_ONGOING_EVENT;	// Ensure that the notification appears in Ongoing
 		nm.notify(R.string.app_name, NavitNotification);	// Set the notification
 		
