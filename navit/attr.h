51c1157cd4c314581febfb7169d84cc41ab6f3d6
sleske <sleske@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2014-07-20 11:53:08 +0000

    Refactor:core:Mark unused attribute types.
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@5822 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/attr.h
+++ navit/attr.h
@@ -28,10 +28,25 @@ extern "C" {
 
 enum item_type;
 
+/**
+ * Attribute type values, created using macro magic.
+ */
 enum attr_type {
 #define ATTR2(x,y) attr_##y=x,
 #define ATTR(x) attr_##x,
+
+/* Special macro for unused attribute types. Creates a placeholder entry
+ * in the enum so the following values do not change. */
+#define ATTR_UNUSED ATTR_UNUSED_L(__LINE__)
+#define ATTR_UNUSED_L(x) ATTR_UNUSED_WITH_LINE_NUMBER(x)
+#define ATTR_UNUSED_WITH_LINE_NUMBER(x) ATTR_UNUSED_##x,
+
 #include "attr_def.h"
+
+#undef ATTR_UNUSED_WITH_LINE_NUMBER
+#undef ATTR_UNUSED_L
+#undef ATTR_UNUSED
+
 #undef ATTR2
 #undef ATTR
 };
