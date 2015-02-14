440628a9eda98d8735c084c311306dc7155a77ae
Pierre GRANDIN <grandinp@altern.org>
2015-02-14 10:24:02 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export PATH=~/navit/cov-analysis-linux64-7.6.0/bin:$PATH
 
 mkdir bin && cd bin
 cov-build --dir cov-int cmake ../
-cov-build --dir cov-int make
+cov-build --dir cov-int make -j32
 tar czvf navit.tgz cov-int
