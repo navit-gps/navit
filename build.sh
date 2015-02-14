45c706a9df0b7266db2d8e7f4696f359ebe1e429
Pierre GRANDIN <grandinp@altern.org>
2015-02-14 11:54:53 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ mkdir bin && cd bin
 cov-build --dir cov-int cmake ../
 cov-build --dir cov-int make -j32
 tar czvf navit.tgz cov-int
+
+curl --form token=$COVERITY_TOKEN \
+  --form email=$COVERITY_EMAIL \
+  --form file=@navit.tgz \
+  --form version="$CIRCLE_SHA1" \
+  --form description="$CIRCLE_SHA1" \
+  https://scan.coverity.com/builds?project=navit-gps
