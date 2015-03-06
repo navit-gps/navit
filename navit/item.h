d3b35da3b27f251414b7943936032fbc0ad3bcf6
martin-s <martin-s@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2015-03-06 08:09:39 +0000

    Add:Core:More custom icons for those who used all up
    git-svn-id: http://svn.code.sf.net/p/navit/code/trunk/navit@6021 ffa7fe5e-494d-0410-b361-a75ebd5db220

--- navit/item.h
+++ navit/item.h
@@ -52,7 +52,7 @@ extern int default_flags[];
 #define item_is_district(item) ((item).type >= type_district_label && (item).type <= type_district_label_1e7)
 #define item_is_poly_place(item) ((item).type >= type_poly_place1 && (item).type <= type_poly_place6)
 #define item_is_point(item) ((item).type < type_line) 
-#define item_is_custom_poi(item) ((item).type >= type_poi_custom0 && (item).type < type_line)
+#define item_is_custom_poi(item) ((item).type >= type_poi_customg && (item).type < type_line)
 #define item_is_street(item) (((item).type >= type_street_0 && (item).type < type_street_1_land) \
                                ||  (item).type == type_street_pedestrian \
                                ||  (item).type == type_living_street  \
