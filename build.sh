b05c35fdc7fdff1e7a37e94b27661c92b3827957
Pierre GRANDIN <grandinp@altern.org>
2015-03-25 15:10:55 -0700

    Adding CI scripts

---
+++ build.sh
@@ -0,0 +1,19 @@
+if [ -e ~/assets/cov-analysis-linux64-7.6.0.tar.gz ]; then 
+  tar xfz ~/assets/cov-analysis-linux64-7.6.0.tar.gz
+  export PATH=~/navit/cov-analysis-linux64-7.6.0/bin:$PATH
+  mkdir bin && cd bin
+  cov-build --dir cov-int cmake ../
+  cov-build --dir cov-int make -j32 || exit 1
+  tar czvf navit.tgz cov-int
+
+  curl --form token=$COVERITY_TOKEN \
+  --form email=$COVERITY_EMAIL \
+  --form file=@navit.tgz \
+  --form version="$CIRCLE_SHA1" \
+  --form description="$CIRCLE_SHA1" \
+  https://scan.coverity.com/builds?project=navit-gps
+else
+  mkdir bin && cd bin
+  cmake ../
+  make -j32 || exit 1
+fi
