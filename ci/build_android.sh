b77e33373ebcea0fc7c92f59308b119e118da753
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2015-08-06 09:33:01 -0700

    Workaround for git-based only builds

--- ci/build_android.sh
+++ ci/build_android.sh
@@ -131,7 +131,7 @@ cd $BUILD_PATH
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_TOOLS:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
 android list targets
 svn_rev=` cd ~/navit/; git log -1|grep git-svn-id:|cut -c 65-68`
-if [[ "$svn_rev" == "" ]]; then svn_rev="0"; fi
+if [[ "$svn_rev" == "" ]]; then svn_rev="6136"; fi # Workaround for git-only builds
 sed -i -e "s/ANDROID_VERSION_INT=\"0\"/ANDROID_VERSION_INT=\"${svn_rev}\"/g" ~/navit/navit/android/CMakeLists.txt
 cp ~/navit/navit/android/CMakeLists.txt $CIRCLE_ARTIFACTS/
 
