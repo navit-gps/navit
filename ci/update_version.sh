6ea07f242cece8fb75f127fab732e934187b94fb
mdankov <mdankov@users.noreply.github.com>
2015-09-07 20:46:07 +0300

    Fix quatation marks in update_version.sh

--- ci/update_version.sh
+++ ci/update_version.sh
@@ -24,10 +24,9 @@ if [ "$1" == "prepare" ] ; then
 fi
 
 if [ "$1" == "push" ] ; then
-  git log -1 --format="%H %d" | grep 'tag: $TAG' 
+  git log -1 --format="%H %d" | grep "tag: $TAG"
   if [ $? -eq 0 ] ; then
     echo Pushing tag $TAG to origin...
     git push origin $TAG
   fi
 fi
-
