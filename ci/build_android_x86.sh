ca74f662e79691b9f572bd74b55c35af30ee0d35
jandegr <jandegr@users.noreply.github.com>
2015-12-31 13:05:25 +0100

    Update build_android_x86.sh

--- ci/build_android_x86.sh
+++ ci/build_android_x86.sh
@@ -1,3 +1,3 @@
 mkdir android-x86 && cd android-x86
-cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain/i686-android.cmake -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ../ || exit -1
+cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain/i686-android.cmake -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ../ || exit -1
 make || exit -1
