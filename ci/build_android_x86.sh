77e7978867976aabd2f3fe4ca93f60754a8f2cdd
jandegr <jandegr@users.noreply.github.com>
2016-01-01 19:19:25 +0100

    Update build_android_x86.sh

--- ci/build_android_x86.sh
+++ ci/build_android_x86.sh
@@ -8,5 +8,20 @@ export ANDROID_SDK_PLATFORM_TOOLS=$ANDROID_SDK"/platform-tools"
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
 
 mkdir android-x86 && cd android-x86
+
+android list targets
+
 cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_FILE -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ../ || exit -1
 make || exit -1
+
+if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
+  make apkg-release && mv navit/android/bin/Navit-release-unsigned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-x86-release-unsigned.apk || exit 1
+else
+  make apkg && mv navit/android/bin/Navit-debug.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-x86-debug.apk || exit 1
+fi
+
+#cp ~/android-build/navit/*.xml $CIRCLE_ARTIFACTS/android/
+
+echo
+echo "Build leftovers :"
+ls navit/android/bin/
