5ebf9fa113d39f5a33d595fcee34e8761eda123f
mvglasow <michael -at- vonglasow.com>
2016-01-24 16:14:05 +0100

    Refactor:gui_internal:Determine menu widget size based on parent size
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- navit/gui/internal/gui_internal_menu.c
+++ navit/gui/internal/gui_internal_menu.c
@@ -117,8 +117,8 @@ gui_internal_menu(struct gui_priv *this, const char *label)
         topbox->h=this->root.h;
         gui_internal_widget_append(&this->root, topbox);
 	menu=gui_internal_box_new(this, gravity_left_center|orientation_vertical);
-	menu->w=this->root.w;
-	menu->h=this->root.h;
+	menu->w=topbox->w;
+	menu->h=topbox->h;
 	menu->background=this->background;
 	gui_internal_apply_config(this);
  	topbox->menu_data=g_new0(struct menu_data, 1);
@@ -149,8 +149,8 @@ gui_internal_menu(struct gui_priv *this, const char *label)
 	}
 	if (this->flags & 192) {
 		menu=gui_internal_box_new(this, gravity_left_center|orientation_vertical);
-		menu->w=this->root.w;
-		menu->h=this->root.h;
+		menu->w=topbox->w;
+		menu->h=topbox->h;
 		w1=gui_internal_time_help(this);
 		gui_internal_widget_append(menu, w1);
 		w1=gui_internal_box_new(this, gravity_center|orientation_horizontal_vertical|flags_expand|flags_fill);
@@ -162,8 +162,8 @@ gui_internal_menu(struct gui_priv *this, const char *label)
 	gui_internal_widget_reset_pack(this, topbox);
         topbox->w=this->root.w;
         topbox->h=this->root.h;
-	menu->w=this->root.w;
-	menu->h=this->root.h;
+	menu->w=topbox->w;
+	menu->h=topbox->h;
 	return w;
 }
 
