a7e2f1d4ace56e5cd2c4b9d0a60bf9cbefe27d1d
pohlinkzei <pohlinkzei@gmx-topmail.de>
2016-02-27 12:54:37 +0100

    Add wiringpi search for switch layout

---
+++ cmake/FindWiringPi.cmake
@@ -0,0 +1,8 @@
+include(LibFindMacros)
+
+find_library(WIRINGPI_LIBRARIES NAMES wiringPi)
+find_path(WIRINGPI_INCLUDE_DIRS NAMES wiringPi.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(wiringPi DEFAULT_MSG WIRINGPI_LIBRARIES WIRINGPI_INCLUDE_DIRS)
+
