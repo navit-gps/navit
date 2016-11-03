759a24a5937fb2ac38bc47fad7a225f19f37ce67
jandegr <jandegr@users.noreply.github.com>
2016-11-03 19:37:54 +0100

    Update build_android_x86.sh

--- ci/build_android_x86.sh
+++ ci/build_android_x86.sh
@@ -1,7 +1,7 @@
 export START_PATH=~/
 export SOURCE_PATH=$START_PATH"/"${CIRCLE_PROJECT_REPONAME}"/"
 export CMAKE_FILE=$SOURCE_PATH"/Toolchain/i686-android.cmake"
-export ANDROID_NDK="/usr/local/android-ndk/"
+export ANDROID_NDK=~/android-ndk-r13
 export ANDROID_NDK_BIN=$ANDROID_NDK"/toolchains/x86-4.9/prebuilt/linux-x86_64/bin"
 export ANDROID_SDK="/usr/local/android-sdk-linux/"
 export ANDROID_SDK_PLATFORM_TOOLS=$ANDROID_SDK"/platform-tools"
@@ -11,7 +11,7 @@ mkdir android-x86 && cd android-x86
 
 android list targets
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_FILE -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=21 -DANDROID_NDK_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ../ || exit -1
+cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_FILE -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=23 -DANDROID_NDK_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ../ || exit -1
 make || exit -1
 
 if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
