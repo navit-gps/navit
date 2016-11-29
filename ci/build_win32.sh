60918fcdd2ce03233cf6c66dfd669f260f8b14c5
Pierre Grandin <grandinp@altern.org>
2016-11-29 23:44:57 +0000

    Enabling build parallelization

---
+++ ci/build_win32.sh
@@ -0,0 +1,6 @@
+mkdir win32
+pushd win32
+cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw32.cmake ../ && make && make package
+popd
+
+cp win32/*.exe $CIRCLE_ARTIFACTS/
