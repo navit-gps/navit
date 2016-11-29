25fd4d4f0607677f221397912b7eec6e17602925
Pierre Grandin <grandinp@altern.org>
2016-11-29 22:42:23 +0000

    Moving linux dependencies installation to the build script

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -1,3 +1,5 @@
+sudo apt-get install cmake libpng12-dev librsvg2-bin libfreetype6-dev libdbus-glib-1-dev g++ libgtk2.0-dev
+
 if [[ "${CIRCLE_PROJECT_USERNAME}" == "navit-gps" && "${CIRCLE_BRANCH}" == "trunk" ]]; then
 	tar xfz ~/assets/cov-analysis-linux64-7.6.0.tar.gz
 	export PATH=~/navit/cov-analysis-linux64-7.6.0/bin:$PATH
