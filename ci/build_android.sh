36498985cb459aa2287a5b055ab3835aa72d24c1
Pierre Grandin <grandinp@altern.org>
2016-12-30 15:27:43 -0800

    Fixed xml artifacts path in build scripts

--- ci/build_android.sh
+++ ci/build_android.sh
@@ -29,7 +29,7 @@ else
   make apkg && mv $BUILD_PATH/navit/android/bin/Navit-debug.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-debug.apk || exit 1
 fi
 
-cp $BUILD_PATH/navit/android/bin/navit/*.xml $CIRCLE_ARTIFACTS/android/
+cp $BUILD_PATH/navit/*.xml $CIRCLE_ARTIFACTS/android-${ARCH}/
 
 echo
 echo "Build leftovers :"
