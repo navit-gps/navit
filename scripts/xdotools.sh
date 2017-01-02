61b76bba51c7fe53e6fa424ff3a8f5338fb2818a
Pierre Grandin <grandinp@altern.org>
2017-01-02 15:03:25 -0800

    Capture image when clicking for UI tests

--- scripts/xdotools.sh
+++ scripts/xdotools.sh
@@ -6,18 +6,19 @@ sudo apt-get install xdotool
 event=0
 
 send_event (){
-    xdotool $@
-    sleep 1
     file=`printf "%05d\n" $event`
 
     import -window root $CIRCLE_ARTIFACTS/frames/tmp.png
     if [[ "$1" == "mousemove" ]]; then
-    	composite -geometry +$2+$3 ~/navit/ci/pointer-64.png $CIRCLE_ARTIFACTS/frames/tmp.png $CIRCLE_ARTIFACTS/frames/${file}.png
+        composite -gravity NorthWest -geometry +$2+$3 ~/navit/ci/pointer-64.png $CIRCLE_ARTIFACTS/frames/tmp.png $CIRCLE_ARTIFACTS/frames/${file}.png
     else
-	mv $CIRCLE_ARTIFACTS/frames/tmp.png $CIRCLE_ARTIFACTS/frames/${file}.png
+        mv $CIRCLE_ARTIFACTS/frames/tmp.png $CIRCLE_ARTIFACTS/frames/${file}.png
     fi
     event=$((event+1))
+    xdotool $@
+    sleep 1
 }
+
 
 # Center the view
 send_event mousemove 450 475 click 1 # Open main menu
@@ -35,7 +36,7 @@ send_event mousemove 150 280 click 1 # Click 'view on map'
 send_event mousemove 482 318 click 1 # Open main menu, clicking on a somewhat random position on the map
 send_event mousemove 450 475 click 1 # Click 'Actions'
 send_event mousemove 850 430 click 1 # Click 'Coordinates'
-send_event mousemove 180 150 click 1 # Click 'Set as position'
+send_event mousemove 180 120 click 1 # Click 'Set as position'
 
 # Set a destination
 send_event mousemove 450 475 click 1 # Open main menu
@@ -54,6 +55,9 @@ send_event mousemove 640 450 click 1 # Open main menu
 send_event mousemove 640 450 click 1 # Click 'Settings'
 send_event mousemove 475 450 click 1 # Click 'Display'
 send_event mousemove 870 450 click 1 # Click '3D'
+
+# capture the last image
+import -window root $CIRCLE_ARTIFACTS/frames/99999.png
 
 # Assemble the gif
 convert   -delay 100 -loop 0 $CIRCLE_ARTIFACTS/frames/*.png $CIRCLE_ARTIFACTS/town_search.gif
