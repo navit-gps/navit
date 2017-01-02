454b9abf08607987faf11e9ca000a296f15c1a33
Pierre Grandin <grandinp@altern.org>
2017-01-02 14:28:30 -0800

    Set a destination for UI tests

--- scripts/xdotools.sh
+++ scripts/xdotools.sh
@@ -1,4 +1,5 @@
 sudo apt-get install xdotool
+# Use xinput test 4 when running x11vnc on the circleci server to find mouse coordinates
 
 [ -d $CIRCLE_ARTIFACTS/frames/ ] || mkdir $CIRCLE_ARTIFACTS/frames/
 
@@ -18,26 +19,49 @@ send_event (){
     event=$((event+1))
 }
 
+# Center the view
 # Open main menu
-send_event mousemove 350 350 click 1
-
+send_event mousemove 450 475 click 1
 # Click 'Actions'
-send_event mousemove 350 350 click 1
-
+send_event mousemove 450 475 click 1
 # Click 'Town'
-send_event mousemove 300 500 click 1
-
+send_event mousemove 420 600 click 1
 # Send 'Berk'
 send_event key b
 send_event key e
 send_event key r
 send_event key k
-
 # Click on the first result
-send_event mousemove 150 100 click 1
-
+send_event mousemove 100 100 click 1
 # Click 'view on map'
 send_event mousemove 150 280 click 1
+
+# Set the position
+# Open main menu, clicking on a somewhat random position on the map
+send_event mousemove 482 318 click 1
+# Click 'Actions'
+send_event mousemove 450 475 click 1
+# Click 'Coordinates'
+send_event mousemove 850 430 click 1
+# Click 'Set as position'
+send_event mousemove 180 150 click 1
+
+# Set a destination
+# Open main menu
+send_event mousemove 450 475 click 1
+# Click 'Actions'
+send_event mousemove 450 475 click 1
+# Click 'Town'
+send_event mousemove 420 600 click 1
+# Send 'Berk'
+send_event key o
+send_event key a
+send_event key k
+send_event key l
+# Click on the first result
+send_event mousemove 100 100 click 1
+# Click 'set as destination'
+send_event mousemove 150 150 click 1
 
 # Assemble the gif
 convert   -delay 100 -loop 0 $CIRCLE_ARTIFACTS/frames/*.png $CIRCLE_ARTIFACTS/town_search.gif
