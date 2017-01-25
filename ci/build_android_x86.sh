94a3c994a7a5970be3b3bb9d290f9e1d8d437034
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2017-01-25 10:42:08 -0800

    Android patches from Jandegr (#202)
    * ndk downgrade
    
    ndk downgrade
    
    * ndk downgrade
    
    * Update build_android.sh
    
    * Update build_android_x86.sh
    
    * Update build_android.sh
    
    * Update build_android_x86.sh
    
    * Update build_android_x86.sh
    
    * Update build_android.sh
    
    * Update NavitAppConfig.java
    
    * Add files via upload
    
    * Delete acra-4.3.0b2.jar
    
    * Update NavitAppConfig.java

--- ci/build_android_x86.sh
+++ ci/build_android_x86.sh
@@ -5,15 +5,15 @@ export arch="x86"
 export START_PATH=~/
 export SOURCE_PATH=$START_PATH"/"${CIRCLE_PROJECT_REPONAME}"/"
 export CMAKE_FILE=$SOURCE_PATH"/Toolchain/i686-android.cmake"
-export ANDROID_NDK=~/android-ndk-r13
+export ANDROID_NDK=~/android-ndk-r11c
 export ANDROID_NDK_BIN=$ANDROID_NDK"/toolchains/x86-4.9/prebuilt/linux-x86_64/bin"
 export ANDROID_SDK="/usr/local/android-sdk-linux/"
 export ANDROID_SDK_PLATFORM_TOOLS=$ANDROID_SDK"/platform-tools"
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
 export BUILD_PATH=$START_PATH"/android-${ARCH}"
 
-wget -nv -c http://dl.google.com/android/repository/android-ndk-r13-linux-x86_64.zip
-[ -d ~/android-ndk-r13 ] || unzip -q -d ~ android-ndk-r13-linux-x86_64.zip
+wget -nv -c http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
+[ -d ~/android-ndk-r11c ] || unzip -q -d ~ android-ndk-r11c-linux-x86_64.zip
 
 [ -d $BUILD_PATH ] || mkdir -p $BUILD_PATH
 cd $BUILD_PATH
