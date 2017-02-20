e80c3634788ac4f5440a5e92178488a5beb9438a
Pierre Grandin <grandinp@altern.org>
2017-02-20 06:35:53 -0800

    Added automatic publishing to Playstore Beta

---
+++ ci/publish.sh
@@ -0,0 +1,10 @@
+cd ~/
+git clone git@github.com:navit-gps/infrastructure-blackbox.git
+cd infrastructure-blackbox/
+openssl aes-256-cbc -d -in keystore.gpg -k $KEY > ~/.keystore
+openssl aes-256-cbc -d -in clientsecret.gpg -k $KEY > client_secrets.json
+openssl aes-256-cbc -d -in androidpublisher.gpg -k $KEY > androidpublisher.dat
+
+jarsigner -storepass $SP $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $key_name
+/usr/local/android-sdk-linux/build-tools/25.0.1/zipalign 4 $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
+python ~/navit/ci/basic_upload_apks.py org.navitproject.navit $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
