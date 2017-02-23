0ba8f875914f91dae76d83feaa37afdaf72c49a7
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2017-02-23 07:12:57 -0800

    Add:CI:Add basic UI tests (#197)
    * Ensures that navit has time to start before starting tests
    
    * Adding basic UI town search tests
    
    * Add pointer icon when clicking via CI
    
    * Fetch zlib from github, reduces download issues
    
    * Download a sample map for UI tests
    
    * Start navit in the background
    
    * Fixed Demo vehicle activation
    
    * Set a destination for UI tests
    
    * UI tests to switch to 3d view
    
    * Capture image when clicking for UI tests
    
    * Append 5 frames of usage to the UI tests
    
    * Use keyboard instead of mouse emulation for UI tests
    
    * Temporarily disabling dbus test because of issues in circleCI

--- ci/run_linux_tests.sh
+++ ci/run_linux_tests.sh
@@ -4,15 +4,26 @@ set -e
 
 pushd ~/linux-bin/navit/
 sed -i -e 's@name="Local GPS" profilename="car" enabled="yes" active="1"@name="Local GPS" profilename="car" enabled="no" active="0"@' navit.xml
-sed -i -e 's@name="Demo" profilename="car" enabled="no" active="yes"@name="Demo" profilename="car" enabled="yes" active="yes" follow="1" refresh="1"@' navit.xml
+sed -i -e 's@name="Demo" profilename="car" enabled="no" @name="Demo" profilename="car" enabled="yes" follow="1" refresh="1"@' navit.xml
 sed -i -e 's@type="internal" enabled@type="internal" fullscreen="1" font_size="350" enabled@' navit.xml
 sed -i -e 's@libbinding_dbus.so" active="no"@libbinding_dbus.so" active="yes"@' navit.xml
 
-./navit &
+cat > maps/berkeley.xml << EOF
+<map type="binfile" data="\$NAVIT_SHAREDIR/maps/berkeley.bin" />
+EOF
+wget http://sd-55475.dedibox.fr/berkeley.bin -O maps/berkeley.bin
+
+mkdir $CIRCLE_ARTIFACTS/logs
+
+./navit >$CIRCLE_ARTIFACTS/logs/stdout.txt 2>$CIRCLE_ARTIFACTS/logs/stderr.txt &
 pid=$!
+
+sleep 5
 
 import -window root $CIRCLE_ARTIFACTS/default.png
 
+bash ~/navit/ci/xdotools.sh
+
 # python ~/navit/ci/dbus_tests.py $CIRCLE_TEST_REPORTS/
-# 
+
 # dbus-send  --print-reply --session --dest=org.navit_project.navit /org/navit_project/navit/default_navit org.navit_project.navit.navit.quit
