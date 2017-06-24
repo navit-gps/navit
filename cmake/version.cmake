4d9e16c8b68cbf807c3729140868711132a3f213
jkoan <jkoan@gmx.de>
2017-06-24 14:09:20 +0200

    Dont delete to much? :D

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -4,6 +4,17 @@ get_filename_component(SOURCE_DIR ${SRC} PATH)
 
 string(TIMESTAMP VERSION "%y%m%d%H%M")
 
+string(REGEX MATCH "[0-9]+" VERSION_NUM ${VERSION} ) 
+if(NOT VERSION_NUM) 
+   message(STATUS "I can't find a release tag. This is probably not Navit's official tree") 
+   message(STATUS "It's OK, I will default to 0000") 
+   set(VERSION_NUM "0000") 
+endif() 
+string(REPLACE "R" "" VERSION_NUM ${VERSION_NUM} ) 
+if(NOT VERSION_NUM) 
+   set(VERSION_NUM "0000") 
+endif() 
+
 string(REGEX MATCH "^[a-z0-9]+" VERSION ${VERSION} )
 
 if (STRIP_M)
