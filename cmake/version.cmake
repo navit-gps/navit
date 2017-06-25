1d82081603d4ea967db0bf2e5e982bdb97ba07ec
jkoan <jkoan@users.noreply.github.com>
2017-06-25 03:49:45 +0200

    Use datecode insted of Tags as Android id (#274)
    * Remove all git repo test and use date to get rid of all tags
    
    * Del rests...
    
    * Dont delete to much? :D

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,34 +2,18 @@ FIND_PROGRAM(GIT_EXECUTABLE NAMES git git.exe DOC "git command line client")
 
 get_filename_component(SOURCE_DIR ${SRC} PATH)
 
-if (GIT_EXECUTABLE)
-   EXECUTE_PROCESS(
-      COMMAND ${GIT_EXECUTABLE} log "--format=%h %d"
-      WORKING_DIRECTORY "${SOURCE_DIR}"
-      OUTPUT_VARIABLE VERSION
-      ERROR_VARIABLE GIT_ERROR
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_STRIP_TRAILING_WHITESPACE
-   )
-   if(NOT VERSION)
-      message(STATUS "Cannot determine current git commit - git error: '${GIT_ERROR}'")
-      set(VERSION "0000")
-   endif()
-else()
-   message(STATUS "git not found, cannot record git commit")
-   set(VERSION "0000")
-endif(GIT_EXECUTABLE)
+string(TIMESTAMP VERSION "%y%m%d%H%M")
 
-string(REGEX MATCH "R[0-9]+" VERSION_NUM ${VERSION} )
-if(NOT VERSION_NUM)
-   message(STATUS "I can't find a release tag. This is probably not Navit's official tree")
-   message(STATUS "It's OK, I will default to 0000")
-   set(VERSION_NUM "0000")
-endif()
-string(REPLACE "R" "" VERSION_NUM ${VERSION_NUM} )
-if(NOT VERSION_NUM)
-   set(VERSION_NUM "0000")
-endif()
+string(REGEX MATCH "[0-9]+" VERSION_NUM ${VERSION} ) 
+if(NOT VERSION_NUM) 
+   message(STATUS "I can't find a release tag. This is probably not Navit's official tree") 
+   message(STATUS "It's OK, I will default to 0000") 
+   set(VERSION_NUM "0000") 
+endif() 
+string(REPLACE "R" "" VERSION_NUM ${VERSION_NUM} ) 
+if(NOT VERSION_NUM) 
+   set(VERSION_NUM "0000") 
+endif() 
 
 string(REGEX MATCH "^[a-z0-9]+" VERSION ${VERSION} )
 
