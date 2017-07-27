6129de73e418ab67ef3e5cc034d703ccbaac2723
Johan Fitié <jfitie@gmail.com>
2017-07-27 09:27:13 +0200

    Static value and comment

--- navit/transform.c
+++ navit/transform.c
@@ -91,8 +91,8 @@ transform_setup_matrix(struct transformation *t)
 	navit_float fac;
 	navit_float yawc=navit_cos(-M_PI*t->yaw/180);
 	navit_float yaws=navit_sin(-M_PI*t->yaw/180);
-	navit_float pitchc=navit_cos(-M_PI*t->pitch*sqrt(240*320)/sqrt(240*320)/180);
-	navit_float pitchs=navit_sin(-M_PI*t->pitch*sqrt(240*320)/sqrt(240*320)/180);
+	navit_float pitchc=navit_cos(-M_PI*t->pitch*277/sqrt(240*320)/180); // Pitch corrected for window resolution
+	navit_float pitchs=navit_sin(-M_PI*t->pitch*277/sqrt(240*320)/180); //   sqrt(240*320)=277
 #ifdef ENABLE_ROLL	
 	navit_float rollc=navit_cos(M_PI*t->roll/180);
 	navit_float rolls=navit_sin(M_PI*t->roll/180);
