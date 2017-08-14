7a6ee647751c055ea0741a8740f74e299e0ed43d
jkoan <jkoan@gmx.de>
2017-08-14 23:30:38 +0200

    Added basic logging to circleci to be sure to have the FULL logs

--- ci/build.sh
+++ ci/build.sh
@@ -5,5 +5,9 @@ echo "$# build script (s) to run"
 for i in $(seq 1 $#); do
 	eval s=\$$i
 	echo "Starting build script #$i : $s"
-	bash -e $s
+	if [ ! -z $CIRCLE_ARTIFACTS ]; then
+		bash -e $s 2>&1 | tee $CIRCLE_ARTIFACTS/${s}.log
+	else
+		bash -e $s
+	fi
 done
