28478e7f26c1a0eedc06fb4765e2f736079c6f0c
jkoan <jkoan@gmx.de>
2017-08-15 00:25:00 +0200

    we always want the failed code from bash not from tee

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,7 @@ for i in $(seq 1 $#); do
 	eval s=\$$i
 	echo "Starting build script #$i : $s"
 	if [ ! -z $CIRCLE_ARTIFACTS ]; then
+		set -o pipefail
 		bash -e $s 2>&1 | tee $CIRCLE_ARTIFACTS/${i}.log
 	else
 		bash -e $s
