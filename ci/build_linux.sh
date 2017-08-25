1377bdd53f9e7a5420b2d4182cc727cb52bd2284
Pierre Grandin <grandinp@gmail.com>
2017-08-25 14:32:58 -0700

    Disable QT5 for now

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -1,7 +1,7 @@
 set -e
 
 apt-get update
-apt-get install -y cmake libpng12-dev librsvg2-bin libfreetype6-dev libdbus-glib-1-dev g++ libgtk2.0-dev libqt5svg5-dev qtdeclarative5-qtquick2-plugin qtdeclarative5-window-plugin qtmultimedia5-dev
+apt-get install -y cmake libpng12-dev librsvg2-bin libfreetype6-dev libdbus-glib-1-dev g++ libgtk2.0-dev
 
 cmake_opts="-Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSVG2PNG:BOOL=FALSE -DSAMPLE_MAP=n -Dgraphics/gtk_drawing_area:BOOL=TRUE"
 
