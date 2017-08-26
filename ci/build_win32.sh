81b9243b1f060c4d1fcbdb61f9ce3efc2eed3df8
Pierre Grandin <grandinp@gmail.com>
2017-08-25 17:54:54 -0700

    Install gettext for the win32 build

--- ci/build_win32.sh
+++ ci/build_win32.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y mingw32 mingw32-binutils mingw32-runtime default-jdk nsis cmake
+apt-get update && apt-get install -y mingw32 mingw32-binutils mingw32-runtime default-jdk nsis cmake gettext
 
 mkdir win32
 pushd win32
