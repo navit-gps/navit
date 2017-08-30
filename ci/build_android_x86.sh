bcf965e37f92a179e4584afc19123f88d6e787ab
Pierre Grandin <grandinp@gmail.com>
2017-08-30 16:56:00 -0700

    Updating android x86 scripts for ci v2

--- ci/build_android_x86.sh
+++ ci/build_android_x86.sh
@@ -12,17 +12,22 @@ export ANDROID_NDK_BIN=$ANDROID_NDK"/toolchains/x86-4.9/prebuilt/linux-x86_64/bi
 export ANDROID_SDK="/usr/local/android-sdk-linux/"
 export ANDROID_SDK_PLATFORM_TOOLS=$ANDROID_SDK"/platform-tools"
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
-export BUILD_PATH=$START_PATH"/android-${ARCH}"
+export BUILD_PATH=android-${ARCH}
+
+export ANDROID_SDK_HOME=/opt/android-sdk-linux
+export ANDROID_HOME=/opt/android-sdk-linux
+export PATH=${PATH}:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools:/opt/tools
+
 
 wget -nv -c http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
 [ -d ~/android-ndk-r11c ] || unzip -q -d ~ android-ndk-r11c-linux-x86_64.zip
 
 [ -d $BUILD_PATH ] || mkdir -p $BUILD_PATH
-cd $BUILD_PATH
+pushd $BUILD_PATH
 
 android list targets
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_FILE -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=25 -DANDROID_NDK_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 $SOURCE_PATH
+cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain/arm-eabi.cmake -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=25 -DANDROID_NDK_API_VERSION=19 -DDISABLE_CXX=1 -DDISABLE_QT=1 ..
 make -j $(nproc --all)
 
 if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
