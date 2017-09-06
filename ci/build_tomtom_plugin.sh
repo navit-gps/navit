e591ee38623d0f734f89cd2cdb3c897916b46462
Pierre Grandin <grandinp@gmail.com>
2017-09-05 21:14:16 -0700

    Switching to a pre-built image to build Tomtom builds

--- ci/build_tomtom_plugin.sh
+++ ci/build_tomtom_plugin.sh
@@ -136,4 +136,4 @@ convert -background none ../navit/icons/tomtom_minus.svg -resize 80x80 $OUT_PATH
 
 
 cd $OUT_PATH
-zip -r $CIRCLE_ARTIFACTS/navitom.zip navit SDKRegistry
+zip -r navitom_plugin.zip navit SDKRegistry
