65f27e676225f0da52d02378fece6ad43bcd2592
Pierre Grandin <grandinp@gmail.com>
2017-09-12 11:53:26 -0700

    Fixed coverity path issues for ci v2

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -6,14 +6,15 @@ apt-get install -y libpng12-dev librsvg2-bin libfreetype6-dev libdbus-glib-1-dev
 
 cmake_opts="-Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSVG2PNG:BOOL=FALSE -DSAMPLE_MAP=n -Dgraphics/gtk_drawing_area:BOOL=TRUE"
 
+mkdir ${CIRCLE_WORKING_DIRECTORY}/linux-bin && cd ${CIRCLE_WORKING_DIRECTORY}/linux-bin
+
 if [[ "${CIRCLE_PROJECT_USERNAME}" == "navit-gps" && "${CIRCLE_BRANCH}" == "coverity_upgrade" ]]; then
 	# If we are building the official trunk code, push an update to coverity
 	wget -nv -c -O /tmp/cov-analysis-linux64-${COVERITY_VERSION}.tar.gz http://sd-55475.dedibox.fr/cov-analysis-linux64-${COVERITY_VERSION}.tar.gz
 	tar xfz /tmp/cov-analysis-linux64-${COVERITY_VERSION}.tar.gz -C /usr/local/share/
 	export PATH=/usr/local/share/cov-analysis-linux64-${COVERITY_VERSION}/bin:$PATH
 	
-	mkdir ~/linux-bin && cd ~/linux-bin
-	cov-build --dir cov-int cmake ~/${CIRCLE_PROJECT_REPONAME}/ ${cmake_opts}
+	cov-build --dir cov-int cmake ${CIRCLE_WORKING_DIRECTORY}/ ${cmake_opts}
 	cov-build --dir cov-int make -j $(nproc --all) || exit -1
 	tar czvf navit.tgz cov-int
 	
@@ -30,7 +31,6 @@ if [[ "${CIRCLE_PROJECT_USERNAME}" == "navit-gps" && "${CIRCLE_BRANCH}" == "cove
 	curl "https://translations.launchpad.net/navit/${CIRCLE_BRANCH}/+translations-upload" -H "$lp_cookie" -H "Referer: https://translations.launchpad.net/navit/${CIRCLE_BRANCH}/+translations-upload" -F file=@po/navit.pot | grep title
 
 else
-	mkdir ${CIRCLE_WORKING_DIRECTORY}/linux-bin && cd ${CIRCLE_WORKING_DIRECTORY}/linux-bin
 	cmake ${CIRCLE_WORKING_DIRECTORY}/ ${cmake_opts} || exit -1
 	make -j $(nproc --all) || exit -1
 fi
