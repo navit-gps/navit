1a14d1fad896f5a680892a05543301f0f2e0ca14
Pierre Grandin <grandinp@gmail.com>
2017-10-01 23:37:36 -0700

    First rftd.io import

---
+++ docs/source/conf.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+# General information about the project.
+project = u'navit'
+year = datetime.now().year
+copyright = u'%d, the Navit Team' % year
+author = u'The Navit Team'
