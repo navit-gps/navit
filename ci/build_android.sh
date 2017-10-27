31c9535d5066f9d611f34fc09acd804c48650062
Pierre Grandin <grandinp@gmail.com>
2017-10-27 10:45:15 -0700

    Always build 'release' package for android

--- ci/build_android.sh
+++ ci/build_android.sh
@@ -30,11 +30,8 @@ cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain/arm-eabi.cmake -DCACHE_SIZE='(20*1024*
 
 make -j $(nproc --all)
 
-if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
-  make -j $(nproc --all) apkg-release && mv navit/android/bin/Navit-release-unsigned.apk navit/android/bin/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk || exit 1
-else
-  make -j $(nproc --all) apkg && mv navit/android/bin/Navit-debug.apk navit/android/bin/navit-$CIRCLE_SHA1-${ARCH}-debug.apk || exit 1
-fi
+make -j $(nproc --all) apkg && mv navit/android/bin/Navit-debug.apk navit/android/bin/navit-$CIRCLE_SHA1-${ARCH}-debug.apk
+make -j $(nproc --all) apkg-release && mv navit/android/bin/Navit-release-unsigned.apk navit/android/bin/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk
 
 echo
 echo "Build leftovers :"
