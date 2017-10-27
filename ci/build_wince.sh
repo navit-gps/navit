d08536c7de24d9e49c1fa8b9ea7e1a8bdda9e15e
Nick Geoghegan <nick@nickgeoghegan.net>
2017-10-27 20:27:42 +0100

    Update build_wince.sh

--- ci/build_wince.sh
+++ ci/build_wince.sh
@@ -2,6 +2,8 @@
 #!/bin/bash
 set -x
 
+find .
+
 export MING_PATH=$(pwd)/opt/mingw32ce
 export SOURCE_PATH=$START_PATH"/"${CIRCLE_PROJECT_REPONAME}"/"
 export CMAKE_FILE=$SOURCE_PATH"/Toolchain/arm-mingw32ce.cmake"
@@ -13,6 +15,8 @@ wget -c https://sourceforge.net/projects/cegcc/files/cegcc/0.59.1/mingw32ce-0.59
 echo "##########PWD IS:" pwd
 mv download ming232ce.tar.bz
 tar xjf ming232ce.tar.bz
+
+find .
 
 cmake -DCMAKE_TOOLCHAIN_FILE=Toolchain/arm-mingw32ce.cmake -DXSLTS=windows -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32 -Dsvg2png_scaling_nav:STRING=32 -Dsvg2png_scaling_flag=16 -DSAMPLE_MAP=n .
 make
