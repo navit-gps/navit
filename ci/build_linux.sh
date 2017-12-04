f4c9ba44605eeca7833b063cb04005185d68eb9c
Pierre Grandin <grandinp@gmail.com>
2017-12-03 19:25:10 -0800

    Moved linux deps install to another step

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -4,8 +4,6 @@ set -e
 COVERITY_VERSION="2017.07"
 BUILD_PATH="linux"
 
-apt-get install -y libpng12-dev librsvg2-bin libfreetype6-dev libdbus-glib-1-dev libgtk2.0-dev curl
-
 cmake_opts="-Dgraphics/qt_qpainter:BOOL=FALSE -Dgui/qml:BOOL=FALSE -DSVG2PNG:BOOL=FALSE -DSAMPLE_MAP=n -Dgraphics/gtk_drawing_area:BOOL=TRUE"
 
 [ -d $BUILD_PATH ] || mkdir -p $BUILD_PATH
