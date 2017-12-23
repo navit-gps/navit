d9cbfd2e4f3b17e45ef6bd8a9186e3745123e685
Pierre Grandin <grandinp@gmail.com>
2017-12-22 22:40:12 -0800

    Store build artifacts

--- ci/build_wince.sh
+++ ci/build_wince.sh
@@ -1,3 +1,12 @@
 mkdir wince && cd wince
 cmake ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/arm-mingw32ce.cmake -DXSLTS=windows -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32 -Dsvg2png_scaling_nav:STRING=32 -Dsvg2png_scaling_flag=16 -DSAMPLE_MAP=n 
 make
+
+mkdir output
+cp navit/navit.exe output/
+cp navit/navit.xml output/
+cp -r locale/ output/
+cp -r navit/icons/ output
+mkdir output/maps
+cp navit/maps/*.bin output/maps
+cp navit/maps/*.xml output/maps
