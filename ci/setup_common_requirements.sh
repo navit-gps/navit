b212b8d27f9ddeeb5c6786551939da4e0f3b2efc
jkoan <jkoan@users.noreply.github.com>
2018-03-06 13:02:23 +0100

    fix:ci:Fix linux build

--- ci/setup_common_requirements.sh
+++ ci/setup_common_requirements.sh
@@ -1,1 +1,1 @@
-apt-get update && apt-get install -y wget unzip cmake build-essential gettext imagemagick util-linux git ssh
+apt-get update && apt-get install -y wget unzip cmake build-essential gettext imagemagick util-linux git ssh rpmbuild 
