f71dc2b8c89c7562fcbfedd69d7f32d6c9fd6693
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2018-03-28 10:29:36 -0700

    Fix:wince:Remove useless files from the output (#412)

--- ci/build_wince.sh
+++ ci/build_wince.sh
@@ -12,6 +12,7 @@ cp -r navit/icons/ output
 mkdir output/maps
 cp navit/maps/*.bin output/maps
 cp navit/maps/*.xml output/maps
+rm -rf output/icons/CMakeFiles/ icons/cmake_install.cmake
 
 cd output/
 zip -r navit.zip  .
