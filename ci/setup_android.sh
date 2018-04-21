7843ea768ce69eefc47398f206886b54bf978d50
Joseph Herlant <herlantj@gmail.com>
2018-04-21 06:10:01 -0700

    Update:CI:Make sure that all the scripts in the ci folder have a shebang and use set -e

--- ci/setup_android.sh
+++ ci/setup_android.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 apt-get update && apt-get install -y software-properties-common
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update && apt-get install -y openjdk-8-jdk wget expect git curl libsaxonb-java ant
