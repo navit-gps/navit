011bb15468b4cb626e9facecba924b04bd494d7f
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2018-04-25 09:09:43 -0700

    Fix:CI:Automated upload to playstore (#540)
    * Working on automated playstore updates
    
    * Working on automated playstore updates
    
    * Install certs before pulling CI cache
    
    * Testing playstore update from android build job
    
    * Updated publish.sh
    
    * Setup git to pull from blackbox
    
    * Setup git to pull from blackbox in the correct job
    
    * Install pip, needed for playstore updates
    
    * Push to playstore from the android build, not a seperate step
    
    * upload to playstore from the official trunk only
    
    * Sign apks before updating download center
    
    * Cache isn't needed anymore for android arm builds
    
    * Save signed apks as part of the artifacts
    
    * Moved package installation to the Dockerfile
    
    * Install ca-certs and pip when preparing android build

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
+
+if [ -z $CI ];then
+    echo "This Script needs to be run by CI"
+fi
+if [ -z $CIRCLECI ];then
+    echo "This Script needs to be run on CircleCI"
+fi
+if [[ "${CIRCLE_PROJECT_USERNAME}" != "navit-gps" || "${CIRCLE_BRANCH}" != "trunk" ]]; then
+    echo "Only trunk on navit-gps may upload to the Play Store"
+    exit 0
+fi
+
 set -e
 
 ARCH="arm"
+BINPATH="android-arm/navit/android/bin"
+
+export PATH="/opt/android-sdk-linux/build-tools/25.0.1/":$PATH
 
 cd ~/
 git clone git@github.com:navit-gps/infrastructure-blackbox.git
 cd infrastructure-blackbox/keyrings/
 openssl aes-256-cbc -d -in keystore.gpg -k $KEY > ~/.keystore
-openssl aes-256-cbc -d -in client_secrets.gpg -k $KEY > ~/navit/ci/client_secrets.json
-openssl aes-256-cbc -d -in androidpublisher.gpg -k $KEY > androidpublisher.dat
+openssl aes-256-cbc -d -in client_secrets.gpg -k $KEY > /root/project/ci/client_secrets.json
+openssl aes-256-cbc -d -in androidpublisher.gpg -k $KEY > /root/project/androidpublisher.dat
+cd ~/project
 
 pip install google-api-python-client
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass $SP $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $key_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass $SP $BINPATH/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $key_name
 
-/usr/local/android-sdk-linux/build-tools/25.0.1/zipalign 4 $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
-# /usr/local/android-sdk-linux/build-tools/25.0.1/apksigner sign -v --ks-pass pass:$SP --key $key_name $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk 
-/usr/local/android-sdk-linux/build-tools/25.0.1/apksigner verify -v $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
-python ~/navit/ci/basic_upload_apks.py org.navitproject.navit $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
+zipalign 4 $BINPATH/navit-$CIRCLE_SHA1-${ARCH}-release-unsigned.apk $BINPATH/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
+apksigner verify -v $BINPATH/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk
+python ~/project/ci/basic_upload_apks.py org.navitproject.navit $BINPATH/navit-$CIRCLE_SHA1-${ARCH}-release-signed.apk --noauth_local_webserver
