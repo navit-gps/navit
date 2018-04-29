660ec1cfba39c3ac20acdd9421e3f4b934e46879
Charles Curley <charlescurley@charlescurley.com>
2018-04-29 15:48:03 -0600

    1) Changed the translation string for "Distance(m)" from "" to "Distance". Rational: the "m" is ambiguous, it could be meters or miles. However, in the POI context, feet are more likely than miles. So take it out entirely. I did it this way rather than run sed on the entire collection of po input files.
    2) The display code sorts the entries by distance. It has no way of knowing if "5" is five miles or five feet. So we leave the distances in feet or meters (depending on the setting for imperial). Allowing kilometers or miles might be a future enhancement.
    
    3) See bug 556 (https://github.com/navit-gps/navit/issues/556). Rather than remove the spurious one as part of this fix, I will leave that to later. But both now have the same English "translation", "Select a search radius from screen center". This hides the problem, at least from English users. This affects all four en_*.po.in files.
    
    4) Someone who knows more than I do about the translation strings might have a better idea. Particularly, is there a graceful way to indicate the input units, either miles or kilometers?
    
    	modified:   navit/gui/gtk/gui_gtk_poi.c
    	modified:   po/en.po.in
    	modified:   po/en_AU.po.in
    	modified:   po/en_CA.po.in
    	modified:   po/en_GB.po.in

--- navit/gui/gtk/gui_gtk_poi.c
+++ navit/gui/gtk/gui_gtk_poi.c
@@ -35,6 +35,8 @@
 #include "attr.h"
 #include "util.h"
 
+#include "navigation.h"         /* for KILOMETERS_TO_MILES */
+
 static struct gtk_poi_search{
 	GtkWidget *entry_distance;
 	GtkWidget *label_distance;
@@ -108,14 +110,27 @@ model_poi (struct gtk_poi_search *search)
 	struct coord coord_item,center;
 	struct pcoord pc;
 	struct mapset_handle *h;
-	int search_distance_meters,idist;
+	int search_distance_meters; /* distance to search the POI database, in meters, from the center of the screen. */
+        int idist; /* idist appears to be the distance in meters from the center of the screen to a POI. */
 	struct map *m;
 	struct map_rect *mr;
 	struct item *item;
 	struct point cursor_position;
 	enum item_type selected;
 
-	search_distance_meters=1000*atoi((char *) gtk_entry_get_text(GTK_ENTRY(search->entry_distance)));
+        /* Respect the Imperial attribute as we enlighten the user. */
+	struct attr attr;
+        int imperial = FALSE;  /* default to using metric measures. */
+        if (navit_get_attr(gtk_poi_search.nav, attr_imperial, &attr, NULL))
+            imperial=attr.u.num;
+
+        if (imperial == FALSE) {
+                /* Input is in kilometers */
+                search_distance_meters=1000*atoi((char *) gtk_entry_get_text(GTK_ENTRY(search->entry_distance)));
+        } else {
+                /* Input is in miles. */
+                search_distance_meters=atoi((char *) gtk_entry_get_text(GTK_ENTRY(search->entry_distance)))/METERS_TO_MILES;
+        }
 
 	cursor_position.x=navit_get_width(search->nav)/2;
 	cursor_position.y=navit_get_height(search->nav)/2;
@@ -146,6 +161,21 @@ model_poi (struct gtk_poi_search *search)
 					char direction[5];
 					gtk_list_store_append(search->store_poi, &iter);
 					get_compass_direction(direction,transform_get_angle_delta(&center,&coord_item,0),1);
+
+                                        /* If the user has selected
+                                         * imperial, translate idist
+                                         * from meters to feet. We
+                                         * convert to feet only
+                                         * because the code sorts on
+                                         * the numeric value of the
+                                         * distance, so it doesn't
+                                         * like two different
+                                         * units. Possible future
+                                         * enhancement? */
+                                        if (imperial != FALSE) {
+                                                idist = idist * (FEET_PER_METER); /* convert meters to feet. */
+                                        }
+
 					gtk_list_store_set(search->store_poi, &iter, 0,direction, 1,idist,
 						2,g_strdup(label_attr.u.str), 3,coord_item.x, 4,coord_item.y ,-1);
 				}
