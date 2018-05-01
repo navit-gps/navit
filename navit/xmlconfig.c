98b06544033be1fb83c8771e9a355639b8e8de7e
mvglasow <michael -at- vonglasow.com>
2018-05-01 23:34:22 +0200

    Fix:core:Correctly initialize XML elements array
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- navit/xmlconfig.c
+++ navit/xmlconfig.c
@@ -342,7 +342,7 @@ static char *element_fixmes[]={
 };
 
 static void initStatic(void) {
-	elements=g_new0(struct element_func,44); //43 is a number of elements + ending NULL element
+	elements=g_new0(struct element_func, 45); //44 is a number of elements + ending NULL element
 
 	elements[0].name="config";
 	elements[0].parent=NULL;
