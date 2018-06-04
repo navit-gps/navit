d68ab5f662969376a889629c72a0cb5ab120cfda
Joseph Herlant <aerostitch@users.noreply.github.com>
2018-06-04 10:30:23 -0700

    update:style:optimize the usage of cat in shell scripts (#612)

--- navit/android/change_xslt.sh
+++ navit/android/change_xslt.sh
@@ -24,35 +24,30 @@ REG4='{round(@1@-number($LAYOUT_001_ORDER_DELTA_1))}'
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 	a=`echo "$REG0"|sed -e "s#@1@#$i#"`
-	cat "$temp"|sed -e "s#order=\"0-${i}\"#order=\"${a}\"#g" > "$out"
-	cp -p "$out" "$temp"
+	sed -e "s#order=\"0-${i}\"#order=\"${a}\"#g" -i "$temp"
 done
 
 # dont change order="0-" values !!
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 	a=`echo "$REG1"|sed -e "s#@1@#$i#"`
-	cat "$temp"|sed -e "s#order=\"${i}-\"#order=\"${a}\"#g" > "$out"
-	cp -p "$out" "$temp"
+	sed -e "s#order=\"${i}-\"#order=\"${a}\"#g" -i "$temp"
 done
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 	a=`echo "$REG2"|sed -e "s#@1@#$i#"`
-	cat "$temp"|sed -e "s#order=\"-${i}\"#order=\"${a}\"#g" > "$out"
-	cp -p "$out" "$temp"
+	sed -e "s#order=\"-${i}\"#order=\"${a}\"#g" -i "$temp"
 done
 
 
 for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 	for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 		a=`echo "$REG3"|sed -e "s#@1@#$i#"|sed -e "s#@2@#$j#"`
-		cat "$temp"|sed -e "s#order=\"${i}-${j}\"#order=\"${a}\"#g" > "$out"
-		cp -p "$out" "$temp"
+		sed -e "s#order=\"${i}-${j}\"#order=\"${a}\"#g" -i "$temp"
 	done
 done
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18;do
 	a=`echo "$REG4"|sed -e "s#@1@#$i#"`
-	cat "$temp"|sed -e "s#order=\"${i}\"#order=\"${a}\"#g" > "$out"
-	cp -p "$out" "$temp"
+	sed -e "s#order=\"${i}\"#order=\"${a}\"#g" -i "$temp"
 done
-
+cp -p "$temp" "$out"
