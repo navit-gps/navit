769896d6870449b73d4a3da80a5468ced2f67bf4
Stefan Wildemann <gta04@metalstrolche.de>
2018-06-05 23:55:24 +0200

    redo. removing the files from build was the false way

--- navit/speech/espeak/speak.c
+++ navit/speech/espeak/speak.c
@@ -68,8 +68,8 @@ int GetFileLength(const char *filename) {
     return(statbuf.st_size);
 }
 
-//void MarkerEvent(int type, unsigned int char_position, int value, unsigned char *out_ptr) {
-//}
+void MarkerEvent(int type, unsigned int char_position, int value, int value2, unsigned char *out_ptr){
+}
 
 char *Alloc(int size) {
     return g_malloc(size);
