6381e2cd7a1a22aace8da1e67c1ecb99c1879ec9
Bignaux Ronan <ronan@aimao.org>
2018-09-06 23:21:48 +0200

    Refactoring:mingw:simplify toolchain

--- scripts/build_win32.sh
+++ scripts/build_win32.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 set -e
+<<<<<<< HEAD
 
 apt-get update && apt-get install -y mingw-w64 mingw-w64-tools  \
   default-jdk nsis libsaxonb-java curl
+=======
+>>>>>>> Refactoring:mingw:simplify toolchain
 
 mkdir win32
 pushd win32
 
+<<<<<<< HEAD
 cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n \
   -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw32.cmake ..
+=======
+cmake -DTARGET_ARCH=i686-w64-mingw32 -DCMAKE_SYSTEM_NAME=Windows \
+  -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n \
+  -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw.cmake ..
+
+>>>>>>> Refactoring:mingw:simplify toolchain
 make -j $(nproc --all)
 make -j $(nproc --all) package
 
