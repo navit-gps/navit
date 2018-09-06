4a66b802b528a20fcc937750fb5e69399a3b5a89
Bignaux Ronan <ronan@aimao.org>
2018-09-06 23:21:48 +0200

    Refactoring:mingw:simplify toolchain

--- scripts/build_wince.sh
+++ scripts/build_wince.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
-mkdir -p wince && cd wince
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/arm-mingw32ce.cmake -DXSLTS=windows,wince -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32 -Dsvg2png_scaling_nav:STRING=32 -Dsvg2png_scaling_flag=16 -DSAMPLE_MAP=n
+mkdir -p wince
+pushd wince
+#
+cmake \
+  -DTARGET_ARCH=arm-mingw32ce -DCMAKE_SYSTEM_NAME=WindowsCEtest \
+  -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw.cmake \
+  -DXSLTS=windows,wince -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32 \
+  -Dsvg2png_scaling_nav:STRING=32 -Dsvg2png_scaling_flag=16 -DSAMPLE_MAP=n ..
 make
 
 test -d output && rm -rf output
