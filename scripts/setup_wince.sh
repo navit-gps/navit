35a3b8fa2c0c7ae2c4a607144fb8f45360f5c74e
Joseph Herlant <herlantj@gmail.com>
2018-09-17 09:39:12 -0700

    Print the deprecation warning for setup_wince.sh

--- scripts/setup_wince.sh
+++ scripts/setup_wince.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# #############################################################
-# This script has been deprecated. If you need to add packages,
-# add it to the wince/Dockerfile on the navit/Dockerfiles repo.
-# #############################################################
+echo "#############################################################"
+echo "This script has been deprecated. If you need to add packages,"
+echo "add it to the wince/Dockerfile on the navit/Dockerfiles repo."
+echo "#############################################################"
 
 set -e
 
