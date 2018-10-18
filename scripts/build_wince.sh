4425893943f79f34eebdcd51d0bc782bc064d883
Pierre Grandin <grandinp@gmail.com>
2018-10-17 23:58:29 -0700

    working on the wince build

--- scripts/build_wince.sh
+++ scripts/build_wince.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p wince && cd wince
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/arm-mingw32ce.cmake -DXSLTS=windows,wince -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32 -Dsvg2png_scaling_nav:STRING=32 -Dsvg2png_scaling_flag=16 -DSAMPLE_MAP=n
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/arm-mingw32ce.cmake -DXSLTS=windows,wince -DCACHE_SIZE=10485760 -Dsvg2png_scaling:STRING=16,32,64,96 -Dsvg2png_scaling_nav:STRING=32,64 -Dsvg2png_scaling_flag=16,32 -DSAMPLE_MAP=n
 make
 
 test -d output && rm -rf output
