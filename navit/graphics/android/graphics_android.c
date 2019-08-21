7a2b43a5b332357fa3ec888c73b613da45d72641
mvglasow <michael -at- vonglasow.com>
2019-08-21 23:25:22 +0300

    Fix:graphics/android:Remove faulty debug statement causing crashes
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- navit/graphics/android/graphics_android.c
+++ navit/graphics/android/graphics_android.c
@@ -479,7 +479,7 @@ static struct graphics_methods graphics_methods = {
 
 static void resize_callback(struct graphics_priv *gra, int w, int h) {
     dbg(lvl_debug,"w=%d h=%d ok",w,h);
-    dbg(lvl_debug,"gra=%p, %d callbacks in list", gra, g_list_length(gra->cbl));
+    dbg(lvl_debug,"gra=%p, gra->cbl=%p", gra, gra->cbl);
     callback_list_call_attr_2(gra->cbl, attr_resize, (void *)w, (void *)h);
 }
 
