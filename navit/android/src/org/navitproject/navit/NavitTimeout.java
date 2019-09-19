37a8ca299c9cd6d94cc4a867ca5c3189959b6bf3
jandegr <jandegr@users.noreply.github.com>
2019-09-19 14:59:05 +0200

    64 2 issues (#63)
    * fix android 64
    
    * Update gslice.c
    
    * cosmetic
    
    * some changes ported from iOS 32
    
    * Update fake.h
    
    * Update checkstyle.xml
    
    * Update checkstyle.xml
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update checkstyle.xml
    
    * Update checkstyle.xml
    
    * Update checkstyle.xml
    
    * style fixes
    
    * fix access
    
    * Add files via upload
    
    * don't litter recent devices
    
    * one more attempt to fix JLS
    
    * Add files via upload
    
    * Add files via upload
    
    * JLS
    
    * style issues
    
    * Update checkstyle.xml
    
    * style issues
    
    * style issue
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update checkstyle.xml
    
    * style issues
    
    * Add files via upload
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * Update checkstyle.xml
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * Add files via upload
    
    * Update checkstyle.xml
    
    * style issues
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update NavitVehicle.java
    
    * Update build.gradle
    
    * Update config.yml
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * doc style test
    
    * another doc style test
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issues
    
    * style issue
    
    * Add files via upload
    
    * Update build.gradle
    
    * Update build.gradle
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * cleanup
    
    * fixes
    
    * cleanup
    
    * cleanup
    
    * navitUtils
    
    * cleanup
    
    * fixes
    
    * fixes
    
    * test for cyclomatic complexity
    
    * test
    
    * cyclomatic complexity
    
    * test ontouch
    
    * further test onTouch
    
    * test onTouch
    
    * test onTouch
    
    * test onKeyDown
    
    * onKeyUp
    
    * handlerissue
    
    * cleanup
    
    * indent
    
    * make error default
    
    * indent
    
    * test onKeyDown
    
    * test onKeyDown
    
    * test callbackMessageChannel
    
    * test callBackMessageChannel
    
    * Update AndroidManifest.xml
    
    * test
    
    * test
    
    * more test
    
    * Update android.c
    
    * more test
    
    * more test
    
    * Add files via upload
    
    * fixes
    
    * Create test_junk.c
    
    * Create test_junk2.c
    
    * Update test_junk2.c
    
    * Update test_junk2.c
    
    * Update test_junk.c
    
    * Update test_junk2.c
    
    * Update test_junk.c
    
    * Update test_junk.c
    
    * Update test_junk.c
    
    * Update test_junk.c
    
    * Delete test_junk2.c
    
    * Delete test_junk.c
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * AOSPfixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * Add files via upload
    
    * fixes
    
    * switch break style
    
    * fixes
    
    * fixes
    
    * fixes
    
    * Update checkstyle.xml
    
    * fixes
    
    * style fixes
    
    * style fixes
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * fix & pedestrian preliminary
    
    * whitespace
    
    * revert
    
    * lineendings
    
    * fixes and keyboard
    
    * fixes and keyboard
    
    * fixes and keyboard
    
    * pedestrian plugin
    
    * ROLL for pedestrian plugin
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * fix mistake in commit for pedestrian plugin
    
    * keyboard
    
    * fixes
    
    * remove sdk version
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * whitespace
    
    * fixes
    
    * fixes
    
    * whitsepace

--- navit/android/src/org/navitproject/navit/NavitTimeout.java
+++ navit/android/src/org/navitproject/navit/NavitTimeout.java
@@ -1,20 +1,20 @@
-/**
- * Navit, a modular navigation system.
- * Copyright (C) 2005-2008 Navit Team
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
+/*
+  Navit, a modular navigation system.
+  Copyright (C) 2005-2008 Navit Team
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA  02110-1301, USA.
  */
 
 package org.navitproject.navit;
@@ -24,36 +24,37 @@ import android.os.Message;
 import android.util.Log;
 
 
+class NavitTimeout implements Runnable {
 
-public class NavitTimeout implements Runnable {
-    private static Handler handler = new Handler() {
-        public void handleMessage(Message m) {
-            Log.e("Navit","Handler received message");
-        }
-    };
-    private boolean event_multi;
-    private int event_callbackid;
-    private int event_timeout;
+    private static final TimeoutHandler handler = new TimeoutHandler();
+    private final long mEventCallbackid;
+    private final int mEventTimeout;
+    private final boolean mEventMulti;
 
-    public native void TimeoutCallback(int id);
-
-    NavitTimeout(int timeout, boolean multi, int callbackid) {
-        event_timeout = timeout;
-        event_multi = multi;
-        event_callbackid = callbackid;
-        handler.postDelayed(this, event_timeout);
+    NavitTimeout(int timeout, boolean multi, long callbackid) {
+        mEventTimeout = timeout;
+        mEventMulti = multi;
+        mEventCallbackid = callbackid;
+        handler.postDelayed(this, mEventTimeout);
     }
 
+    public native void timeoutCallback(long id);
+
     public void run() {
-        // Log.e("Navit","Handle Event");
-        if (event_multi) {
-            handler.postDelayed(this, event_timeout);
+        Log.v("Navit","Handle Event");
+        if (mEventMulti) {
+            handler.postDelayed(this, mEventTimeout);
         }
-        TimeoutCallback(event_callbackid);
+        timeoutCallback(mEventCallbackid);
     }
 
     public void remove() {
         handler.removeCallbacks(this);
     }
-}
 
+    static class TimeoutHandler extends Handler {
+        public void handleMessage(Message m) {
+            Log.d("NavitTimeout", "Handler received message");
+        }
+    }
+}
