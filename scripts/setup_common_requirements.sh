3d66aeceafaacb605ba8e35440999fa2f32150c8
gefin <g.fink@gmx.net>
2019-09-22 17:53:45 +0200

    Update setup_common_requirements.sh

--- scripts/setup_common_requirements.sh
+++ scripts/setup_common_requirements.sh
@@ -3,4 +3,4 @@ set -e
 
 apt-get update && apt-get install -y wget unzip cmake build-essential \
   gettext librsvg2-bin util-linux git ssh sed astyle \
-  libprotobuf-c-dev protobuf-c-compiler xsltproc
+  libprotobuf-c-dev protobuf-c-compiler
