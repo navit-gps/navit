1e63497049e61c2be670ef8928a23558ae154fda
Wildemann Stefan <stefan.wildemann@corpuls.com>
2019-10-17 08:43:17 +0200

    fix:maptool:fix doxygen documentation
    Remove some stars to fix doxygen doc.

--- navit/maptool/itembin.c
+++ navit/maptool/itembin.c
@@ -1,4 +1,4 @@
-/**
+/*
  * Navit, a modular navigation system.
  * Copyright (C) 2005-2011 Navit Team
  *
--- navit/maptool/zip.c
+++ navit/maptool/zip.c
@@ -1,4 +1,4 @@
-/**
+/*
  * Navit, a modular navigation system.
  * Copyright (C) 2005-2008 Navit Team
  *
