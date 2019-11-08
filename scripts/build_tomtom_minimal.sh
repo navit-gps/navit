06f12838858e851ea708a4ba8bedca35aa7dbe49
gefin <g.fink@gmx.net>
2019-11-08 08:47:17 +0100

    fix:tomtom:Make sure the tomtom_plugin and tomtom_minimal contain navit.xml (#875)
    * Issue/823 
    
    Fix navit.xml (tomtom480.xml) and locales path
    
    * Issue/823
    
    Fix missing navit.xml (tomtom480.xml) and locale path
    
    * add xslt support
    
    * Update setup_common_requirements.sh
    
    * Moving change performed in aa10e05a89f9bd26a4c5cf46a264b41fd9ec93b9 inside setup_tomtom_requirements.sh (and adding it to circleci process)
    
    * Only running installation for xsltproc in circleci
    
    * Update build_tomtom_minimal.sh
    
    copy also the layout xml files to target
    
    * copy also the layout xml to target
    
    * Selecting specific xml prefixes during copy
    
    * Fixing tomtom's xslt following split of xml config files at e70a28963ff9754d3257361fad93aebf59edee08

--- scripts/build_tomtom_minimal.sh
+++ scripts/build_tomtom_minimal.sh
@@ -57,11 +57,11 @@ cp $PREFIX/share/navit/icons/*48.png $OUT_PATH/navit/share/icons
 cp $PREFIX/share/navit/icons/*64.png $OUT_PATH/navit/share/icons
 cp $PREFIX/share/navit/icons/nav*.* $OUT_PATH/navit/share/icons
 cp $PREFIX/share/navit/icons/country*.png $OUT_PATH/navit/share/icons
-cd ..
-cp $PREFIX/share/navit/navit.xml ./tomtom480.xml
+cp $PREFIX/share/navit/navit.xml $OUT_PATH/navit/share
+cp $PREFIX/share/navit/navit_layout_*.xml $OUT_PATH/navit/share
 
 # locale
-cp -r $PREFIX/share/locale $OUT_PATH/navit/share/locale
+cp -r $PREFIX/share/locale $OUT_PATH/navit/share
 
 
 cd $OUT_PATH
