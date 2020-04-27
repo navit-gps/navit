25cac92d72d46ca4cd6bbf24734049016c08167e
Lionel AINS <lains@users.noreply.github.com>
2020-04-27 07:14:48 +0200

    Refactoring gui_internal_show_coord_actions()

--- navit/gui/internal/gui_internal_command.h
+++ navit/gui/internal/gui_internal_command.h
@@ -4,6 +4,4 @@ struct gui_priv;
 struct pcoord;
 int gui_internal_cmd2_quit(struct gui_priv *this, char *function, struct attr **in, struct attr ***out);
 void gui_internal_command_init(struct gui_priv *this, struct attr **attrs);
-//void gui_internal_cmd2_show_geopos_actions(struct gui_priv *this, char *function, struct attr **in, struct attr ***out);
-void gui_internal_cmd2_show_geopos_actions(struct gui_priv *this, struct pcoord *c, char *description);
 /* end of prototypes */
