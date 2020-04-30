5db4bc092ab6663ddba1ea73434631114f4b381e
Stefan Wildemann <gta04@metalstrolche.de>
2020-04-30 17:58:16 +0200

    trigger (in)sanity check

--- navit/maptool/osm.c
+++ navit/maptool/osm.c
@@ -1,6 +1,6 @@
 /*
  * Navit, a modular navigation system.
- * Copyright (C) 2005-2011 Navit Team
+ * Copyright (C) 2005-2020 Navit Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
