b04bee64ebb22235a086c4d745a1746117495b5d
Stefan Wildemann <gta04@metalstrolche.de>
2020-05-01 19:53:32 +0200

    fix:build:android: Don't sign apk if no valid keyring is available
    This happens usually if building with CI on fork repository

--- scripts/setup_publish_keys.sh
+++ scripts/setup_publish_keys.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ -n $GOOGLE_KEY ]; then
+if [[ -n $GOOGLE_KEY ]]; then
   echo $GOOGLE_KEY | base64 -d > key.json
 fi
-if [ -n $KEY ]; then
+if [[ -n $KEY ]]; then
   wget "https://github.com/navit-gps/infrastructure-blackbox/raw/master/keyrings/keystore.gpg"
   openssl aes-256-cbc -d -in keystore.gpg -md md5 -k $KEY > ~/.keystore
   keytool -importkeystore -srcstorepass "$STORE_PASS" -deststorepass "$STORE_PASS" -srckeystore ~/.keystore -destkeystore ~/.keystore -deststoretype pkcs12
