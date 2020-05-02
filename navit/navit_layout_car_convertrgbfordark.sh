78b3a1504053fbec02ec6cbdaf81814b4d079957
Johan Fitié <jfitie@gmail.com>
2020-05-02 20:46:41 +0200

    Simplify script

--- navit/navit_layout_car_convertrgbfordark.sh
+++ navit/navit_layout_car_convertrgbfordark.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo Light: $1
-r=$(printf '%02x' $(echo $(printf "%d" 0x${1:0:2})/34 | bc))
-g=$(printf '%02x' $(echo $(printf "%d" 0x${1:2:2})/8 | bc))
-b=$(printf '%02x' $(echo $(printf "%d" 0x${1:4:2})/9 | bc))
-echo Dark:\  $r$g$b
+echo Dark:\  $(printf '%02x' $(echo $(printf "%d" 0x${1:0:2})/34 | bc))$(printf '%02x' $(echo $(printf "%d" 0x${1:2:2})/8 | bc))$(printf '%02x' $(echo $(printf "%d" 0x${1:4:2})/9 | bc))
