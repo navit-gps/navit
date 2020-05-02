58196ffdf7e8b4deeac2ade181a77d24f632e2a2
Johan Fitié <jfitie@gmail.com>
2020-05-02 21:02:00 +0200

    Remove script

--- navit/navit_layout_car_convertrgbfordark.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo Light: $1
-echo Dark:\  $(printf '%02x' $(echo $(printf "%d" 0x${1:0:2})/34 | bc))$(printf '%02x' $(echo $(printf "%d" 0x${1:2:2})/8 | bc))$(printf '%02x' $(echo $(printf "%d" 0x${1:4:2})/9 | bc))
