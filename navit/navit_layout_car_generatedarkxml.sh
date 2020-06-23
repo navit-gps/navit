d1bc82efc2820a4364fa92a98aba930ddd02133e
Johan Fitié <jfitie@gmail.com>
2020-06-23 19:25:37 +0200

    Color tweak

--- navit/navit_layout_car_generatedarkxml.sh
+++ navit/navit_layout_car_generatedarkxml.sh
@@ -70,13 +70,13 @@ do
      if [[ $l =~ .*color=\"#[0-9a-fA-F]{6}.* ]]; then # Contains rgb(a) color
       coll=$(echo $l | cut -d# -f2 | cut -c-6)        # Get rgb color and convert
       # Get color values in decimal
-      cr=$(echo $(printf "%d" 0x${coll:0:2}))
-      cg=$(echo $(printf "%d" 0x${coll:2:2}))
-      cb=$(echo $(printf "%d" 0x${coll:4:2}))
+      cr=$(printf "%d" 0x${coll:0:2})
+      cg=$(printf "%d" 0x${coll:2:2})
+      cb=$(printf "%d" 0x${coll:4:2})
       # Modify color values
       crn=$(echo $cr/16+$cg/32+$cb/32+16 | bc)
-      cgn=$(echo $cr/24+$cg/12+$cb/24+14 | bc)
-      cbn=$(echo $cr/16+$cg/16+$cb/ 8+12 | bc)
+      cgn=$(echo $cr/24+$cg/12+$cb/24+12 | bc)
+      cbn=$(echo $cr/16+$cg/16+$cb/ 8+ 8 | bc)
       # Convert new color values to hex
       cold=$(     printf '%02x' $crn)
       cold=$cold$(printf '%02x' $cgn)
