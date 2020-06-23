69704b63a2059a6b56072026fe2282268545691f
Johan Fitié <jfitie@gmail.com>
2020-06-23 19:39:56 +0200

    Variable

--- navit/navit_layout_car_generatedarkxml.sh
+++ navit/navit_layout_car_generatedarkxml.sh
@@ -5,6 +5,10 @@
 # navit_layout_car_dark_shipped.xml.
 # It's a bit of a mess, but gets the job done.
 
+# Variables
+defcol='55c4bd'
+
+# Settings
 IFS='' # Keep whitespace
 
 # Light and dark input files
@@ -86,8 +90,8 @@ do
 
      # Misc. modifications
      l=$(echo $l | sed -r "s/_bk\./_wh\./") # Black to white icons
-     l=$(echo $l | sed -r "s/(<text )/\1color=\"#55c4bd\" background_color=\"#000000\" /") # Add text color
-     l=$(echo $l | sed -r "s/(<circle color=\"#[0-9a-fA-F]{6,8}\" )/\1background_color=\"#55c4bd\" /") # Add circle bg color
+     l=$(echo $l | sed -r "s/(<text )/\1color=\"#$defcol\" background_color=\"#000000\" /") # Add text color
+     l=$(echo $l | sed -r "s/(<circle color=\"#[0-9a-fA-F]{6,8}\" )/\1background_color=\"#$defcol\" /") # Add circle bg color
 
      echo $l >> $ofd # Modified line from light input file to dark output file
 
