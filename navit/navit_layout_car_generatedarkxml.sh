17f516a150c90aa25a42e3fe21398e137c14b2af
Johan Fitié <jfitie@gmail.com>
2020-06-24 15:44:40 +0200

    improvement:layout:car-dark Improve Car-Dark layout colors (#1028)
    * Improve Car-Dark layout colors
    
    * Color tweak
    
    * Circle text bg color
    
    * Variable
    
    * Small script cleanups

--- navit/navit_layout_car_generatedarkxml.sh
+++ navit/navit_layout_car_generatedarkxml.sh
@@ -5,6 +5,10 @@
 # navit_layout_car_dark_shipped.xml.
 # It's a bit of a mess, but gets the job done.
 
+# Variables
+textcolor='55c4bd'
+
+# Settings
 IFS='' # Keep whitespace
 
 # Light and dark input files
@@ -68,23 +72,41 @@ do
 
      # Modify color
      if [[ $l =~ .*color=\"#[0-9a-fA-F]{6}.* ]]; then # Contains rgb(a) color
-      coll=$(echo $l | cut -d# -f2 | cut -c-6)        # Get rgb color and convert
-      cold=$(     printf '%02x' $(echo $(printf "%d" 0x${coll:0:2})/16+16 | bc))
-      cold=$cold$(printf '%02x' $(echo $(printf "%d" 0x${coll:2:2})/10+14 | bc))
-      cold=$cold$(printf '%02x' $(echo $(printf "%d" 0x${coll:4:2})/8+12  | bc))
-      l=$(echo $l | sed "s/#$coll/#$cold/")           # Replace color
+
+      # Get hexadecimal color
+      coll=$(echo $l | cut -d# -f2 | cut -c-6)
+
+      # Get color values in decimal
+      cr=$(printf "%d" 0x${coll:0:2})
+      cg=$(printf "%d" 0x${coll:2:2})
+      cb=$(printf "%d" 0x${coll:4:2})
+
+      # Modify decimal color values
+      crn=$(echo $cr/16+$cg/32+$cb/32+16 | bc)
+      cgn=$(echo $cr/24+$cg/12+$cb/24+12 | bc)
+      cbn=$(echo $cr/16+$cg/16+$cb/ 8+ 8 | bc)
+
+      # Convert new decimal color values to hexadecimal
+      cold=$(     printf '%02x' $crn)
+      cold=$cold$(printf '%02x' $cgn)
+      cold=$cold$(printf '%02x' $cbn)
+
+      # Replace old color with new hexadecimal color values
+      l=$(echo $l | sed "s/#$coll/#$cold/")
+
      fi
 
-     # Misc. modifications
+     # Miscellaneous modifications
      l=$(echo $l | sed -r "s/_bk\./_wh\./") # Black to white icons
-     l=$(echo $l | sed -r "s/(<text )/\1color=\"#55c4bd\" background_color=\"#000000\" /") # Add text color
-     l=$(echo $l | sed -r "s/(<circle color=\"#[0-9a-fA-F]{6,8}\" )/\1background_color=\"#000000\" /") # Add circle bg color
+     l=$(echo $l | sed -r "s/(<text )/\1color=\"#$textcolor\" background_color=\"#000000\" /") # Add text color
+     l=$(echo $l | sed -r "s/(<circle color=\"#[0-9a-fA-F]{6,8}\" )/\1background_color=\"#$textcolor\" /") # Add circle bg color
 
      echo $l >> $ofd # Modified line from light input file to dark output file
 
     fi
 
    done < $ifl # Read light input file
+
    inlayer=true # Done inserting, still in layer in dark input file
 
   else
