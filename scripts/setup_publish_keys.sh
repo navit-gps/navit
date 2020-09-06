6e256078cae4073f107d88baf617e1672727f5d5
jkoan <jkoan@gmx.de>
2020-09-06 16:43:11 +0000

    fix:build:Fix Codesigning with newer ndk image versions

--- scripts/setup_publish_keys.sh
+++ scripts/setup_publish_keys.sh
@@ -7,8 +7,7 @@ if [[ -n $GOOGLE_KEY ]]; then
 fi
 if [[ -n $KEY ]]; then
   wget "https://github.com/navit-gps/infrastructure-blackbox/raw/master/keyrings/keystore.gpg"
-  openssl aes-256-cbc -d -in keystore.gpg -md md5 -k $KEY > ~/.keystore
-  keytool -importkeystore -srcstorepass "$STORE_PASS" -deststorepass "$STORE_PASS" -srckeystore ~/.keystore -destkeystore ~/.keystore -deststoretype pkcs12
-  keytool -keypasswd -alias $KEY_ALIAS -storepass $STORE_PASS -new $KEY_PASS -keystore ~/.keystore
-  rm keystore.gpg
+  openssl aes-256-cbc -d -in keystore.gpg -md md5 -k $KEY > keystore
+  keytool -importkeystore -srcstorepass "$STORE_PASS" -deststorepass "$STORE_PASS" -srckeystore keystore -destkeystore ~/.keystore -deststoretype pkcs12
+  rm keystore.gpg keystore
 fi
