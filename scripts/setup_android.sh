9cb4dcec171b778f0661b6f429d834e4d22bc261
OLFDB <olf@eisenzelt.de>
2021-05-23 14:39:37 +0200

    Cleanup and sanitycheck

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -2,6 +2,6 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y cmake gettext libsaxonb-java librsvg2-bin pkg-config rename 
+sudo apt-get install -y cmake gettext libsaxonb-java librsvg2-bin pkg-config rename
 gem install --no-document fastlane git
 
