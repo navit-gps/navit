58efe26f48b02583985a4a46449ce1570cbaad7d
Viktor Verebelyi <vviktor2@gmail.com>
2021-06-22 17:27:42 +0000

    fix:qt5_gui move NavitHelper to gt5_graphics and link qt5_graphics
    # Conflicts:
    #	navit/graphics/qt5/CMakeLists.txt

---
+++ navit/graphics/qt5/navithelper.h
@@ -0,0 +1,53 @@
+#ifndef NAVITHELPER_H
+#define NAVITHELPER_H
+
+#include <QString>
+#include <QDebug>
+
+#include "navitinstance.h"
+
+#include <glib.h>
+extern "C" {
+#include "config.h"
+#include "item.h" /* needs to be first, as attr.h depends on it */
+#include "navit.h"
+
+#include "coord.h"
+#include "attr.h"
+#include "xmlconfig.h" // for NAVIT_OBJECT
+#include "layout.h"
+#include "map.h"
+#include "transform.h"
+
+#include "mapset.h"
+#include "search.h"
+#include "bookmarks.h"
+
+#include "proxy.h"
+
+#include "layout.h"
+
+#include "debug.h"
+
+#include "search.h"
+}
+
+class NavitHelper
+{
+ public:
+  NavitHelper();
+
+  static QString getAddress(NavitInstance * navitInstance, struct coord center, QString filter = "");
+  static QVariantMap getPOI(NavitInstance *navitInstance, struct coord center, int distance = 2);
+  static QString getClosest(QList<QVariantMap> items, int maxDistance = -1);
+  static QString formatDist(int dist);
+  static pcoord positionToPcoord (NavitInstance *navitInstance, int x, int y);
+  static coord positionToCoord (NavitInstance *navitInstance, int x, int y);
+  static pcoord coordToPcoord(NavitInstance *navitInstance, int x, int y);
+  static void setDestination(NavitInstance *navitInstance, QString label, int x, int y);
+  static void setPosition(NavitInstance *navitInstance, int x, int y);
+  static void addBookmark(NavitInstance *navitInstance, QString label, int x, int y);
+  static void addStop(NavitInstance *navitInstance, int position, QString label, int x, int y);
+};
+
+#endif // NAVITHELPER_H
