fad676b98dd9937cddc19741186c6a2c40088cb8
OLFDB <olf@eisenzelt.de>
2021-10-09 08:52:35 +0200

    Changes for ios

--- navit/start_apple.m
+++ navit/start_apple.m
@@ -27,27 +27,29 @@ int main(int argc, char **argv) {
     int ret;
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     NSString *appFolderPath = [[NSBundle mainBundle] resourcePath];
-    /*NSString *locale = [[NSLocale currentLocale] localeIdentifier];*/
-    NSString * locale = [[NSLocale preferredLanguages] objectAtIndex:0];
-    char *lang=g_strdup_printf("%s.UTF-8",[locale UTF8String]);
+    //NSString *locale = [[NSLocale currentLocale] localeIdentifier];
+    NSString *language = [[NSLocale preferredLanguages] firstObject];
+    if(language.UTF8String[2]=='-') //IOS returns e.g. de-DE instead of de_DE
+        language=[language stringByReplacingOccurrencesOfString:@"-" withString:@"_"];
+    char *lang=g_strdup_printf("%s.UTF-8",[language UTF8String]);
     dbg(0,"lang %s",lang);
     setenv("LANG",lang,0);
     setlocale(LC_ALL, NULL);
+    setlocale(LC_NUMERIC,"C");
 
     const char *s=[appFolderPath UTF8String];
-    char *user=g_strdup_printf("%s/../Documents",s);
+#if IOS
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    char *user=(char *)[documentsDirectory cStringUsingEncoding:[NSString defaultCStringEncoding]]	;
+#else
+    char *user=g_strdup_printf("%s/.navit", NSHomeDirectory().UTF8String);
+#endif
     chdir(s);
     argv[0]=g_strdup_printf("%s/bin/navit",s);
-    char *home = getenv("HOME");
-    home = g_strdup_printf("%s/.navit",home);
-    setenv("NAVIT_USER_DATADIR", home,0);
+    setenv("NAVIT_USER_DATADIR",user,0);
 
-    char *share=g_strdup_printf("%s/share",s);
-    setenv("NAVIT_SHAREDIR", share,0);
-
-    setenv("NAVIT_LIBDIR", s, 0);
-
-    dbg(0,"calling main_real data: %s  share: %s", home, share);
+    dbg(0,"calling main_real");
     ret=main_real(argc, argv);
     g_free(argv[0]);
     g_free(user);
