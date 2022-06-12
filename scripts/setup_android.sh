1b1126ff1cd227183c3e0e1c07f732a365316a81
jkoan <jkoan@gmx.de>
2022-06-12 13:06:55 +0200

    fix:build:android:run gem install with as root

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -3,6 +3,6 @@ set -e
 
 sudo apt-get update
 sudo apt-get install -y cmake gettext libsaxonb-java librsvg2-bin pkg-config rename ruby ruby-dev build-essential rake-compiler git default-jdk-headless
-gem install rake
-gem install --no-document fastlane git
+sudo gem install rake
+sudo gem install --no-document fastlane git
 
