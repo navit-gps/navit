527f80689f39438355ae61ad627fe14aba5f3a4b
jkoan <jkoan@gmx.de>
2022-06-12 12:40:48 +0200

    fix:build:android:Make build compatible with act (https://github.com/nektos/act/) to test localy

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -2,6 +2,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y cmake gettext libsaxonb-java librsvg2-bin pkg-config rename
+sudo apt-get install -y cmake gettext libsaxonb-java librsvg2-bin pkg-config rename ruby ruby-dev build-essential rake-compiler git default-jdk-headless
+gem install rake
 gem install --no-document fastlane git
 
