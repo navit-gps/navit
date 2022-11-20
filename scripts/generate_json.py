9b7fc5f36c42ad564c9669b665d2c22051ea5249
Patrick Höhn <hoehnp@gmx.de>
2022-11-20 23:14:24 +0100

    change to tsv format for menu

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -93,4 +93,5 @@ for continent in continents:
         listdata.append([continent, size_continent])
     size_planet = size_planet + size_continent
 
-pd.concat([pd.DataFrame([['planet',size_planet]]), pd.DataFrame(listdata).sort_values(by=[0]).drop_duplicates()]).to_csv('./menu.csv', header=False, index=False)
+pd.concat([pd.DataFrame([['planet',size_planet]]),
+pd.DataFrame(listdata).sort_values(by=[0]).drop_duplicates()]).to_csv('./menu.tsv', header=False, index=False, sep="\t")
