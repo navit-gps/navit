71bc50eaac4342c79f935394beef7686d7acb870
jkoan <jkoan@gmx.de>
2023-01-17 21:07:15 +0100

    change:core:Update googletest to last cmake tutorial

---
+++ test/suntime.cpp
@@ -0,0 +1,24 @@
+extern "C"{
+#include <math.h>
+#include "sunriset.h"
+}
+#include <gtest/gtest.h>
+
+TEST(SearchTestSuite, sunriset_equator) {
+    double rise;
+    double tset;
+    // Values from https://api.sunrise-sunset.org/json?lat=0.0&lng=0.0&date=2020-01-01
+    ASSERT_EQ(__sunriset__(2020,1,1,0.0,0.0,-0.8,1,&rise,&tset), 0);
+    ASSERT_EQ(HOURS(rise),5);
+    ASSERT_EQ(HOURS(tset),18);
+    // TODO: Check minutes in Range?
+}
+
+TEST(SearchTestSuite, pole) {
+    double rise;
+    double tset;
+    // https://api.sunrise-sunset.org/json?lat=90.0&lng=0.0&date=2020-01-01
+    ASSERT_EQ(__sunriset__(2020,1,1,90.0,0.0,-0.8,1,&rise,&tset), 0);
+    ASSERT_EQ(HOURS(rise),12);
+    ASSERT_EQ(HOURS(tset),12);
+}
