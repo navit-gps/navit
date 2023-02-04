0b08fad7995bd09553d64ef2f37117ca645ccc01
jkoan <jkoan@gmx.de>
2023-02-04 21:19:09 +0100

    fix northpole coords

--- test/suntime.cpp
+++ test/suntime.cpp
@@ -4,7 +4,7 @@ extern "C" {
 }
 #include <gtest/gtest.h>
 
-TEST(SearchTestSuite, sunriset_equator) {
+TEST(SunTimeTestSuite, sunriset_equator) {
     double rise;
     double tset;
     // Values from https://api.sunrise-sunset.org/json?lat=0.0&lng=0.0&date=2020-01-01
@@ -14,11 +14,11 @@ TEST(SearchTestSuite, sunriset_equator) {
     // TODO: Check minutes in Range?
 }
 
-TEST(SearchTestSuite, pole) {
+TEST(SunTimeTestSuite, pole) {
     double rise;
     double tset;
     // https://api.sunrise-sunset.org/json?lat=90.0&lng=0.0&date=2020-01-01
-    ASSERT_EQ(__sunriset__(2020,1,1,90.0,0.0,-0.8,1,&rise,&tset), 0);
+    ASSERT_EQ(__sunriset__(2020,1,1,0.0,90.0,-0.8,1,&rise,&tset), -1);
     ASSERT_EQ(HOURS(rise),12);
     ASSERT_EQ(HOURS(tset),12);
 }
