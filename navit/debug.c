3acba4bcb26fece139f9606464cce0d89a421ba6
OLFDB <olf@eisenzelt.de>
2023-02-05 18:46:17 +0100

    WINCE compiler needs pragma to be outside of a function

--- navit/debug.c
+++ navit/debug.c
@@ -472,6 +472,9 @@ void debug_dump_mallocs(void) {
     }
 }
 
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wframe-address"			// We know what we are doing here, suppress warning
 void *debug_malloc(const char *where, int line, const char *func, int size) {
     struct malloc_head *head;
     struct malloc_tail *tail;
@@ -495,8 +498,6 @@ void *debug_malloc(const char *where, int line, const char *func, int size) {
 #if !defined (__GNUC__)
 #define __builtin_return_address(x) NULL
 #endif
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wframe-address"			// We know what we doing here, suppress warnings
     head->return_address[0]=__builtin_return_address(0);
     head->return_address[1]=__builtin_return_address(1);
     head->return_address[2]=__builtin_return_address(2);
@@ -505,13 +506,12 @@ void *debug_malloc(const char *where, int line, const char *func, int size) {
     head->return_address[5]=__builtin_return_address(5);
     head->return_address[6]=__builtin_return_address(6);
     head->return_address[7]=__builtin_return_address(7);
-#pragma GCC diagnostic pop
     head++;
     tail=(struct malloc_tail *)((unsigned char *)head+size);
     tail->magic=0xdeadbef0;
     return head;
 }
-
+#pragma GCC diagnostic pop
 
 void *debug_malloc0(const char *where, int line, const char *func, int size) {
     void *ret=debug_malloc(where, line, func, size);
