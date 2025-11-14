b881be45c5a2fdb2aa1d9919644ebc7b2f07f86a
Christof Schulze <christof.schulze@gmx.net>
2025-11-14 08:39:22 +0100

    DRAFT: prefer localized german names if they exist
    This implements an illustration of option 4 outlined in #1325.

--- navit/maptool/osm.c
+++ navit/maptool/osm.c
@@ -1191,7 +1191,10 @@ void osm_add_tag(char *k, char *v) {
     }
     if (! g_strcmp0(k,"note"))
         level=5;
-    if (! g_strcmp0(k,"name")) {
+    if (! g_strcmp0(k,"name:de")) {
+        attr_strings_save(attr_string_label, v);
+        level=5;
+    } else if (! g_strcmp0(k, "name")) {
         attr_strings_save(attr_string_label, v);
         level=5;
     } else if (! g_strcmp0(k,"description")) {
