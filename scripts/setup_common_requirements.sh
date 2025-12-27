4154efe04932d3893b78e729a580c1ae6d35616b
jkoan <jkoan@gmx.de>
2025-12-27 11:22:10 +0100

    Add:build:Dockerfile: Add Dockerfile for maptool Container

--- scripts/setup_common_requirements.sh
+++ scripts/setup_common_requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-apt-get update && apt-get install -y wget unzip cmake build-essential \
+apt-get update && apt-get install -y --no-install-recommends wget unzip cmake build-essential \
   gettext librsvg2-bin util-linux git ssh sed astyle \
   libprotobuf-c-dev protobuf-c-compiler
