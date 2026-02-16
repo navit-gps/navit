8907c2f96e758219942ba051200a8bc2504f81ab
adevoss <arnobugs@hotmail.com>
2026-02-16 18:12:22 +0100

    add name to speech struct

--- navit/speech.c
+++ navit/speech.c
@@ -72,9 +72,7 @@ int speech_say(struct speech *this_, const char *text) {
     return (this_->meth.say)(this_->priv, text);
 }
 
-//TODO Voice: name should be a string
-struct attr name = ATTR_INT(name, 0);
-
+struct attr name = ATTR_INT(name, "Voice name");
 struct attr active = ATTR_INT(active, 1);
 struct attr *speech_default_attrs[] = {
     &name,
