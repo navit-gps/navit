499c3785af6718756b5180c39ac3be24710d85fa
Christof Schulze <christof.schulze@gmx.net>
2026-02-21 18:54:20 +0100

    chore(whitespace): All CPP files formatted

--- navit/graphics/qt5/QNavitWidget.cpp
+++ navit/graphics/qt5/QNavitWidget.cpp
@@ -19,7 +19,7 @@
 
 #include <glib.h>
 #ifdef HAVE_UNISTD_H
-#include <unistd.h>
+#    include <unistd.h>
 #endif
 extern "C" {
 #include "config.h"
@@ -38,31 +38,29 @@ extern "C" {
 #include "window.h"
 }
 #if defined(WINDOWS) || defined(WIN32) || defined(HAVE_API_WIN32_CE)
-#include <windows.h>
+#    include <windows.h>
 #endif
 #include "QNavitWidget.h"
-//#include "QNavitWidget.moc"
+// #include "QNavitWidget.moc"
 #include "graphics_qt5.h"
 
-QNavitWidget::QNavitWidget(struct graphics_priv* my_graphics_priv,
-                           QWidget* parent,
-                           Qt::WindowFlags flags)
+QNavitWidget::QNavitWidget(struct graphics_priv *my_graphics_priv, QWidget *parent, Qt::WindowFlags flags)
     : QWidget(parent, flags) {
     graphics_priv = my_graphics_priv;
 }
 
-bool QNavitWidget::event(QEvent* event) {
+bool QNavitWidget::event(QEvent *event) {
     if (event->type() == QEvent::Gesture)
         dbg(lvl_debug, "Gesture event caught");
-    //return gestureEvent(static_cast<QGestureEvent*>(event));
+    // return gestureEvent(static_cast<QGestureEvent*>(event));
     return QWidget::event(event);
 }
 
-static void paintOverlays(QPainter* painter, struct graphics_priv* gp, QPaintEvent* event) {
+static void paintOverlays(QPainter *painter, struct graphics_priv *gp, QPaintEvent *event) {
     GHashTableIter iter;
     struct graphics_priv *key, *value;
     g_hash_table_iter_init(&iter, gp->overlays);
-    while (g_hash_table_iter_next(&iter, (void**)&key, (void**)&value)) {
+    while (g_hash_table_iter_next(&iter, (void **)&key, (void **)&value)) {
         if (!value->disable) {
             QRect rr(value->x, value->y, value->pixmap->width(), value->pixmap->height());
             if (event->rect().intersects(rr)) {
@@ -76,7 +74,7 @@ static void paintOverlays(QPainter* painter, struct graphics_priv* gp, QPaintEve
     }
 }
 
-void QNavitWidget::paintEvent(QPaintEvent* event) {
+void QNavitWidget::paintEvent(QPaintEvent *event) {
     dbg(lvl_debug, "enter (%d, %d, %d, %d)", event->rect().x(), event->rect().y(), event->rect().width(),
         event->rect().height());
     QPainter painter(this);
@@ -89,14 +87,14 @@ void QNavitWidget::paintEvent(QPaintEvent* event) {
                        event->rect().x() - graphics_priv->scroll_x, event->rect().y() - graphics_priv->scroll_y,
                        event->rect().width(), event->rect().height());
     /* disable on root pane disables ALL overlays (for drag of background) */
-    if(!(graphics_priv->disable)) {
+    if (!(graphics_priv->disable)) {
         paintOverlays(&painter, graphics_priv, event);
     };
 }
 
-void QNavitWidget::resizeEvent(QResizeEvent* event) {
+void QNavitWidget::resizeEvent(QResizeEvent *event) {
     if (graphics_priv->pixmap != NULL) {
-        if((width() != graphics_priv->pixmap->width()) || (height() != graphics_priv->pixmap->height())) {
+        if ((width() != graphics_priv->pixmap->width()) || (height() != graphics_priv->pixmap->height())) {
             delete graphics_priv->pixmap;
             graphics_priv->pixmap = NULL;
         }
@@ -113,7 +111,7 @@ void QNavitWidget::resizeEvent(QResizeEvent* event) {
         resize_callback(graphics_priv, width(), height());
 }
 
-void QNavitWidget::mouseEvent(int pressed, QMouseEvent* event) {
+void QNavitWidget::mouseEvent(int pressed, QMouseEvent *event) {
     struct point p;
     //        dbg(lvl_debug,"enter");
     p.x = event->x();
@@ -136,11 +134,11 @@ void QNavitWidget::mouseEvent(int pressed, QMouseEvent* event) {
     }
 }
 
-void QNavitWidget::keyPressEvent(QKeyEvent* event) {
+void QNavitWidget::keyPressEvent(QKeyEvent *event) {
     dbg(lvl_debug, "enter");
     char key[2];
     int keycode;
-    char* text = NULL;
+    char *text = NULL;
 
     keycode = event->key();
     key[0] = '\0';
@@ -193,41 +191,41 @@ void QNavitWidget::keyPressEvent(QKeyEvent* event) {
         }
     }
     if (text != NULL)
-        callback_list_call_attr_1(graphics_priv->callbacks, attr_keypress, (void*)text);
+        callback_list_call_attr_1(graphics_priv->callbacks, attr_keypress, (void *)text);
     else if (key[0])
-        callback_list_call_attr_1(graphics_priv->callbacks, attr_keypress, (void*)key);
+        callback_list_call_attr_1(graphics_priv->callbacks, attr_keypress, (void *)key);
     else
         dbg(lvl_debug, "keyval 0x%x", keycode);
 }
 
-void QNavitWidget::mousePressEvent(QMouseEvent* event) {
+void QNavitWidget::mousePressEvent(QMouseEvent *event) {
     //        dbg(lvl_debug,"enter");
     mouseEvent(1, event);
 }
 
-void QNavitWidget::mouseReleaseEvent(QMouseEvent* event) {
+void QNavitWidget::mouseReleaseEvent(QMouseEvent *event) {
     //        dbg(lvl_debug,"enter");
     mouseEvent(0, event);
 }
 
-void QNavitWidget::mouseMoveEvent(QMouseEvent* event) {
+void QNavitWidget::mouseMoveEvent(QMouseEvent *event) {
     struct point p;
     //        dbg(lvl_debug,"enter");
     p.x = event->x();
     p.y = event->y();
-    callback_list_call_attr_1(graphics_priv->callbacks, attr_motion, (void*)&p);
+    callback_list_call_attr_1(graphics_priv->callbacks, attr_motion, (void *)&p);
 }
 
-void QNavitWidget::wheelEvent(QWheelEvent* event) {
+void QNavitWidget::wheelEvent(QWheelEvent *event) {
     struct point p;
     int button;
     dbg(lvl_debug, "enter");
-    p.x = event->x(); // xy-coordinates of the mouse pointer
+    p.x = event->x();  // xy-coordinates of the mouse pointer
     p.y = event->y();
 
-    if (event->delta() > 0) // wheel movement away from the person
+    if (event->delta() > 0)  // wheel movement away from the person
         button = 4;
-    else if (event->delta() < 0) // wheel movement towards the person
+    else if (event->delta() < 0)  // wheel movement towards the person
         button = 5;
     else
         button = -1;
