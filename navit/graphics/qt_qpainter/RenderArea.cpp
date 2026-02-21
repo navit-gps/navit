b7e1e07b2d11eddd3827f52daf1895b5b61b20e8
Christof Schulze <christof.schulze@gmx.net>
2026-02-21 18:54:20 +0100

    chore(whitespace): All CPP files formatted

--- navit/graphics/qt_qpainter/RenderArea.cpp
+++ navit/graphics/qt_qpainter/RenderArea.cpp
@@ -16,92 +16,91 @@
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  */
-#include "graphics_qt_qpainter.h"
 #include "RenderArea.h"
+#include "graphics_qt_qpainter.h"
 
 #include "RenderArea.moc"
 
 #ifdef QT_QPAINTER_USE_EMBEDDING
-EmbeddedWidget::EmbeddedWidget(struct graphics_priv *priv, QWidget* child, QWidget *parent)
-    : QX11EmbedWidget(parent) {
-    this->gra=priv;
+EmbeddedWidget::EmbeddedWidget(struct graphics_priv *priv, QWidget *child, QWidget *parent) : QX11EmbedWidget(parent) {
+    this->gra = priv;
     this->setWindowTitle(priv->window_title);
-    QStackedLayout* _outerLayout = new QStackedLayout(this);
+    QStackedLayout *_outerLayout = new QStackedLayout(this);
     this->setLayout(_outerLayout);
     _outerLayout->addWidget(child);
     _outerLayout->setCurrentWidget(child);
 }
 
-void EmbeddedWidget::closeEvent(QCloseEvent* event) {
+void EmbeddedWidget::closeEvent(QCloseEvent *event) {
     gra->widget->processClose();
 }
 #endif /* QT_QPAINTER_USE_EMBEDDING */
 
-//##############################################################################################################
-//# Description: Constructor
-//# Comment: Using a QPixmap for rendering the graphics
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: Constructor
+// # Comment: Using a QPixmap for rendering the graphics
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
 RenderArea::RenderArea(struct graphics_priv *priv, QT_QPAINTER_RENDERAREA_PARENT *parent, int w, int h, int overlay)
     : QT_QPAINTER_RENDERAREA_PARENT(parent) {
     pixmap = new QPixmap(w, h);
 #ifndef QT_QPAINTER_NO_WIDGET
     if (!overlay) {
-#if QT_VERSION >= 0x040700
+#    if QT_VERSION >= 0x040700
         grabGesture(Qt::PinchGesture);
         grabGesture(Qt::SwipeGesture);
         grabGesture(Qt::PanGesture);
-#endif
+#    endif
         setWindowTitle(priv->window_title);
     }
 #endif
-    is_overlay=overlay;
-    gra=priv;
+    is_overlay = overlay;
+    gra = priv;
 #ifdef QT_QPAINTER_USE_EVENT_QT
-    timer_type=g_hash_table_new(NULL, NULL);
-    timer_callback=g_hash_table_new(NULL, NULL);
-    watches=g_hash_table_new(NULL, NULL);
-#ifndef QT_QPAINTER_NO_WIDGET
+    timer_type = g_hash_table_new(NULL, NULL);
+    timer_callback = g_hash_table_new(NULL, NULL);
+    watches = g_hash_table_new(NULL, NULL);
+#    ifndef QT_QPAINTER_NO_WIDGET
     setAttribute(Qt::WA_OpaquePaintEvent, true);
-#endif
+#    endif
 #endif
 }
 
-//##############################################################################################################
-//# Description: QWidget:closeEvent
-//# Comment: Deletes navit object and stops event loop on graphics shutdown
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: QWidget:closeEvent
+// # Comment: Deletes navit object and stops event loop on graphics shutdown
+// ##############################################################################################################
 void RenderArea::processClose() {
     callback_list_call_attr_0(this->cbl, attr_window_closed);
 }
-void RenderArea::closeEvent(QCloseEvent* event) {
+void RenderArea::closeEvent(QCloseEvent *event) {
     this->processClose();
 }
 
 bool RenderArea::event(QEvent *event) {
 #if QT_VERSION >= 0x040700
     if (event->type() == QEvent::Gesture) {
-        dbg(lvl_debug,"gesture");
+        dbg(lvl_debug, "gesture");
         return true;
     }
 #endif
     return QWidget::event(event);
 }
-//##############################################################################################################
-//# Description: QWidget:sizeHint
-//# Comment: This property holds the recommended size for the widget
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: QWidget:sizeHint
+// # Comment: This property holds the recommended size for the widget
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
 QSize RenderArea::sizeHint() const {
     return QSize(gra->w, gra->h);
 }
 
-//##############################################################################################################
-//# Description: QWidget:paintEvent
-//# Comment: A paint event is a request to repaint all or part of the widget.
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
-void RenderArea::paintEvent(QPaintEvent * event) {
+// ##############################################################################################################
+// # Description: QWidget:paintEvent
+// # Comment: A paint event is a request to repaint all or part of the widget.
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
+void RenderArea::paintEvent(QPaintEvent *event) {
     qt_qpainter_draw(gra, &event->rect(), 1);
 }
 
@@ -110,45 +109,48 @@ void RenderArea::do_resize(QSize size) {
         pixmap->paintEngine()->painter()->end();
     }
     delete pixmap;
-    pixmap=new QPixmap(size);
+    pixmap = new QPixmap(size);
     pixmap->fill();
     QPainter painter(pixmap);
     QBrush brush;
     painter.fillRect(0, 0, size.width(), size.height(), brush);
-    dbg(lvl_debug,"size %dx%d", size.width(), size.height());
-    dbg(lvl_debug,"pixmap %p %dx%d", pixmap, pixmap->width(), pixmap->height());
+    dbg(lvl_debug, "size %dx%d", size.width(), size.height());
+    dbg(lvl_debug, "pixmap %p %dx%d", pixmap, pixmap->width(), pixmap->height());
     callback_list_call_attr_2(this->cbl, attr_resize, GINT_TO_POINTER(size.width()), GINT_TO_POINTER(size.height()));
 }
 
-//##############################################################################################################
-//# Description: QWidget::resizeEvent()
-//# Comment: When resizeEvent() is called, the widget already has its new geometry.
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
-void RenderArea::resizeEvent(QResizeEvent * event) {
+// ##############################################################################################################
+// # Description: QWidget::resizeEvent()
+// # Comment: When resizeEvent() is called, the widget already has its new geometry.
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
+void RenderArea::resizeEvent(QResizeEvent *event) {
     if (!this->is_overlay) {
         RenderArea::do_resize(event->size());
     }
 }
 
-//##############################################################################################################
-//# Description: Method to handle mouse clicks
-//# Comment: Delegate of QWidget::mousePressEvent and QWidget::mouseReleaseEvent (see below)
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: Method to handle mouse clicks
+// # Comment: Delegate of QWidget::mousePressEvent and QWidget::mouseReleaseEvent (see below)
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
 void RenderArea::mouseEvent(int pressed, QMouseEvent *event) {
     struct point p;
-    p.x=event->x();
-    p.y=event->y();
+    p.x = event->x();
+    p.y = event->y();
     switch (event->button()) {
     case Qt::LeftButton:
-        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(1), GINT_TO_POINTER(&p));
+        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(1),
+                                  GINT_TO_POINTER(&p));
         break;
     case Qt::MidButton:
-        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(2), GINT_TO_POINTER(&p));
+        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(2),
+                                  GINT_TO_POINTER(&p));
         break;
     case Qt::RightButton:
-        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(3), GINT_TO_POINTER(&p));
+        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(pressed), GINT_TO_POINTER(3),
+                                  GINT_TO_POINTER(&p));
         break;
     default:
         break;
@@ -163,41 +165,42 @@ void RenderArea::mouseReleaseEvent(QMouseEvent *event) {
     mouseEvent(0, event);
 }
 
-//##############################################################################################################
-//# Description: QWidget::mouseMoveEvent
-//# Comment: If mouse tracking is switched on, mouse move events occur even if no mouse button is pressed.
-//# Authors: Martin Schaller (04/2008)
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: QWidget::mouseMoveEvent
+// # Comment: If mouse tracking is switched on, mouse move events occur even if no mouse button is pressed.
+// # Authors: Martin Schaller (04/2008)
+// ##############################################################################################################
 void RenderArea::mouseMoveEvent(QMouseEvent *event) {
     struct point p;
-    p.x=event->x();
-    p.y=event->y();
+    p.x = event->x();
+    p.y = event->y();
     callback_list_call_attr_1(this->cbl, attr_motion, (void *)&p);
 }
 
-
-//##############################################################################################################
-//# Description: Qt Event :: Zoom in/out with the mouse's scrollwheel
-//# Comment:
-//# Authors: Stefan Klumpp (04/2008)
-//##############################################################################################################
+// ##############################################################################################################
+// # Description: Qt Event :: Zoom in/out with the mouse's scrollwheel
+// # Comment:
+// # Authors: Stefan Klumpp (04/2008)
+// ##############################################################################################################
 void RenderArea::wheelEvent(QWheelEvent *event) {
     struct point p;
     int button;
 
-    p.x=event->x();	// xy-coordinates of the mouse pointer
-    p.y=event->y();
+    p.x = event->x();  // xy-coordinates of the mouse pointer
+    p.y = event->y();
 
-    if (event->delta() > 0)	// wheel movement away from the person
-        button=4;
-    else if (event->delta() < 0) // wheel movement towards the person
-        button=5;
+    if (event->delta() > 0)  // wheel movement away from the person
+        button = 4;
+    else if (event->delta() < 0)  // wheel movement towards the person
+        button = 5;
     else
-        button=-1;
+        button = -1;
 
     if (button != -1) {
-        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(1), GINT_TO_POINTER(button), GINT_TO_POINTER(&p));
-        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(0), GINT_TO_POINTER(button), GINT_TO_POINTER(&p));
+        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(1), GINT_TO_POINTER(button),
+                                  GINT_TO_POINTER(&p));
+        callback_list_call_attr_3(this->cbl, attr_button, GINT_TO_POINTER(0), GINT_TO_POINTER(button),
+                                  GINT_TO_POINTER(&p));
     }
 
     event->accept();
@@ -206,24 +209,23 @@ void RenderArea::wheelEvent(QWheelEvent *event) {
 #define CASE(x) case x:
 
 void RenderArea::keyPressEvent(QKeyEvent *event) {
-    QString str=event->text();
-    const char *text=str.toUtf8().constData();
-    dbg(lvl_debug,"enter text='%s' 0x%x (%zu) key=%d", text, text[0], strlen(text), event->key());
+    QString str = event->text();
+    const char *text = str.toUtf8().constData();
+    dbg(lvl_debug, "enter text='%s' 0x%x (%zu) key=%d", text, text[0], strlen(text), event->key());
     if (!text || !text[0] || text[0] == 0x7f) {
-        dbg(lvl_debug,"special key");
+        dbg(lvl_debug, "special key");
         switch (event->key()) {
         case 4099: {
-            char text_backspace[] = {NAVIT_KEY_BACKSPACE,'\0'};
-            text=text_backspace;
-        }
-        break;
+            char text_backspace[] = {NAVIT_KEY_BACKSPACE, '\0'};
+            text = text_backspace;
+        } break;
         case 4101:
 #ifdef QT_QPAINTER_CUSTOM_RETURN
             QT_QPAINTER_CUSTOM_RETURN
 #endif
             {
-                char text_return[] = {NAVIT_KEY_RETURN,'\0'};
-                text=text_return;
+                char text_return[] = {NAVIT_KEY_RETURN, '\0'};
+                text = text_return;
             }
             break;
         case 4114:
@@ -231,8 +233,8 @@ void RenderArea::keyPressEvent(QKeyEvent *event) {
             QT_QPAINTER_CUSTOM_LEFT
 #endif
             {
-                char text_left[] = {NAVIT_KEY_LEFT,'\0'};
-                text=text_left;
+                char text_left[] = {NAVIT_KEY_LEFT, '\0'};
+                text = text_left;
             }
             break;
         case 4115:
@@ -240,8 +242,8 @@ void RenderArea::keyPressEvent(QKeyEvent *event) {
             QT_QPAINTER_CUSTOM_UP
 #endif
             {
-                char text_up[] = {NAVIT_KEY_UP,'\0'};
-                text=text_up;
+                char text_up[] = {NAVIT_KEY_UP, '\0'};
+                text = text_up;
             }
             break;
         case 4116:
@@ -249,8 +251,8 @@ void RenderArea::keyPressEvent(QKeyEvent *event) {
             QT_QPAINTER_CUSTOM_RIGHT
 #endif
             {
-                char text_right[] = {NAVIT_KEY_RIGHT,'\0'};
-                text=text_right;
+                char text_right[] = {NAVIT_KEY_RIGHT, '\0'};
+                text = text_right;
             }
             break;
         case 4117:
@@ -258,8 +260,8 @@ void RenderArea::keyPressEvent(QKeyEvent *event) {
             QT_QPAINTER_CUSTOM_DOWN
 #endif
             {
-                char text_down[] = {NAVIT_KEY_DOWN,'\0'};
-                text=text_down;
+                char text_down[] = {NAVIT_KEY_DOWN, '\0'};
+                text = text_down;
             }
             break;
         }
@@ -270,20 +272,19 @@ void RenderArea::keyPressEvent(QKeyEvent *event) {
 
 void RenderArea::watchEvent(int fd) {
 #ifdef QT_QPAINTER_USE_EVENT_QT
-    struct event_watch *ev=(struct event_watch *)g_hash_table_lookup(watches, (void *)fd);
-    dbg(lvl_debug,"fd=%d ev=%p cb=%p", fd, ev, ev->cb);
+    struct event_watch *ev = (struct event_watch *)g_hash_table_lookup(watches, (void *)fd);
+    dbg(lvl_debug, "fd=%d ev=%p cb=%p", fd, ev, ev->cb);
     callback_call_0(ev->cb);
 #endif
 }
 
 #ifdef QT_QPAINTER_USE_EVENT_QT
 void RenderArea::timerEvent(QTimerEvent *event) {
-    int id=event->timerId();
-    struct callback *cb=(struct callback *)g_hash_table_lookup(timer_callback, (void *)id);
+    int id = event->timerId();
+    struct callback *cb = (struct callback *)g_hash_table_lookup(timer_callback, (void *)id);
     if (cb)
         callback_call_0(cb);
     if (!g_hash_table_lookup(timer_type, (void *)id))
         event_qt_remove_timeout((struct event_timeout *)id);
 }
 #endif /* QT_QPAINTER_USE_EVENT_QT */
-
