5a23ef878787bde386f11330320b049cd5ed8cb1
Christof Schulze <christof.schulze@gmx.net>
2026-03-12 23:13:47 +0100

    fixup! desperate fix

--- navit/support/glib/gutils.c
+++ navit/support/glib/gutils.c
@@ -89,6 +89,7 @@
 #  include <direct.h>
 #endif /* NOT_NEEDED_FOR_NAVIT */
 #  include <windows.h>
+#  include <winbase.h>
 #  include <shlobj.h>
    /* older SDK (e.g. msvc 5.0) does not have these*/
 #  ifndef CSIDL_MYMUSIC
