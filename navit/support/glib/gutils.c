5e2b564b378a5031fd2ceba85ea9a1b3ba51d041
Christof Schulze <christof.schulze@gmx.net>
2026-03-12 23:55:11 +0100

    fixup! desperate fix

--- navit/support/glib/gutils.c
+++ navit/support/glib/gutils.c
@@ -89,7 +89,7 @@
 #  include <direct.h>
 #endif /* NOT_NEEDED_FOR_NAVIT */
 #  include <windows.h>
-typedef IShellExecuteHookA void
+typedef void IShellExecuteHookA;
 #  include <shlobj.h>
    /* older SDK (e.g. msvc 5.0) does not have these*/
 #  ifndef CSIDL_MYMUSIC
