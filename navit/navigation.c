3045a9db9123da3f20cd92d95cc80e581fbb0f07
Christof Schulze <christof.schulze@gmx.net>
2026-09-20 02:09:54 +0200

    fix(navigation): remove leftover no-op name_attr statement
    name_attr.type == attr_name is a comparison without effect, a leftover
    typo from an earlier refactor. Initialising it on declaration silences
    the unused/possibly-uninitialised warnings for the same variable.

--- navit/navigation.c
+++ navit/navigation.c
@@ -758,9 +758,8 @@ static void navigation_set_turnaround(struct navigation *this_, int val) {
 }
 
 int navigation_set_attr(struct navigation *this_, struct attr *attr) {
-    struct attr name_attr;
+    struct attr name_attr = {0};
 
-    name_attr.type == attr_name;
     switch (attr->type) {
     case attr_speech:
         this_->speech = attr->u.speech;
