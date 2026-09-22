49d14754ccec4056fa632b9ce4d29f2a9b77537d
Christof Schulze <christof.schulze@gmx.net>
2026-09-23 00:02:32 +0200

    feat(build): enable gcc -fanalyzer on debug builds
    filter -fanalyzer when passing flags to iwyu. iwyu rejects non
    clang-flags and that is one of them.

---
+++ cmake/iwyu-wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# First argument is the path to include-what-you-use, the rest are the
+# compiler arguments. include-what-you-use forwards all arguments to its
+# bundled clang driver, which rejects the GCC-only -fanalyzer flag used on
+# Debug builds, so strip it before invoking include-what-you-use.
+set -e
+
+iwyu="$1"
+shift
+
+for arg in "$@"; do
+	case "$arg" in
+		-fanalyzer) ;;
+		*) set -- "$@" "$arg" ;;
+	esac
+	shift
+done
+
+exec "$iwyu" "$@"
